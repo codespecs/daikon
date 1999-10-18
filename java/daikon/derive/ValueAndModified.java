e35e8729b08c5daea0926e9f0b825da4b2d2809b
michael.ernst <devnull@localhost>
1999-10-18 18:26:01 -0400

    Initial revision of Daikon 2 (Java implementation).

---
+++ java/daikon/derive/ValueAndModified.java
@@ -0,0 +1,20 @@
+package daikon.derive;
+
+import daikon.*;
+
+// Just a struct of two elements
+
+// This is a temporary structure for grouping elements to be returned from
+// computeValueAndModified, not for permanent storage.
+public final class ValueAndModified {
+  public int modified;
+  public Object value;		// not necessarily an interned value
+
+  public final static ValueAndModified MISSING
+    = new ValueAndModified(null, ValueTuple.MISSING);
+
+  public ValueAndModified(Object val, int mod) {
+    value = val;
+    modified = mod;
+  }
+}
