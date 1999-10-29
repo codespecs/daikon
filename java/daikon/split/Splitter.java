f43d87912be9dc150b27a960a7882fdbceb4eadb
michael.ernst <devnull@localhost>
1999-10-29 15:18:43 -0400

    Initial revision. Josh, notice that I have moved these out of the top level. I'm still not sure whether this will survive past November, but as long as we are working on it, it is convenient to have it checked in.

---
+++ java/daikon/split/Splitter.java
@@ -0,0 +1,35 @@
+package daikon.split;
+
+/**
+ * A Splitter represents a test that can be used to separate all samples
+ * into two parts.  For instance, a Splitter might represent the condition
+ * "x > 0".  The Splitter is used to divide a collection of variable values
+ * into sub-sets.  Invariant detection can then occur for the two subsets
+ * independently.
+ */
+
+abstract class Splitter {
+
+  /**
+   * Creates a splitter "factory" that should only be used for creating
+   * new copies via instantiate(Ppt).
+   * There is no need for subclasses to override this.  */
+  public Splitter() { }
+
+  /**
+   * Creates a valid splitter than can be used for testing the condition
+   * via test(ValueTuple).
+   */
+  abstract public Splitter instantiate(Ppt ppt);
+
+  /**
+   * Returns true or false according as whether the values in the specified
+   * VarTuple satisfy the condition represented by this Splitter.
+   */
+  abstract public boolean test(ValueTuple vt);
+
+  // This method could be static; but don't bother making it so.
+  /** Returns the condition being tested, as a String. */
+  abstract public String condition();
+
+}
---
+++ java/daikon/split/SplitterExample.java
@@ -0,0 +1,28 @@
+package daikon.split;
+
+// This splitter tests the condition "X>0".
+public class SplitterExample extends Splitter {
+
+  VarInfo x_varinfo;
+
+  public SplitterExample() {
+  }
+
+  public SplitterExample(Ppt ppt) {
+    x_varinfo = ppt.findVar("X");
+  }
+
+  public Splitter instantiate(Ppt ppt) {
+    return new SplitterExample(ppt);
+  }
+
+  public boolean test(ValueTuple vt) {
+    // Alternately, use vt.getIntArrayValue(x_varinfo);
+    return (x_varinfo.getIntValue(vt) > 0);
+  }
+
+  public String condition() {
+    return "X > 0";
+  }
+
+}
