d02bf866e0f15f43caedc1d2c15b44d0e2cfeb3c
joshkata <devnull@localhost>
1999-11-09 18:16:00 -0500

    I added two splitters which are used in a new example Sample13.java. I modified SplitterList4Dsaa.java so that daikon can find proper splitters efficiently.

---
+++ java/daikon/split/dsaa/OrderedListSplitter_insert.java
@@ -0,0 +1,60 @@
+package daikon.split.dsaa;
+
+import daikon.*;
+import daikon.split.*;
+
+class OrderedListSplitter_insert extends Splitter {
+
+    VarInfo ptr_next_varinfo;
+
+// Constructor: OrderedListSplitter_insert()
+// This is a constructor. But I don't know how this could be used...
+
+    public OrderedListSplitter_insert() {}
+
+// Constructor: OrderedListSplitter_insert(Ppt ppt)
+// This instantiates the splitter for a given program point. This
+// also initializes the variable information used in the current
+// splitter.
+
+    public OrderedListSplitter_insert(Ppt ppt) {
+	ptr_next_varinfo = ppt.findVar("ptr.next");
+    }
+
+// Method: valid()
+// This method returns whether all the variables used in the condition
+// are valid at the context or not.
+
+    public boolean valid() {
+	return ((ptr_next_varinfo != null) && true);
+    }
+
+// Method: instantiate(Ppt ppt)
+// This method instantiates the splitter for the given program point.
+// This simply invokes the constructor.
+
+    public Splitter instantiate(Ppt ppt) {
+	return new OrderedListSplitter_insert(ppt);
+    }
+
+// Method: condition()
+// This method returns a String object which contains the original
+// expression of the condition. The word "original" means that if
+// the condition was expressed as a simple boolean method call,
+// this method will return the boolean method call itself though
+// the evaluation might be done in another form.
+
+    public String condition() {
+	return "ptr.next == null";
+    }
+
+// Method: test(ValueTuple vt)
+// This method returns whether the condition is true or false at
+// the given program point. A single boolean method condition can
+// be evaluated by replacing the method with the corresponding
+// expression in the boolean method definition.
+
+    public boolean test(ValueTuple vt) {
+	return (ptr_next_varinfo.getIntValue(vt) == 0);
+    }
+}
