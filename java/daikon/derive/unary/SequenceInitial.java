dc86d08efedf8af8928647d9f3d5a864b8310d70
michael.ernst <devnull@localhost>
2000-10-30 19:50:34 -0500

    Element at a particular offset.

---
+++ java/daikon/derive/unary/SequenceInitial.java
@@ -0,0 +1,80 @@
+package daikon.derive.unary;
+import daikon.*;
+import daikon.derive.*;
+import daikon.derive.binary.*;
+import utilMDE.*;
+
+// This represents a sequence element at a particular offset (such as
+// first, second, penultimate, last).
+
+// originally from pass1.
+public final class SequenceInitial extends UnaryDerivation {
+
+  public final int index;       // negative if counting from end
+  // array length required for the subscript to be meaningful:  (ie, 1 or 2)
+  final int minLength;
+
+  public SequenceInitial(VarInfo vi, int index_) {
+    super(vi);
+    index = index_;
+    if (index < 0)
+      minLength = -index;
+    else
+      minLength = index+1;
+  }
+
+  public VarInfo seqvar() {
+    return var_info;
+  }
+
+  public static boolean applicable(VarInfo vi) {
+    Assert.assert(vi.rep_type == ProglangType.INT_ARRAY);
+    // For now, applicable if not a derived variable; a better test is if
+    // not a prefix subsequence (sequence slice) we have added.
+    if (vi.derived != null) {
+      Assert.assert(vi.derived instanceof SequenceScalarSubsequence);
+      return false;
+    }
+
+    VarInfo lengthvar = vi.sequenceSize();
+    if (lengthvar.isConstant()) {
+      int length_constant = ((Integer) lengthvar.constantValue()).intValue();
+      if (length_constant == 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public ValueAndModified computeValueAndModified(ValueTuple vt) {
+    int source_mod = var_info.getModified(vt);
+    if (source_mod == ValueTuple.MISSING)
+      return ValueAndModified.MISSING;
+    Object val = var_info.getValue(vt);
+    if (val == null)
+      return ValueAndModified.MISSING;
+    if (var_info.rep_type == ProglangType.INT_ARRAY) {
+      int[] val_array = (int[])val;
+      if (val_array.length < minLength)
+        return ValueAndModified.MISSING;
+      int real_index = (index<0 ? val_array.length + index : index);
+      return new ValueAndModified(Intern.internedInteger(val_array[real_index]), source_mod);
+    } else {
+      Object[] val_array = (Object[])val;
+      if (val_array.length < minLength)
+        return ValueAndModified.MISSING;
+      int real_index = (index<0 ? val_array.length + index : index);
+      return new ValueAndModified(val_array[real_index], source_mod);
+    }
+  }
+
+  protected VarInfo makeVarInfo() {
+    String name = var_info.name + "[" + index + "]";
+    ProglangType ptype = var_info.type.elementType();
+    ProglangType rtype = var_info.rep_type.elementType();
+    VarComparability comp = var_info.comparability.elementType();
+    return new VarInfo(name, ptype, rtype, comp);
+  }
+
+}
---
+++ java/daikon/derive/unary/SequenceInitialFactory.java
@@ -0,0 +1,75 @@
+package daikon.derive.unary;
+
+import daikon.*;
+import utilMDE.*;
+
+public final class SequenceInitialFactory extends UnaryDerivationFactory {
+
+  public UnaryDerivation[] instantiate(VarInfo vi) {
+    // System.out.println("SequenceInitialFactory.instantiate(" + vi + ")");
+    // return (UnaryDerivation)new SequenceFirst(vi);
+
+    if (vi.rep_type != ProglangType.INT_ARRAY)
+      return null;
+
+    // System.out.println("SequenceInitial.applicable(" + vi.name + ") = "
+    //                    + SequenceInitial.applicable(vi));
+
+    if (!SequenceInitial.applicable(vi)) {
+      Global.tautological_suppressed_derived_variables += 4;
+      return null;
+    }
+
+    // by default, we use the indices 0, 1, -1, -2.
+    int lowerbound = -2;
+    int upperbound = 1;
+
+    // If the length is constant, adjust the bounds accordingly.
+    VarInfo lengthvar = vi.sequenceSize();
+    if (lengthvar.isConstant()) {
+      int length_constant = ((Integer) lengthvar.constantValue()).intValue();
+      if (length_constant == 0) {
+        Global.tautological_suppressed_derived_variables += 4;
+        return null;
+      } else if (length_constant <= 4) {
+        lowerbound = 0;
+        upperbound = length_constant - 1;
+      }
+    }
+
+    boolean suppress_zero = false;
+    // We know that var.~ll~[0] == var and var.~ll~.field[0] == var.field.
+    if (vi.isClosure()) {
+      suppress_zero = true;
+      if ((lowerbound == 0) && (upperbound == 0))
+        Global.tautological_suppressed_derived_variables += 4;
+        return null;
+    }
+
+    int num_invs = upperbound - lowerbound + 1 - (suppress_zero ? 1 : 0);
+    Assert.assert(num_invs > 0,
+                  "No SequenceInitial invariants to instantiate; "
+                  + "lowerbound=" + lowerbound
+                  + ", upperbound=" + upperbound
+                  + ", suppress_zero=" + suppress_zero);
+    UnaryDerivation[] result = new UnaryDerivation[num_invs];
+    int j=0;
+    for (int i=lowerbound; i<=upperbound; i++) {
+      if (! ((i == 0) && suppress_zero)) {
+        result[j] = new SequenceInitial(vi, i);
+        j++;
+      }
+    }
+    // No longer needed (I hope!).
+    // Assert.assert(j == num_invs,
+    //               "SequenceInitial(" + vi.name + "): "
+    //               + "j=" + j + ", num_invs=" + num_invs
+    //               + ",lowerbound=" + lowerbound
+    //               + ", upperbound=" + upperbound
+    //               + ", suppress_zero=" + suppress_zero);
+
+    Global.tautological_suppressed_derived_variables += 4 - num_invs;
+    return result;
+  }
+
+}
