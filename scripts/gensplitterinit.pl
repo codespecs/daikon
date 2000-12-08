83f8aa96f692be8ffda1f190ff735fa46f189d38
michael.ernst <devnull@localhost>
2000-12-07 20:23:44 -0500

    gensplitterinit.pl cleaned up; documentation added. Josh Kataoka made these changes, but Michael Ernst checked them in.

--- scripts/gensplitterinit.pl
+++ scripts/gensplitterinit.pl
@@ -1,71 +1,92 @@
-#!/uns/bin/perl
+#!/usr/bin/perl
 
-$listclass = $ARGV[0];
-$package = $ARGV[1];
-$depfile = $ARGV[2];
+use Getopt::Std;
 
-if ($listclass eq "") {
-    $listclass = "SplitterListForSomething";
-}
-if ($package eq "") {
-    $package = "daikon.split";
-}
-if ($depfile eq "") {
-    $depfile = "depends.lst";
-}
+main: {
+getopt('cpl');
+
+$listclass = $opt_c;
+$package = $opt_p;
+$splitterlist = $opt_l;
+
+$listclass = "SplitterListForSomething" if ($listclass eq "");
+$package = "daikon.split" if ($package eq "");
+$splitterlist = "splitter.lst" if ($splitterlist eq "");
 $outfile = "$listclass.java";
 
-open(DEPFILE, "$depfile") || die "$depfile: Cannot open file:$!\n!";
-
-while (<DEPFILE>) {
-    if (/(\S+)\s*\:\s*(\S.*)$/) {
-	$base = $1;
-	$_ = $2;
-    }
-    while (/^\s*((\S+)Splitter\S*)($|\s+\S.*)/) {
-	$splitter = $1;
-	if ($class eq $2) {
-	    $i++;
-	    $depends{$class}[$i] = $splitter;
-	} else {
-	    $class = $2;
-	    $i = 0;
-	    $depends{$class}[$i] = $splitter;
-	}
-	$_ = $3;
-    }
-}
-
-close(DEPFILE);
-
-#  foreach $base (keys %depends) {
-#      for ($i = 0;
-#  	 $depends{$base}[$i];
-#  	 $i++) {
-#  	print "$base: $depends{$base}[$i]\n";
-#      }
-#  }
+&read_splitterlist($splitterlist);
 
 open(OUTFILE, ">$outfile") || die "$outfile: Cannot open file: $!\n";
 
-print OUTFILE "package $package;\n\n";
+&generate_target;
 
-print OUTFILE "import daikon.*;\n";
-print OUTFILE "import daikon.split.*;\n\n";
+print OUTFILE <<EOF;
+        }
+}
+EOF
 
-print OUTFILE "public class $listclass {\n\n";
+close(OUTFILE);
+}
 
-print OUTFILE "\tstatic {\n\n";
+sub read_splitterlist {
+    my ($list) = @_;
 
-foreach $base (keys %depends) {
-    @splitterlist = ();
-    %registered = ();
-    foreach $class (keys %depends) {
+    open(LIST, "$list") || die "$list: Cannot open file:$!\n!";
+
+    while (<LIST>) {
+	$class = "";
+	$index = 0;
+	chomp;
+	$splitter = $_;
+	if (/^(\S+)_(\d+)\.java$/) {
+	    $class = $1;
+	    $index = $2;
+	} elsif (/^(\S+)\.java$/) {
+	    $class = $1;
+	    $index = 0;
+	}
+	$class =~ s/Splitter_/\./;
+	$splitters{$class}[$index] = $splitter;
+    }
+
+# The following awkward routine resolves duplication, which should be
+# managed in gensplitter.pl.
+
+    foreach $class (keys %splitters) {
+	for ($i = 1;
+	     $splitters{"$class$i"} != ();
+	     $i++) {
+	    $j = $#{$splitters{$class}};
+	    foreach $splitter (@{$splitters{"$class$i"}}) {
+		$j++;
+		$splitters{$class}[$j] = $splitter;
+	    }
+	    delete $splitters{"$class$i"};
+	}
+    }
+
+    close(LIST);
+}
+
+sub generate_target {
+
+print OUTFILE <<EOF;
+package $package;
+
+import daikon.*;
+import daikon.split.*;
+
+public class $listclass {
+
+        static {
+
+EOF
+
+    foreach $class (keys %splitters) {
 	@splitterlist = ();
 	for ($i = 0;
-	     $splitter = $depends{$class}[$i];
+	     $file = $splitters{$class}[$i];
 	     $i++) {
-	    $file = "$splitter\.java";
 	    open(SPLITTER, "$file") || die "$file: Cannot open file: $!\n";
 	    %registered = ();
 	    while (<SPLITTER>) {
@@ -83,29 +104,22 @@ foreach $base (keys %depends) {
 	    }
 	    close(SPLITTER);
 	}
-	&output_instantiator;
+	&output_instantiator($class, @splitterlist);
     }
 }
 
-print OUTFILE "\t}\n";
-print OUTFILE "}\n";
-
-close(OUTFILE);
-
 sub output_instantiator {
-    if (!$generated{$class}) {
-	if ($splitterlist[0]) {
-	    $generated{$class} = 1;
-	    print OUTFILE "\t\tSplitterList.put(\"$class\", new Splitter[] {\n";
-	    for ($j = 0;
-		 $splitterlist[$j];
-		 $j++) {
-		print OUTFILE "\t\t\tnew $splitterlist[$j]()";
-		if ($splitterlist[$j+1]) {
-		    print OUTFILE ",\n";
-		} else {
-		    print OUTFILE "});\n\n";
-		}
+    my ($class, @list) = @_;
+    if ($list[0]) {
+	print OUTFILE "\t\tSplitterList.put(\"$class\", new Splitter[] {\n";
+	for ($j = 0;
+	     $list[$j];
+	     $j++) {
+	    print OUTFILE "\t\t\tnew $list[$j]()";
+	    if ($list[$j+1]) {
+		print OUTFILE ",\n";
+	    } else {
+		print OUTFILE "});\n\n";
 	    }
 	}
     }
