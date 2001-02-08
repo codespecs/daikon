6ad6dc39c7fdd7afb3bd2189712baa5f8c5be32d
mhao <devnull@localhost>
2001-02-08 15:01:44 -0500

    Added variable filtering.

--- java/daikon/inv/filter/InvariantFilters.java
+++ java/daikon/inv/filter/InvariantFilters.java
@@ -2,10 +2,10 @@ package daikon.inv.filter;
 
 import java.util.*;
 import daikon.inv.*;
+import daikon.VarInfo;
 
 public class InvariantFilters {
-    //  These id numbers are stored in HashMap's and such, so it's
-    //  convenient for them to be String's rather than int's.
+    // ID numbers for property filters
     public static final int UNJUSTIFIED_FILTER = 0;
     public static final int OBVIOUS_FILTER = 1;
     public static final int FEW_MODIFIED_SAMPLES_FILTER = 2;
@@ -13,22 +13,65 @@ public class InvariantFilters {
     public static final int ONLY_CONSTANT_VARIABLES_FILTER = 4;
     public static final int IMPLIED_POSTCONDITION_FILTER = 5;
 
-    List filters = new ArrayList();
+    public static final int ANY_VARIABLE = 10;
+    public static final int ALL_VARIABLES = 11;
+    int variableFilterType = ANY_VARIABLE;
 
+    List propertyFilters = new ArrayList();
+    List variableFilters = new ArrayList();
+    
     public InvariantFilters() {
-	filters.add( InvariantFilters.UNJUSTIFIED_FILTER,             new UnjustifiedFilter());
-	filters.add( InvariantFilters.OBVIOUS_FILTER,                 new ObviousFilter());
-	filters.add( InvariantFilters.FEW_MODIFIED_SAMPLES_FILTER,    new FewModifiedSamplesFilter());
-	filters.add( InvariantFilters.NON_CANONICAL_VARIABLES_FILTER, new NonCanonicalVariablesFilter());
-	filters.add( InvariantFilters.ONLY_CONSTANT_VARIABLES_FILTER, new OnlyConstantVariablesFilter());
-	filters.add( InvariantFilters.IMPLIED_POSTCONDITION_FILTER,   new ImpliedPostconditionFilter());
+	propertyFilters.add( InvariantFilters.UNJUSTIFIED_FILTER,             new UnjustifiedFilter());
+	propertyFilters.add( InvariantFilters.OBVIOUS_FILTER,                 new ObviousFilter());
+	propertyFilters.add( InvariantFilters.FEW_MODIFIED_SAMPLES_FILTER,    new FewModifiedSamplesFilter());
+	propertyFilters.add( InvariantFilters.NON_CANONICAL_VARIABLES_FILTER, new NonCanonicalVariablesFilter());
+	propertyFilters.add( InvariantFilters.ONLY_CONSTANT_VARIABLES_FILTER, new OnlyConstantVariablesFilter());
+	propertyFilters.add( InvariantFilters.IMPLIED_POSTCONDITION_FILTER,   new ImpliedPostconditionFilter());
     }
 
+    public void addVariableFilter( String variable ) {
+	variableFilters.add( new VariableFilter( variable ));
+    }
+
+    public void removeVariableFilter( String variable ) {
+	for (Iterator iter = variableFilters.iterator(); iter.hasNext(); ) {
+	    if (((VariableFilter) iter.next()).getVariable().equals( variable )) {
+		iter.remove();
+		return;
+	    }
+	}
+	throw new Error( "InvariantFilters.removeVariableFilter():  filter for variable '" + variable + "' not found" );
+    }
+
+    public void setVariableFilterType( int variableFilterType ) {
+	this.variableFilterType = variableFilterType;
+    }
+    
     public boolean shouldKeep( Invariant invariant ) {
-	for (Iterator iter = filters.iterator(); iter.hasNext(); ) {
+	//  Do variable filters first since they eliminate more invariants.
+	if (variableFilters.size() != 0) {
+	    if (variableFilterType == InvariantFilters.ANY_VARIABLE) {
+		boolean hasAnyVariable = false;
+		for (Iterator iter = variableFilters.iterator(); iter.hasNext(); ) {
+		    InvariantFilter filter = (InvariantFilter) iter.next();
+		    if (! filter.shouldDiscard( invariant ))
+			hasAnyVariable = true;
+		}
+		if (! hasAnyVariable)
+		    return false;
+	    } else if (variableFilterType == InvariantFilters.ALL_VARIABLES) {
+		for (Iterator iter = variableFilters.iterator(); iter.hasNext(); ) {
+		    InvariantFilter filter = (InvariantFilter) iter.next();
+		    if (filter.shouldDiscard( invariant ))
+			return false;
+		}
+	    }
+	}
+	//  Property filters.
+	for (Iterator iter = propertyFilters.iterator(); iter.hasNext(); ) {
 	    InvariantFilter filter = (InvariantFilter) iter.next();
 	    if (filter.shouldDiscard( invariant )) {
-		System.out.println( filter.getClass().getName() + " rules out    \t" + invariant.format());
+		//		System.out.println( filter.getClass().getName() + " rules out    \t" + invariant.format());
 		return false;
 	    }
 	}
@@ -36,7 +79,7 @@ public class InvariantFilters {
     }
 
     public void changeFilterSetting( int filterID, boolean turnOn ) {
-	InvariantFilter filter = (InvariantFilter) filters.get( filterID );
+	InvariantFilter filter = (InvariantFilter) propertyFilters.get( filterID );
 	if (turnOn)
 	    filter.turnOn();
 	else
@@ -44,14 +87,14 @@ public class InvariantFilters {
     }
 
     public void turnFiltersOn() {
-	for (Iterator iter = filters.iterator(); iter.hasNext(); ) {
+	for (Iterator iter = propertyFilters.iterator(); iter.hasNext(); ) {
 	    InvariantFilter filter = (InvariantFilter) iter.next();
 	    filter.turnOn();
 	}
     }
 
     public void turnFiltersOff() {
-	for (Iterator iter = filters.iterator(); iter.hasNext(); ) {
+	for (Iterator iter = propertyFilters.iterator(); iter.hasNext(); ) {
 	    InvariantFilter filter = (InvariantFilter) iter.next();
 	    filter.turnOff();
 	}
@@ -117,5 +160,26 @@ class OnlyConstantVariablesFilter extends InvariantFilter {
 class ImpliedPostconditionFilter extends InvariantFilter {
     boolean shouldDiscardInvariant( Invariant invariant ) {
 	return invariant.isImpliedPostcondition();
+    }
+}
+
+
+
+class VariableFilter extends InvariantFilter {
+    String variable;
+
+    public VariableFilter( String variable ) {
+	this.variable = variable;
+    }
+
+    public String getVariable() {
+	return variable;
+    }
+
+    boolean shouldDiscardInvariant( Invariant invariant ) {
+	 if (invariant.ppt.usesVar( variable ))
+	    return false;
+	else
+	    return true;
     }
 }
