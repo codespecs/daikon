a4181eb37e875dfd0ee37aed2f9edf907a7210f0
michael.ernst <devnull@localhost>
2001-02-07 21:46:53 -0500

    Slice where Implications are held

---
+++ java/daikon/PptSlice0.java
@@ -0,0 +1,35 @@
+package daikon;
+
+import daikon.inv.*;
+
+import utilMDE.*;
+
+// This is a fake PptSlice for use with Implication invariants.
+
+public class PptSlice0 extends PptSlice {
+
+  PptSlice0(Ppt parent) {
+     super(parent, new VarInfo[0]);
+  }
+
+  public void addInvariant(Invariant inv) {
+    Assert.assert(inv != null);
+    Assert.assert(inv instanceof Implication);
+    invs.add(inv);
+  }
+
+  // I need to figure out how to set these.
+  public int num_samples() { return 2222; }
+  public int num_mod_non_missing_samples() { return 2222; }
+  public int num_values() { return 2222; }
+  public String tuplemod_samples_summary() { return "tuplemod_samples_summary for PptSlice0 " + name; }
+
+  void instantiate_invariants(int pass) {
+    throw new Error("Shouldn't get called");
+  }
+
+  void add(ValueTuple vt, int count) {
+    throw new Error("Shouldn't get called");
+  }
+
+}
