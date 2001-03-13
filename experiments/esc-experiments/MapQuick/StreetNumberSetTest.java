327ecf96fee94892581ccef31396ba05bf83e0bd
michael.ernst <devnull@localhost>
2001-03-12 23:18:34 -0500

    General cleanup; add abbreviations; add some casts to make equals()   invariants work better.

--- experiments/esc-experiments/MapQuick/StreetNumberSetTest.java
+++ experiments/esc-experiments/MapQuick/StreetNumberSetTest.java
@@ -4,11 +4,16 @@ import junit.framework.*;
 
 public class StreetNumberSetTest extends TestCase {
 
-  
+
   public StreetNumberSetTest(String name) { super(name); }
 
+  // convenience function
+  static StreetNumberSet sns(String s) {
+    return new StreetNumberSet(s);
+  }
+
   public void testEasyContains() {
-    StreetNumberSet sns = new StreetNumberSet("3");
+    StreetNumberSet sns = sns("3");
     assert("StreetNumberSet '3' doesn't contain 3.", sns.contains(3));
     assert("StreetNumberSet '3' does contain 2.",   !sns.contains(2));
     assert("StreetNumberSet '3' does contain 4.",   !sns.contains(4));
@@ -17,7 +22,7 @@ public class StreetNumberSetTest extends TestCase {
   }
 
   public void testContainsBoundary() {
-    StreetNumberSet sns = new StreetNumberSet("3-9");
+    StreetNumberSet sns = sns("3-9");
     assert("StreetNumberSet '3-9' doesn't contain 3.", sns.contains(3));
     assert("StreetNumberSet '3-9' doesn't contain 9.", sns.contains(9));
     assert("StreetNumberSet '3-9' does contain 1.",   !sns.contains(1));
@@ -27,7 +32,7 @@ public class StreetNumberSetTest extends TestCase {
   }
 
   public void testContainsInside() {
-    StreetNumberSet sns = new StreetNumberSet("3-9");
+    StreetNumberSet sns = sns("3-9");
     assert("StreetNumberSet '3-9' doesn't contain 5.", sns.contains(5));
     assert("StreetNumberSet '3-9' doesn't contain 7.", sns.contains(7));
     assert("StreetNumberSet '3-9' does contain 4.",   !sns.contains(4));
@@ -36,23 +41,23 @@ public class StreetNumberSetTest extends TestCase {
   }
 
   public void testContainsRigorous() {
-    StreetNumberSet sns = new StreetNumberSet("1-3,7-10001,20221-20223");
+    StreetNumberSet sns = sns("1-3,7-1001,20221-20223");
     for (int i = 1; i<=3; i+=2) {
-      assert("StreetNumberSet '1-3,7-10001,20221-20223' doesn't contain "+i+".",
+      assert("StreetNumberSet '1-3,7-1001,20221-20223' doesn't contain "+i+".",
              sns.contains(i));
     }
-    for (int i = 7; i<=10001; i+=2) {
-      assert("StreetNumberSet '1-3,7-10001,20221-20223' doesn't contain "+i+".",
+    for (int i = 7; i<=1001; i+=2) {
+      assert("StreetNumberSet '1-3,7-1001,20221-20223' doesn't contain "+i+".",
              sns.contains(i));
     }
     for (int i = 20221; i<=20223; i+=2) {
-      assert("StreetNumberSet '1-3,7-10001,20221-20223' doesn't contain "+i+".",
+      assert("StreetNumberSet '1-3,7-1001,20221-20223' doesn't contain "+i+".",
              sns.contains(i));
     }
   }
 
   public void testContainsSingleton() {
-    StreetNumberSet sns = new StreetNumberSet("1-3,5-505,909,2221-2223");
+    StreetNumberSet sns = sns("1-3,5-505,909,2221-2223");
     assert("StreetNumberSet '1-3,5-505,909,2221-2223' doesn't contain 909.",
            sns.contains(909));
     assert("StreetNumberSet '1-3,5-505,909,2221-2223' does contain 908.",
@@ -66,7 +71,7 @@ public class StreetNumberSetTest extends TestCase {
   }
 
   public void testContainsMixedParity() {
-    StreetNumberSet sns = new StreetNumberSet("1-9,6-14");
+    StreetNumberSet sns = sns("1-9,6-14");
     assert("StreetNumberSet '1-9,6-14' doesn't contain 3.",
            sns.contains(3));
     assert("StreetNumberSet '1-9,6-14' does contain 4.",
@@ -90,12 +95,12 @@ public class StreetNumberSetTest extends TestCase {
   }
 
   public void testOrderSimple() {
-    StreetNumberSet sns = new StreetNumberSet("1");
+    StreetNumberSet sns = sns("1");
     assertEquals("Rank of singleton.", 0, sns.orderStatistic(1));
   }
 
   public void testOrderMixedParity() {
-    StreetNumberSet sns = new StreetNumberSet("1-5,6-14");
+    StreetNumberSet sns = sns("1-5,6-14");
     assertEquals("Rank of first element in mixed parity set.",
            0, sns.orderStatistic(1));
     assertEquals("Rank of middle elelemnt in mixed parity set.",
@@ -107,7 +112,7 @@ public class StreetNumberSetTest extends TestCase {
   }
 
   public void testOrderMixedParity2() {
-    StreetNumberSet sns = new StreetNumberSet("1-11,4-14");
+    StreetNumberSet sns = sns("1-11,4-14");
     assertEquals("Rank of first element in mixed parity set.",
            0, sns.orderStatistic(1));
     assertEquals("Rank of middle elelemnt in mixed parity set.",
@@ -119,7 +124,7 @@ public class StreetNumberSetTest extends TestCase {
   }
 
   public void testOrderRigorous() {
-    StreetNumberSet sns = new StreetNumberSet("10-110,81-2001");
+    StreetNumberSet sns = sns("10-110,81-2001");
     int j = 0;
     for (int i=10; i<=80; i+=2) {
       assertEquals("Rank in even portion",
@@ -138,62 +143,44 @@ public class StreetNumberSetTest extends TestCase {
     }
   }
 
+  StreetNumberSet s1_7 = sns("1-7");
+  StreetNumberSet s1_7b = sns("1-7");
+  StreetNumberSet s3_517 = sns("3-5,1,7");
+  StreetNumberSet s3_7 = sns("3-7");
 
   public void testEquals1() {
-    StreetNumberSet s1 = new StreetNumberSet("1-7");
-    StreetNumberSet s2 = new StreetNumberSet("1-7");
-    assertEquals("'1-7'is not equal to '1-7'", s1, s2);
+    assertEquals("'1-7' is not equal to '1-7'", s1_7, s1_7b);
   }
 
   public void testEquals2() {
-    StreetNumberSet s1 = new StreetNumberSet("1-7");
-    StreetNumberSet s2 = new StreetNumberSet("3-5,1,7");
-    assertEquals("'3-5,1,7'is not equal to '1-7'", s1, s2);
+    assertEquals("'3-5,1,7' is not equal to '1-7'", s3_517, s1_7);
+    assertEquals("'1-7' is not equal to '3-5,1,7'", s1_7, s3_517);
   }
 
   public void testEquals3() {
-    StreetNumberSet s1 = new StreetNumberSet("1-7");
-    assert("'1-7'is equal to null", !s1.equals(null));
-    assert("'1-7'is equal to a String", !s1.equals("Foo"));
+    assert("'1-7' is equal to null", !s1_7.equals(null));
+    assert("'1-7' is equal to a String", !s1_7.equals("Foo"));
   }
 
   public void testEquals4() {
-    StreetNumberSet s1 = new StreetNumberSet("1-7");
-    StreetNumberSet s3 = new StreetNumberSet("3-7");
-    assert("'1-7' equals '3-7'", !s1.equals(s3));
-    assert("'3-7' equals '1-7'", !s3.equals(s1));
+    assert("'1-7' equals '3-7'", !s1_7.equals((Object) s3_7));
+    assert("'3-7' equals '1-7'", !s3_7.equals((Object) s1_7));
   }
-  
+
   public void testHashCode() {
-    StreetNumberSet s1 = new StreetNumberSet("1-7");
-    StreetNumberSet s2 = new StreetNumberSet("3-5,1,7");
+    StreetNumberSet s1 = sns("1-7");
+    StreetNumberSet s2 = sns("3-5,1,7");
     assertEquals("hashCode for '1-7' is not the same as hashCode for '3-5,1,7'",
                  s1.hashCode(), s2.hashCode());
   }
 
-  
+
   // Tell JUnit what order to run the tests in
   public static Test suite()
-    { 
-      TestSuite suite = new TestSuite(); 
-      suite.addTest(new StreetNumberSetTest("testEasyContains"));
-      suite.addTest(new StreetNumberSetTest("testContainsBoundary"));
-      suite.addTest(new StreetNumberSetTest("testContainsInside"));
-      suite.addTest(new StreetNumberSetTest("testContainsRigorous"));
-      suite.addTest(new StreetNumberSetTest("testContainsSingleton"));
-      suite.addTest(new StreetNumberSetTest("testContainsMixedParity"));
-      suite.addTest(new StreetNumberSetTest("testOrderSimple"));
-      suite.addTest(new StreetNumberSetTest("testOrderMixedParity"));
-      suite.addTest(new StreetNumberSetTest("testOrderMixedParity2"));
-      suite.addTest(new StreetNumberSetTest("testOrderRigorous"));
-      suite.addTest(new StreetNumberSetTest("testEquals1"));
-      suite.addTest(new StreetNumberSetTest("testEquals2"));
-      suite.addTest(new StreetNumberSetTest("testEquals3"));
-      suite.addTest(new StreetNumberSetTest("testEquals4"));
-      suite.addTest(new StreetNumberSetTest("testHashCode"));
+    {
+      // includes all the tests from this file (in arbitrary order)
+      TestSuite suite = new TestSuite(StreetNumberSetTest.class);
       return suite;
     }
-  
+
 }
-  
-  
