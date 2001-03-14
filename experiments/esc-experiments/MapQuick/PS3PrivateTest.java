113eb6c3f3c73b0a5617b90feffc7fd2bc209d8b
jwnimmer <jwnimmer@alum.mit.edu>
2001-03-13 21:48:15 -0500

    Fix name of class (originally called PrivateTest, now has been renamed). k

--- experiments/esc-experiments/MapQuick/PS3PrivateTest.java
+++ experiments/esc-experiments/MapQuick/PS3PrivateTest.java
@@ -2,14 +2,14 @@ package MapQuick;
 
 import junit.framework.*;
 
-public class PrivateTest
+public class PS3PrivateTest
 {
     static final String TESTDIR_PROP = "test.dir";
     public static Test suite()
     {
 	String testDir = System.getProperty(TESTDIR_PROP);
 	if (testDir == null)
-	    return new TestCase("Loading "+PrivateTest.class.getName()) {
+	    return new TestCase("Loading PS3PrivateTest") {
 		    protected void runTest() throws Throwable
 		    {
 			throw new Error("System property not defined: "
