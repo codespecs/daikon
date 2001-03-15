b760457aba56f5ee4de33051170cc76fb01974ce
jwnimmer <jwnimmer@alum.mit.edu>
2001-03-15 16:15:00 -0500

    More tests

--- experiments/esc-experiments/DataStructures/DisjSetsTest.java
+++ experiments/esc-experiments/DataStructures/DisjSetsTest.java
@@ -1,10 +1,64 @@
 package DataStructures;
 
+import java.util.*;
+
 class DisjSetsTest
 {
   // Test main; all finds on same output line should be identical
   public static void main( String [ ] args )
   {
+    DisjSetsTest t = new DisjSetsTest();
+    t.testFromDsaaMain();
+    t.testRandoms();
+    t.testRemoveFrivolous();
+  }
+
+  public void testRandoms() {
+    int[] sizes = { 0, 1, 5, 100, 10 };
+    for (int i=0; i < sizes.length; i++) {
+      int size = sizes[i];
+      System.out.println("size[" + i + "] = " + size);
+      DisjSets ds = new DisjSets(size);
+      observe(ds, size);
+      for (int j=0; j<size; j++) {
+	ds.unionCareful(rnd(size), rnd(size));
+      }
+      //ds.unionCareful(0, size-1);
+    }
+  }
+
+  public void testRemoveFrivolous() {
+    DisjSets ds;
+
+    // we want 0 to be a lower bound
+    for (int i=0; i<20; i++) {
+      new DisjSets(0);
+    }
+
+    ds = new DisjSets(2000);
+    ds.find(123);
+
+    ds = new DisjSets(500);
+    ds.unionCareful(67, 89);
+  }
+
+  private static Random rnd = new Random(1000);
+  private static int rnd(int lessThan)
+  {
+    int result = Math.abs(rnd.nextInt()) % lessThan;
+    if ((0 <= result) && (result < lessThan))
+      return result;
+    else
+      throw new Error();
+  }
+
+  private static void observe(DisjSets s, int size)
+  {
+    for (int i=0; i<size; i++)
+      s.find(i);
+  }
+
+  public void testFromDsaaMain() {
     int numElements = 128;
     int numInSameSet = 16;
 
@@ -15,9 +69,10 @@ class DisjSetsTest
       {
 	for( int j = 0; j + k < numElements; j += 2 * k )
 	  {
-	    set1 = ds.find( j );
-	    set2 = ds.find( j + k );
-	    ds.union( set1, set2 );
+	    ds.unionCareful( j , j+ k);
+	    // set1 = ds.find( j );
+	    // set2 = ds.find( j + k );
+	    // ds.union( set1, set2 );
 	  }
       }
 
