bcc0b9f46467ca996fb2f1a4ede2f83c22abdcfc
jwnimmer <jwnimmer@alum.mit.edu>
2001-03-15 14:40:03 -0500

    Move main() test into separate file

--- experiments/esc-experiments/DataStructures/DisjSets.java
+++ experiments/esc-experiments/DataStructures/DisjSets.java
@@ -58,32 +58,4 @@ class DisjSets
 
   private int [ ] s;
 
-
-  // Test main; all finds on same output line should be identical
-  public static void main( String [ ] args )
-  {
-    int numElements = 128;
-    int numInSameSet = 16;
-
-    DisjSets ds = new DisjSets( numElements );
-    int set1, set2;
-
-    for( int k = 1; k < numInSameSet; k *= 2 )
-      {
-	for( int j = 0; j + k < numElements; j += 2 * k )
-	  {
-	    set1 = ds.find( j );
-	    set2 = ds.find( j + k );
-	    ds.union( set1, set2 );
-	  }
-      }
-
-    for( int i = 0; i < numElements; i++ )
-      {
-	System.out.print( ds.find( i )+ "*" );
-	if( i % numInSameSet == numInSameSet - 1 )
-	  System.out.println( );
-      }
-    System.out.println( );
-  }
 }
