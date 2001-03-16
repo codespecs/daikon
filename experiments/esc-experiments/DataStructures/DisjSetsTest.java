20a3c9b5b7ea1657e1fc60503ff470d14fc03405
jwnimmer <jwnimmer@alum.mit.edu>
2001-03-15 19:07:00 -0500

    Add some hack tests to remove spurious invariants.

--- experiments/esc-experiments/DataStructures/DisjSetsTest.java
+++ experiments/esc-experiments/DataStructures/DisjSetsTest.java
@@ -11,16 +11,26 @@ class DisjSetsTest
     t.testFromDsaaMain();
     t.testRandoms();
     t.testRemoveFrivolous();
+    t.testSmall();
+  }
+
+  public void testSmall() {
+    DisjSets ds = new DisjSets(1);
+    ds.find(0);
+    ds.unionCareful(0, 0);
+    ds = new DisjSets(2);
+    ds.unionCareful(0, 1);
+    ds.find(1);
   }
 
   public void testRandoms() {
-    int[] sizes = { 0, 1, 5, 100, 10 };
+    int[] sizes = { 0, 1, 5, 50, 10 };
     for (int i=0; i < sizes.length; i++) {
       int size = sizes[i];
       System.out.println("size[" + i + "] = " + size);
       DisjSets ds = new DisjSets(size);
       observe(ds, size);
-      for (int j=0; j<size; j++) {
+      for (int j=0; j<size*3; j++) {
 	ds.unionCareful(rnd(size), rnd(size));
       }
       //ds.unionCareful(0, size-1);
@@ -30,16 +40,43 @@ class DisjSetsTest
   public void testRemoveFrivolous() {
     DisjSets ds;
 
-    // we want 0 to be a lower bound
+    // we want 0 to be a lower bound on the constructor
     for (int i=0; i<20; i++) {
       new DisjSets(0);
     }
 
-    ds = new DisjSets(2000);
-    ds.find(123);
+    // we want elts to be maxed at length-1
+    for (int i=5; i<10; i++) {
+      ds = new DisjSets(i);
+      for (int j=0; j<i-1; j++) {
+	ds.unionCareful(i-1, j);
+	observe(ds, i);
+      }
+    }
+    
+    ds = new DisjSets(200);
+    ds.find(197);
 
-    ds = new DisjSets(500);
-    ds.unionCareful(67, 89);
+    ds = new DisjSets(204);
+    ds.unionCareful(191, 195);
+
+    ds = new DisjSets(210);
+    ds.unionDisjoint(200, 209);
+    ds.find(209);
+    ds.unionDisjoint(200, 0);
+    ds.find(0);  
+
+    // this kills some spurious array endpoint invariants
+    ds = new DisjSets(210);
+    ds.unionCareful(208, 209);
+    ds.unionCareful(207, 208);
+    ds.unionCareful(206, 207);
+    ds.find(208);
+
+    ds = new DisjSets(215);
+    ds.unionCareful(214, 0);
+    ds.unionCareful(213, 214);
+    ds.unionCareful(214, 214);
   }
 
   private static Random rnd = new Random(1000);
