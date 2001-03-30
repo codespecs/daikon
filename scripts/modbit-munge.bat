b9ef654fbbe4d61622ffd4b94f8a4014fd6f3f17
mharder <devnull@localhost>
2001-03-30 16:43:15 -0500

    Added comment explaining uses of file.

--- scripts/modbit-munge.bat
+++ scripts/modbit-munge.bat
@@ -1,2 +1,5 @@
 @echo off
+REM Windows users can run this batch file to execute the modbit-munge.pl
+REM Perl script.  UNIX users do not need to use this file at all.
+
 perl -S -wpi.bak modbit-munge.pl %1
