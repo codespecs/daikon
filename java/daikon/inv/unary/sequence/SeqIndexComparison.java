201cd5fd0e1ea9024aabf15dede89de340b2cc2d
michael.ernst <devnull@localhost>
2001-06-09 23:33:15 -0400

    Expand class documentation.

--- java/daikon/inv/unary/sequence/SeqIndexComparison.java
+++ java/daikon/inv/unary/sequence/SeqIndexComparison.java
@@ -7,6 +7,9 @@ import daikon.inv.*;
 import java.util.*;
 import utilMDE.*;
 
+// This class represnts a comparison between elements of a sequence
+// and the indices of those elements; for instance,
+//   "for all i, a[i] > i".
 public final class SeqIndexComparison extends SingleSequence {
 
   public IntComparisonCore core;
