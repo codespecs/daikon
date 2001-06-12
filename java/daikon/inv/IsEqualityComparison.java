7521a36340fc1076f503b2eb678345530d91a951
mhao <devnull@localhost>
2001-06-12 12:09:24 -0400

    Changed two class names:  changed "EqualityInvariant" to "Equality", and "IsEquality" to "IsEqualityComparison".

---
+++ java/daikon/inv/IsEqualityComparison.java
@@ -0,0 +1,31 @@
+package daikon.inv;
+
+import utilMDE.*;
+
+/** This Filter returns true if its argument is an Invariant which satisfies
+ * the following conditions:
+ * <pre>
+ *  * the Invariant is a Comparison (which reports <, >, =, <=, or >=)
+ *  * the relationship reported by the comparison is = (not <, <=, >, or >=)
+ * </pre>
+ **/
+public final class IsEqualityComparison implements Filter {
+
+  // Don't create new instances, just use this existing one
+  public final static IsEqualityComparison it = new IsEqualityComparison();
+
+  private IsEqualityComparison() { }
+
+  public boolean accept(Object o) {
+    Assert.assert(o instanceof Invariant);
+    if (!(o instanceof Comparison))
+      return false;
+    double chance_prob = ((Comparison) o).eq_probability();
+    return chance_prob < Invariant.probability_limit;
+  }
+
+  // Sadly, this does not work -- it conflicts with the member definition.
+  // public static boolean accept(Object o) {
+  //   return it.accept(o);
+  // }
+}
