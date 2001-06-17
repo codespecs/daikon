267da41f3cda3cfd1bada00125e9acdd36927cab
jwnimmer <jwnimmer@alum.mit.edu>
2001-06-17 04:23:12 -0400

    Comment out function which has moved location.

--- java/daikon/VarNames.java
+++ java/daikon/VarNames.java
@@ -1,6 +1,6 @@
 package daikon;
 
-
+// June 17, 2001: perhaps we want to kill this class entirely?
 public class VarNames {
   // Do I want to intern the results of these functions before returning
   // them?  Possibly...
@@ -9,9 +9,10 @@ public class VarNames {
   private VarNames() {
   }
 
-  static String indexVar(String base, int dim) {
-    return (base + "-index" + dim);
-  }
+// mistere: moved to differnt class
+//    static String indexVar(String base, int dim) {
+//      return (base + "-index" + dim);
+//    }
 
   static String elementVar(String base, int dim) {
     StringBuffer sb = new StringBuffer(base);
