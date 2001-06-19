cfcfa968b04ff5ff2bcb7a33914eccd1c97277d7
mharder <devnull@localhost>
2001-06-19 16:59:20 -0400

    Master test suite containing all other test suites.

---
+++ java/daikon/test/MasterTester.java
@@ -0,0 +1,26 @@
+package daikon.test;
+
+import junit.framework.*;
+import daikon.test.diff.*;
+import daikon.test.inv.*;
+
+
+public class MasterTester extends TestCase {
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(suite());
+  }
+  
+  public MasterTester(String name) {
+    super(name);
+  }
+
+  public static Test suite() {
+    TestSuite result = new TestSuite();
+    result.addTest(new TestSuite(DiffTester.class));
+    result.addTest(new TestSuite(InvariantTester.class));
+    result.addTest(new TestSuite(VarInfoNameTest.class));
+    return result;
+  }
+
+}
