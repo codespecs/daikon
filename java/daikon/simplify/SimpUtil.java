ee03be2e692d41c3c2987e3aaf746e6e2be12c6b
jwnimmer <jwnimmer@alum.mit.edu>
2001-07-07 19:46:19 -0400

    Do not allow ~ to appear.

--- java/daikon/simplify/SimpUtil.java
+++ java/daikon/simplify/SimpUtil.java
@@ -14,9 +14,10 @@ public class SimpUtil
     }
     
     Assert.assert(s != null);
-    Assert.assert(s.indexOf("((") == -1, "'((' may not appear");
-    Assert.assert(s.charAt(0) == '(', "starts with lparen");
-    Assert.assert(s.charAt(s.length()-1) == ')', "ends with rparen");
+    Assert.assert(s.indexOf("((") == -1, "'((' may not appear, '"+s+"'");
+    Assert.assert(s.charAt(0) == '(', "starts with lparen, '"+s+"'");
+    Assert.assert(s.indexOf('~') == -1, "~ may not appear, '"+s+"'");
+    Assert.assert(s.charAt(s.length()-1) == ')', "ends with rparen, '"+s+"'");
 
     int paren = 0;
     char[] cs = s.toCharArray();
