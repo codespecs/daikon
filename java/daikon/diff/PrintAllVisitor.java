274765976da47d2d2a12edecbe394a4926fcef39
mharder <devnull@localhost>
2001-07-10 18:12:24 -0400

    Probabilities are now printed for all invariants.  Simplified how PrintDifferingInvariantsVisitor selects which invariants to print.

--- java/daikon/diff/PrintAllVisitor.java
+++ java/daikon/diff/PrintAllVisitor.java
@@ -1,10 +1,14 @@
 package daikon.diff;
 
 import java.io.*;
+import java.text.*;
 import daikon.inv.Invariant;
 
 public class PrintAllVisitor implements NodeVisitor {
   
+  private static DecimalFormat PROBABILITY_FORMAT =
+    new DecimalFormat("0.####");
+
   private PrintStream ps;
   protected boolean verbose;
 
@@ -34,26 +38,47 @@ public class PrintAllVisitor implements NodeVisitor {
   }
 
   public void visitInvNode(InvNode node) {
+    Invariant inv1 = node.getInv1();
+    Invariant inv2 = node.getInv2();
+
     print("    " + "<");
-    if (node.getInv1() == null) {
+    if (inv1 == null) {
       print((String) null);
     } else {
-      printInvariant(node.getInv1(), node);
+      printInvariant(inv1, node);
     }
     print(", ");
-    if (node.getInv2() == null) {
+    if (inv2 == null) {
       print((String) null);
     } else {
-      printInvariant(node.getInv2(), node);
+      printInvariant(inv2, node);
     }
-    println(">");
+    print(">");
+
+    int type = DetailedStatisticsVisitor.determineType(inv1, inv2);
+    String typeLabel = DetailedStatisticsVisitor.TYPE_LABELS[type];
+    int rel = DetailedStatisticsVisitor.determineRelationship(inv1, inv2);
+
+    print(" (" + typeLabel + "," + rel + ")");
+
+    println();
   }
 
   protected void printInvariant(Invariant inv, InvNode node) {
-    if (verbose)
+    if (verbose) {
       print(inv.repr_prob());
-    else
+    } else {
       print(inv.format());
+      printProbability(inv);      
+    }
+  }
+
+  private void printProbability(Invariant inv) {
+    print(" {");
+    double prob = inv.getProbability();
+    String probString = PROBABILITY_FORMAT.format(prob);
+    print(probString);
+    print("}");
   }
 
   protected void print(String s) {
@@ -62,5 +87,9 @@ public class PrintAllVisitor implements NodeVisitor {
 
   protected void println(String s) {
     ps.println(s);
+  }
+
+  protected void println() {
+    ps.println();
   }
 }
--- java/daikon/diff/PrintDifferingInvariantsVisitor.java
+++ java/daikon/diff/PrintDifferingInvariantsVisitor.java
@@ -2,23 +2,12 @@ package daikon.diff;
 
 import daikon.inv.Invariant;
 import java.io.*;
-import java.text.*;
 
 public class PrintDifferingInvariantsVisitor extends PrintAllVisitor {
 
-  private boolean printUninteresting;
-  private boolean considerJustification;
-
-  private static DecimalFormat PROBABILITY_FORMAT =
-    new DecimalFormat("0.####");
-
   public PrintDifferingInvariantsVisitor(PrintStream ps,
-                                         boolean verbose,
-                                         boolean printUninteresting,
-                                         boolean considerJustification) {
+                                         boolean verbose) {
     super(ps, verbose);
-    this.printUninteresting = printUninteresting;
-    this.considerJustification = considerJustification;
   }
 
   public void visitInvNode(InvNode node) {
@@ -30,69 +19,22 @@ public class PrintDifferingInvariantsVisitor extends PrintAllVisitor {
   }
 
   private boolean shouldPrint(Invariant inv1, Invariant inv2) {
-      return
-        (invariantsDiffer(inv1, inv2) ||
-         (considerJustification && justificationsDiffer(inv1, inv2))) &&
-        (interestingDifference(inv1, inv2) || printUninteresting) &&
-        (differenceWorthPrinting(inv1, inv2) || !considerJustification);
-  }
+    boolean shouldPrint = true;
 
-  private boolean differenceWorthPrinting(Invariant inv1, Invariant inv2) {
-    return isJustified(inv1) || isJustified(inv2);
-  }
-
-  private boolean isJustified(Invariant inv) {
-    return (inv != null && inv.justified());
-  }
-
-  private boolean invariantsDiffer(Invariant inv1, Invariant inv2) {
-    return
-      inv1 == null ||
-      inv2 == null ||
-      !inv1.isSameInvariant(inv2);
-  }
-
-  private boolean justificationsDiffer(Invariant inv1, Invariant inv2) {
-    boolean inv1Justified = inv1.justified();
-    boolean inv2Justified = inv2.justified();
-    return
-      (inv1Justified && !inv2Justified) ||
-      (!inv1Justified && inv2Justified);
-  }
-
-  // Returns true if the difference between the two invariants is
-  // considered "interesting".
-  static boolean interestingDifference(Invariant inv1,
-                                       Invariant inv2) {
-    // Use the non-null invariant
-    Invariant inv = (inv1 != null) ? inv1 : inv2;
-    return inv.isInteresting();
-  }
-
-
-  protected void printInvariant(Invariant inv, InvNode node) {
-    super.printInvariant(inv, node);
-    if (!verbose) {
-      printJustificationMaybe(inv, node);
+    int type = DetailedStatisticsVisitor.determineType(inv1, inv2);
+    if (type == DetailedStatisticsVisitor.TYPE_UNARY_UNINTERESTING) {
+      shouldPrint = false;
     }
-  }
 
-  private void printJustificationMaybe(Invariant inv, InvNode node) {
-    if (considerJustification &&
-        ! invariantsDiffer(node.getInv1(), node.getInv2())) {
-      printJustification(inv);
+    int rel = DetailedStatisticsVisitor.determineRelationship(inv1, inv2);
+    if (rel == DetailedStatisticsVisitor.REL_SAME_JUST1_JUST2 ||
+        rel == DetailedStatisticsVisitor.REL_SAME_UNJUST1_UNJUST2 ||
+        rel == DetailedStatisticsVisitor.REL_DIFF_UNJUST1_UNJUST2 ||
+        rel == DetailedStatisticsVisitor.REL_MISS_UNJUST1 ||
+        rel == DetailedStatisticsVisitor.REL_MISS_UNJUST2) {
+      shouldPrint = false;
     }
-  }
 
-  private void printJustification(Invariant inv) {
-    print(" {");
-    double prob = inv.getProbability();
-    String probString = PROBABILITY_FORMAT.format(prob);
-    // Print a ~ if the probability is close to, but not exactly, zero
-    if (probString.equals("0") && prob != 0) {
-      print("~");
-    }
-    print(probString);
-    print("}");
+    return shouldPrint;
   }
 }
