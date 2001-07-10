e72e4e0110a2f1f986cb8eb2e96d23a994109b56
mharder <devnull@localhost>
2001-07-10 18:11:18 -0400

    Added more relationship categories.

--- java/daikon/diff/DetailedStatisticsVisitor.java
+++ java/daikon/diff/DetailedStatisticsVisitor.java
@@ -1,12 +1,16 @@
 package daikon.diff;
 
 import java.io.*;
+import java.text.*;
 import daikon.inv.*;
 import daikon.inv.unary.*;
 import daikon.inv.unary.scalar.*;
 import utilMDE.*;
 
 public class DetailedStatisticsVisitor implements NodeVisitor {
+
+  private static final int FIELD_WIDTH = 5;
+  private static final int LABEL_WIDTH = 7;
 
   // Types of invariants
   public static final int NUM_TYPES = 5;
@@ -16,28 +20,43 @@ public class DetailedStatisticsVisitor implements NodeVisitor {
   public static final int TYPE_BINARY = 3;
   public static final int TYPE_TERNARY = 4;
 
+  public static final String[] TYPE_LABELS =
+  { "Null", "UInt", "U!Int", "Bin", "Ter" };
+
   // Relationships between invariants
-  public static final int NUM_RELATIONSHIPS = 8;
-  // Both present, same invariant, same justification
-  public static final int REL_IDENTICAL = 0;
-  // Both present, same invariant, justified in file1, not justified in file2
-  public static final int REL_JUST1 = 1;
-  // Both present, same invariant, justified in file2, not justified in file1
-  public static final int REL_JUST2 = 2;
-  // Both present, different invariant
-  public static final int REL_DIFF = 3;
+  public static final int NUM_RELATIONSHIPS = 12;
+  // Both present, same invariant, justification in file1, justified in file2
+  public static final int REL_SAME_JUST1_JUST2 = 0;
+  // Both present, same invariant, justified in file1, unjustified in file2
+  public static final int REL_SAME_JUST1_UNJUST2 = 1;
+  // Both present, same invariant, unjustified in file1, justified in file2
+  public static final int REL_SAME_UNJUST1_JUST2 = 2;
+  // Both present, same invariant, unjustified in file1, unjustified in file2
+  public static final int REL_SAME_UNJUST1_UNJUST2 = 3;
+  // Both present, diff invariant, justification in file1, justified
+  // in file2
+  public static final int REL_DIFF_JUST1_JUST2 = 4;
+  // Both present, different invariant, justified in file1,
+  // unjustified in file2
+  public static final int REL_DIFF_JUST1_UNJUST2 = 5;
+  // Both present, different invariant, unjustified in file1,
+  // justified in file2
+  public static final int REL_DIFF_UNJUST1_JUST2 = 6;
+  // Both present, different invariant, unjustified in file1,
+  // unjustified in file2
+  public static final int REL_DIFF_UNJUST1_UNJUST2 = 7;
   // Present in file1, justified in file1, not present in file2
-  public static final int REL_MISS_1JUST = 4;
-  // Present in file1, not justified in file1, not present in file2
-  public static final int REL_MISS_1UNJUST = 5;
-  // Present in file2, justified in file2, not present in file1
-  public static final int REL_MISS_2JUST = 6;
-  // Present in file2, not justified in file2, not present in file1
-  public static final int REL_MISS_2UNJUST = 7;
+  public static final int REL_MISS_JUST1 = 8;
+  // Present in file1, unjustified in file1, not present in file2
+  public static final int REL_MISS_UNJUST1 = 9;
+  // Not present in file1, present in file2, justified in file2
+  public static final int REL_MISS_JUST2 = 10;
+  // Not present in file1, present in file2, unjustified in file2
+  public static final int REL_MISS_UNJUST2 = 11;
 
   // Table of frequencies, indexed by type of invariant, and
   // relationship between the invariants
-  protected int[][] freq = new int[NUM_TYPES][NUM_RELATIONSHIPS];
+  private int[][] freq = new int[NUM_TYPES][NUM_RELATIONSHIPS];
 
 
   public void visitRootNode(RootNode node) { }
@@ -48,17 +67,27 @@ public class DetailedStatisticsVisitor implements NodeVisitor {
     Invariant inv1 = node.getInv1();
     Invariant inv2 = node.getInv2();
 
+    int type = determineType(inv1, inv2);
+    int relationship = determineRelationship(inv1, inv2);
+
+    freq[type][relationship]++;
+  }
+
+  public static int determineType(Invariant inv1, Invariant inv2) {
+    int type;
+
     // Set inv to a non-null invariant
     Invariant inv = (inv1 != null) ? inv1 : inv2;
 
     int arity = inv.ppt.arity;
-    int type;
     switch (arity) {
     case 0:
       type = TYPE_NULLARY;
       break;
     case 1:
-      if (inv.isInteresting()) {
+      // If either invariant is interesting, the pair is interesting
+      if ((inv1 != null && inv1.isInteresting()) ||
+          (inv2 != null && inv2.isInteresting())) {
         type = TYPE_UNARY_INTERESTING;
       } else {
         type = TYPE_UNARY_UNINTERESTING;
@@ -74,27 +103,43 @@ public class DetailedStatisticsVisitor implements NodeVisitor {
       throw new Error("Invalid arity: " + arity);
     }
 
+    return type;
+  }
+
+  public static int determineRelationship(Invariant inv1, Invariant inv2) {
     int relationship;
 
     if (inv1 == null) {
-      relationship = inv2.justified() ? REL_MISS_2JUST : REL_MISS_2UNJUST;
+      relationship = inv2.justified() ? REL_MISS_JUST2 : REL_MISS_UNJUST2;
     } else if (inv2 == null) {
-      relationship = inv1.justified() ? REL_MISS_1JUST : REL_MISS_1UNJUST;
-    } else if (inv1.isSameInvariant(inv2)) {
+      relationship = inv1.justified() ? REL_MISS_JUST1 : REL_MISS_UNJUST1;
+    } else {
       boolean justified1 = inv1.justified();
       boolean justified2 = inv2.justified();
-      if (justified1 && justified2) {
-        relationship = REL_IDENTICAL;
-      } else if (justified1) {
-        relationship = REL_JUST1;
+      if (inv1.isSameInvariant(inv2)) {
+        if (justified1 && justified2) {
+          relationship = REL_SAME_JUST1_JUST2;
+        } else if (justified1 && !justified2) {
+          relationship = REL_SAME_JUST1_UNJUST2;
+        } else if (!justified1 && justified2) {
+          relationship = REL_SAME_UNJUST1_JUST2;
+        } else {
+          relationship = REL_SAME_UNJUST1_UNJUST2;
+        }
       } else {
-        relationship = REL_JUST2;
+        if (justified1 && justified2) {
+          relationship = REL_DIFF_JUST1_JUST2;
+        } else if (justified1 && !justified2) {
+          relationship = REL_DIFF_JUST1_UNJUST2;
+        } else if (!justified1 && justified2) {
+          relationship = REL_DIFF_UNJUST1_JUST2;
+        } else {
+          relationship = REL_DIFF_UNJUST1_UNJUST2;
+        }
       }
-    } else {
-      relationship = REL_DIFF;
     }
 
-    freq[type][relationship]++;
+    return relationship;
   }
 
   // Returns a tab-separated listing of its data, suitable for storing in a
@@ -119,63 +164,32 @@ public class DetailedStatisticsVisitor implements NodeVisitor {
     PrintWriter pw = new PrintWriter(sw);
     
     pw.println("STATISTICS");
-    pw.println("\t0\t1\t2\t3\t4\t5\t6\t7\tTOTAL");
-
-    pw.print("Null");
-    for (int rel=0; rel < NUM_RELATIONSHIPS; rel++) {
-      pw.print("\t" + freq[TYPE_NULLARY][rel]);
+    pw.print("       ");
+    for (int rel = 0; rel < NUM_RELATIONSHIPS; rel++) {
+      pw.print(UtilMDE.rpad(rel, FIELD_WIDTH));
     }
-    pw.print("\t" + ArraysMDE.sum(freq[TYPE_NULLARY]));
-    pw.println();
+    pw.println(UtilMDE.rpad("TOTAL", FIELD_WIDTH));
 
-    /*
-    pw.print("Unary");
-    for (int rel=0; rel < NUM_RELATIONSHIPS; rel++) {
-      pw.print("\t" + (freq[TYPE_UNARY_INTERESTING][rel] +
-                       freq[TYPE_UNARY_UNINTERESTING][rel]));
+    for (int type = 0; type < NUM_TYPES; type++) {
+      pw.print(UtilMDE.rpad(TYPE_LABELS[type], LABEL_WIDTH));
+      for (int rel = 0; rel < NUM_RELATIONSHIPS; rel++) {
+        int f = freq[type][rel];
+        pw.print(UtilMDE.rpad(f, FIELD_WIDTH));
+      }
+      int s = ArraysMDE.sum(freq[type]); 
+      pw.print(UtilMDE.rpad(s, FIELD_WIDTH));
+      pw.println();
     }
-    pw.print("\t" + (ArraysMDE.sum(freq[TYPE_UNARY_INTERESTING]) +
-                     ArraysMDE.sum(freq[TYPE_UNARY_UNINTERESTING])));
-    pw.println();
-    */
 
-    pw.print("UInt");
-    for (int rel=0; rel < NUM_RELATIONSHIPS; rel++) {
-      pw.print("\t" + freq[TYPE_UNARY_INTERESTING][rel]);
-    }
-    pw.print("\t" + ArraysMDE.sum(freq[TYPE_UNARY_INTERESTING]));
-    pw.println();
-
-    pw.print("U!Int");
-    for (int rel=0; rel < NUM_RELATIONSHIPS; rel++) {
-      pw.print("\t" + freq[TYPE_UNARY_UNINTERESTING][rel]);
-    }
-    pw.print("\t" + ArraysMDE.sum(freq[TYPE_UNARY_UNINTERESTING]));
-    pw.println();
-
-    pw.print("Bin");
-    for (int rel=0; rel < NUM_RELATIONSHIPS; rel++) {
-      pw.print("\t" + freq[TYPE_BINARY][rel]);
-    }
-    pw.print("\t" + ArraysMDE.sum(freq[TYPE_BINARY]));
-    pw.println();
-
-    pw.print("Ter");
-    for (int rel=0; rel < NUM_RELATIONSHIPS; rel++) {
-      pw.print("\t" + freq[TYPE_TERNARY][rel]);
-    }
-    pw.print("\t" + ArraysMDE.sum(freq[TYPE_TERNARY]));
-    pw.println();
-
-    pw.print("TOTAL");
+    pw.print(UtilMDE.rpad("TOTAL", LABEL_WIDTH));
     for (int rel = 0; rel < NUM_RELATIONSHIPS; rel++) {
       int sum = 0;
       for (int type = 0; type < NUM_TYPES; type++) {
         sum += freq[type][rel];
       }
-      pw.print("\t" + sum);
+      pw.print(UtilMDE.rpad(sum, FIELD_WIDTH));
     }
-    pw.print("\t" + ArraysMDE.sum(freq));
+    pw.print(UtilMDE.rpad(ArraysMDE.sum(freq), FIELD_WIDTH));
 
     pw.println();
 
