61c9899c78a625c19fa35440336d6a8dfd2b274e
michael.ernst <devnull@localhost>
2001-07-11 14:11:01 -0400

    Add parsing tests.

--- java/daikon/test/ProglangTypeTest.java
+++ java/daikon/test/ProglangTypeTest.java
@@ -1,7 +1,9 @@
 package daikon.test;
 
 import junit.framework.*;
+import java.util.Comparator;
 import daikon.*;
+import utilMDE.ArraysMDE;
 
 public class ProglangTypeTest extends TestCase {
 
@@ -17,10 +19,55 @@ public class ProglangTypeTest extends TestCase {
 
   public void testIsIntegral() {
     if (Daikon.compare_byte_char_to_integer) {
-      Assert.assertEquals(true, CHAR.isIntegral());
+      assertEquals(true, CHAR.isIntegral());
     } else {
-      Assert.assertEquals(false, CHAR.isIntegral());
+      assertEquals(false, CHAR.isIntegral());
     }
+  }
+
+  static Comparator longarrcomparator = new ArraysMDE.LongArrayComparatorLexical();
+  static Comparator comparrcomparator = new ArraysMDE.ComparableArrayComparatorLexical();
+
+  // Runtime type of first argument is long[]
+  boolean longarrcomp(Object a, long[] b) {
+    return longarrcomparator.compare((long[]) a, b) == 0;
+  }
+
+  // Runtime type of first (and second) argument is Comparable[]
+  boolean comparrcomp(Object a, Object[] b) {
+    Object[] a1 = (Object[]) a;
+    boolean result = comparrcomparator.compare(a1, b) == 0;
+    if (!result)
+      System.out.println("Arrays differ: " + ArraysMDE.toString(a1)
+                          + ", " + ArraysMDE.toString(b));
+    return result;
+  }
+
+  public void test_parse_value() {
+    ProglangType pint = ProglangType.INT;
+    assertEquals(pint.parse_value("1"), new Long(1));
+    assertEquals(pint.parse_value("0"), new Long(0));
+    assertEquals(pint.parse_value("-3"), new Long(-3));
+
+    ProglangType pstring = ProglangType.STRING;
+    assertEquals(pstring.parse_value("\"foo\""), "foo");
+    assertEquals(pstring.parse_value("\"\""), "");
+    assertEquals(pstring.parse_value("\"\"foo\"\""), "\"foo\"");
+    assertEquals(pstring.parse_value("\"foo bar\""), "foo bar");
+
+    ProglangType pinta = ProglangType.INT_ARRAY;
+    assertTrue(longarrcomp(pinta.parse_value("[]"), new long[] {}));
+    assertTrue(longarrcomp(pinta.parse_value("[1]"), new long[] { 1 }));
+    assertTrue(longarrcomp(pinta.parse_value("[-2]"), new long[] { -2 }));
+    assertTrue(longarrcomp(pinta.parse_value("[1 2 3]"), new long[] { 1, 2, 3 }));
+
+    ProglangType pstringa = ProglangType.STRING_ARRAY;
+    assertTrue(comparrcomp(pstringa.parse_value("[]"), new String[] {}));
+    assertTrue(comparrcomp(pstringa.parse_value("[\"foo\"]"), new String[] { "foo" }));
+    assertTrue(comparrcomp(pstringa.parse_value("[\"foo\" \"bar\"]"), new String[] { "foo", "bar" }));
+    assertTrue(comparrcomp(pstringa.parse_value("[\"foo bar\" \"baz\"]"), new String[] { "foo bar", "baz" }));
+    assertTrue(comparrcomp(pstringa.parse_value("[\"f\\\"oo\"]"), new String[] { "f\"oo" }));
+    assertTrue(comparrcomp(pstringa.parse_value("[\"f\\noo\"]"), new String[] { "f\noo" }));
   }
 
 }
