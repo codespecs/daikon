07fa97e1c4c9b01d83434391eae618edd7394016
michael.ernst <devnull@localhost>
2001-08-15 08:59:38 -0400

    Comment changes.

--- scripts/study-escjava.pl
+++ scripts/study-escjava.pl
@@ -4,25 +4,20 @@
 # study-escjava.pl -- Invoke either the true escjava or whodini
 # Michael Ernst <mernst@lcs.mit.edu>
 
+# This script acts like an enhanced version of escjava.  It invokes either
+# the true escjava or whodini.  It invokes whodini if any relevant
+# annotation files (.who.txt-esc) exist; if so, it must pass along those
+# annotation files and possibly also other .java files besides those on the
+# command line.
 
-
-# * If .txt file does NOT exist, just pass through the
-#   argument(s), as given, to escjava
-# 
-# * If .txt file DOES exist, pass the following to whodini:
-#   * the txt file
-#   * any file(s) requested on the script's command line
-#   * if the calling code was requested, ensure that the ADT
-#     is also included in the call to whodini
-# 
-# >See, for example, $inv/papers/esc-paper/study/stackar.who.txt-esc
-# 
-# This IS an example of a txt file for whodini, if that's what you were
-# asking.  Presumably I will configure the studyXX accounts to have
-# ~/.houdini/experiment{1,2}.txt as appropriate, and you would check for them
-# there.  I could setup study00 with that now, if you like.
-
-use File::Basename;
+# In particular:
+#  * If .txt file does NOT exist, just pass through the
+#    argument(s), as given, to escjava
+#  * If .txt file DOES exist, pass the following to whodini:
+#     * the txt file
+#     * any file(s) requested on the script's command line
+#     * if the calling code was requested, ensure that the ADT
+#       is also included in the call to whodini
 
 my $escjava_executable = "/g2/users/mernst/bin/Linux-i686/escjava";
 my $whodini_executable = "whodini.pl";
@@ -30,31 +25,27 @@ my $txtfiledir = "$ENV{HOME}/.houdini";
 my $cwd = `pwd`;
 chomp($cwd);
 
-# my $cwdbase = $cwd;
-# $cwd =~ s:^.*/::;
-
-# This includes both arguments and also ADTs they use.
+# This list includes both arguments and also ADTs they use.
 # We won't use it if no txt-esc file is found.
 my @javafiles = ();
 
+# List of relevant whodini annotation (.who.txt-esc) files.
 my @txtescfiles = ();
 
 my $debug = 0;
 
+# Process arguments
 if ((scalar(@ARGV) > 0) && ($ARGV[0] =~ 'escjava$')) {
   shift(@ARGV);
 }
-
 if ((scalar(@ARGV) > 0) && ($ARGV[0] eq '-d')) {
   shift(@ARGV);
   $debug = 1;
 }
 
+# Create a list of all files listed on the command line, plus any
+# ADTs that they use.
 for my $arg (@ARGV) {
-  if (! -f $arg) {
-    print "escjava: file $arg does not exist\n";
-    exit(1);
-  }
   push @javafiles, $arg;
   # Look for the ADT file.
   # This assumes that the ADT for FooCheck.java is Foo.java (in the same
@@ -66,10 +57,11 @@ for my $arg (@ARGV) {
   }
 }
 
+# Find whodini annotation files for all java files from previous step.
 for my $jfile (@javafiles) {
   my $base = $jfile;
   $base =~ s/\.java$//;
-  print "$jfile => $base\n";
+  # print "$jfile => $base\n";
   my $txtescfile = "$txtfiledir/$base.who.txt-esc";
   if (-f $txtescfile) {
     push @txtescfiles, $txtescfile;
@@ -78,6 +70,7 @@ for my $jfile (@javafiles) {
   }
 }
 
+# Call escjava, or call whodini, or err if too many background files found.
 if (scalar(@txtescfiles) == 0) {
   my $cmd = $escjava_executable . " " . join(' ', @ARGV);
   if ($debug) { print STDERR "Executing: $cmd\n"; }
