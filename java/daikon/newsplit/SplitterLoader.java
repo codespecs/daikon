fac6424613e3a7fe4bc389fdf9969deb85fdcd81
dodoo <devnull@localhost>
2001-08-16 18:43:56 -0400

    This file is used to load the compiled classes. Will later move to the Util directory and rename

---
+++ java/daikon/newsplit/SplitterLoader.java
@@ -0,0 +1,52 @@
+package daikon.newsplit;
+
+import java.io.*;
+import java.util.*;
+import java.lang.reflect.*;
+
+public class SplitterLoader extends ClassLoader{
+  
+  
+  private byte[] read_Class_Data(String fileName) {
+    
+    try {
+      FileInputStream fi = new FileInputStream(fileName);
+      byte[] classBytes= new byte[fi.available()];
+      fi.read(classBytes);
+      return classBytes;
+    }catch(FileNotFoundException e){
+      System.out.println("Error reading splitter class data: " + fileName + " not found");
+    }catch(IOException ioe){
+      System.out.println("IO Error while reading class data " + fileName);
+    }
+    return null;
+  }
+  
+  protected Class load_Class(String className, String full_pathname) {
+    Class return_class;  
+    try {
+      return_class = super.findSystemClass(className);
+    }catch(ClassNotFoundException cnf){ 
+      byte[] classData = read_Class_Data(full_pathname);
+      if (classData == null){
+	return null;
+      }else{
+	className = "daikon.split." + className;
+	return_class = defineClass(className, classData, 0, classData.length);
+	resolveClass(return_class);
+      }  
+    } 
+    return return_class;
+  }
+  
+}
+
+
+
+
+
+
+
+
+
+
