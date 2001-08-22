1bc9bb39d7885b44efac362623fae1debf684626
mhao <devnull@localhost>
2001-08-22 11:25:37 -0400

    Fixed the way I handle obvious equality invariants.  Before, I had modified InvariantFilters.addEqualityInvariants() to not create them.  This is wrong because these invariants should exist but simply be hidden.  So I undid the change to addEqualityInvariants() and added an ObviousEqualityFilter.

---
+++ java/daikon/inv/filter/ObviousEqualityFilter.java
@@ -0,0 +1,26 @@
+package daikon.inv.filter;
+
+import daikon.VarInfo;
+import daikon.inv.*;
+import daikon.inv.filter.*;
+
+class ObviousEqualityFilter extends InvariantFilter {
+  public String getDescription() {
+    return "Suppress obvious equality invariants";
+  }
+
+  boolean shouldDiscardInvariant( Invariant invariant ) {
+    if (IsEqualityComparison.it.accept( invariant )) {
+	VarInfo[] variables = invariant.ppt.var_infos; // note: only 2 variables
+	for (int i = 0; i < variables.length; i++)
+	  if (variables[i].isCanonical())
+	    // Test if equality is "nonobvious".  This test rarely fails, but is
+	    // necessary for correctness.
+	    if (variables[i].equalToNonobvious().contains( variables[1-i] ))
+	      return false;
+	    else
+	      return true;
+    }
+    return false;
+  }
+}
