1395e1d032a68e78640371a3658289350e62a661
michael.ernst <devnull@localhost>
2001-08-24 08:25:49 -0400

    Remove extraneous trailing blank lines.

--- java/daikon/newsplit/SplitterLoader.java
+++ java/daikon/newsplit/SplitterLoader.java
@@ -5,10 +5,10 @@ import java.util.*;
 import java.lang.reflect.*;
 
 public class SplitterLoader extends ClassLoader{
-  
-  
+
+
   private byte[] read_Class_Data(String fileName) {
-    
+
     try {
       FileInputStream fi = new FileInputStream(fileName);
       byte[] classBytes= new byte[fi.available()];
@@ -21,13 +21,13 @@ public class SplitterLoader extends ClassLoader{
     }
     return null;
   }
-  
+
   protected Class load_Class(String className, String full_pathname) {
-    Class return_class;  
+    Class return_class;
     try {
       //find the class if it's a system class
       return_class = super.findSystemClass(className);
-    }catch(ClassNotFoundException cnf){ 
+    }catch(ClassNotFoundException cnf){
       byte[] classData = read_Class_Data(full_pathname);
       if (classData == null){
 	return null;
@@ -35,19 +35,9 @@ public class SplitterLoader extends ClassLoader{
 	className = "daikon.split." + className;
 	return_class = defineClass(className, classData, 0, classData.length);
 	resolveClass(return_class);
-      }  
-    } 
+      }
+    }
     return return_class;
   }
-  
+
 }
-
-
-
-
-
-
-
-
-
-
