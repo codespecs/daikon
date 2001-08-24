fcc7c7863e25cc52a0721a39f6e47fca7247415b
dodoo <devnull@localhost>
2001-08-24 17:10:56 -0400

    worked on replacement of function bodies for functions whihc take arguments

--- scripts/createspinfo.pl
+++ scripts/createspinfo.pl
@@ -254,7 +254,7 @@ foreach $filename (@ARGV){
     }
     
     foreach $function (keys %HashOfConds){
-	print SPINFOFILE "PPT_NAME, $function";
+	print SPINFOFILE "PPT_NAME# $function";
 	$conditions = "\nCONDITIONS";
 	$replace = "REPLACE";
 	@conds = @{$HashOfConds{$function}};
@@ -264,13 +264,13 @@ foreach $filename (@ARGV){
 	    if($cond !~ /^\s*true\s*$/){
 		$cond =~ s/^\s*(.*)\s*$/$1/; #strip whitespace from ends 
 		$cond =~ s/^\s*(\S*\s*)\!=(\s*\S*)\s*$/$1==$2/;
-		$conditions = $conditions.", $cond";
+		$conditions = $conditions." # $cond";
 	    }else{ next; }
 	}
 	
 	print SPINFOFILE $conditions."\n";
 	print SPINFOFILE "\n";
-	#unlink <$filename.conds>;
+	unlink <$filename.conds>;
     }    
     
     print "output written to $filename.spinfo \n";
