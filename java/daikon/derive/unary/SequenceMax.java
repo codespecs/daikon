dafd697c4bf13f32f0b7bbbc2eedc19a9c045b8a
jwnimmer <jwnimmer@alum.mit.edu>
2001-10-13 17:18:12 -0400

    Add an dkconfig_enable flag to all invariants. Also add other config parameters to others.

--- java/daikon/derive/unary/SequenceMax.java
+++ java/daikon/derive/unary/SequenceMax.java
@@ -6,6 +6,10 @@ import utilMDE.*;
 // like SequenceMin; if one changes, change the other, too
 public final class SequenceMax extends UnaryDerivation {
 
+  // Variables starting with dkconfig_ should only be set via the
+  // daikon.config.Configuration interface
+  public static boolean dkconfig_enabled = true;
+
   public SequenceMax(VarInfo vi) {
     super(vi);
   }
--- java/daikon/derive/unary/SequenceMin.java
+++ java/daikon/derive/unary/SequenceMin.java
@@ -6,6 +6,10 @@ import utilMDE.*;
 // like SequenceMax; if one changes, change the other, too
 public final class SequenceMin extends UnaryDerivation {
 
+  // Variables starting with dkconfig_ should only be set via the
+  // daikon.config.Configuration interface
+  public static boolean dkconfig_enabled = true;
+
   public SequenceMin(VarInfo vi) {
     super(vi);
   }
