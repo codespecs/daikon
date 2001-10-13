ba1d6873cfbfa353aa78f7ed0f57821b29c25d46
jwnimmer <jwnimmer@alum.mit.edu>
2001-10-13 17:18:12 -0400

    Add an dkconfig_enable flag to all invariants. Also add other config parameters to others.

--- java/daikon/derive/binary/ScalarSequencesIntersectionFactory.java
+++ java/daikon/derive/binary/ScalarSequencesIntersectionFactory.java
@@ -19,6 +19,10 @@ import java.util.*;
 public final class ScalarSequencesIntersectionFactory  extends BinaryDerivationFactory {
 
   public BinaryDerivation[] instantiate(VarInfo seq1, VarInfo seq2) {
+    if (! ScalarSequencesIntersection .dkconfig_enabled) {
+      return null;
+    }
+
     if ((seq1.rep_type != ProglangType.INT_ARRAY )
         || (seq2.rep_type != ProglangType.INT_ARRAY )) {
       return null;
