5da1709d0881cdb04feba678167e0504fed25685
jwnimmer <jwnimmer@alum.mit.edu>
2001-12-27 02:17:10 -0500

    Comment out line since we don't have controlling_ppts anymore:     // this.controlling_ppts.add(parent); // [INCR]

--- java/daikon/PptConditional.java
+++ java/daikon/PptConditional.java
@@ -26,7 +26,7 @@ public final class PptConditional extends PptTopLevel {
     this.parent = parent;
     this.splitter = splitter.instantiate(this);
     this.splitter_inverse = splitter_inverse;
-    this.controlling_ppts.add(parent);
+    // this.controlling_ppts.add(parent); // [INCR]
   }
 
   // This is tested after constructing a PptConditional but before
