1bcd3fb1e38974ecd2d61b41fa323c4d8c23da4c
dodoo <devnull@localhost>
2002-01-18 17:18:07 -0500

    Perl script for adding clustering information to a dtrace file. Adds the variable "cluster" to each program point

---
+++ scripts/decls-add-cluster.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+$decls_file = $ARGV[0];
+$decls_file =~ /.*\/(\S*)\.decls/;
+$decls_new = "$1_new.decls";
+
+open (IN, $decls_file) || die "couldn't open $decls_file\n";
+open (OUT, ">$decls_new") || die "couldn't open $outfile for output\n";
+
+while (<IN>) {
+    $line = $_;
+    print OUT $line;
+    if($line =~ /:::/) {
+	print OUT "cluster\n";
+	print OUT "int\n";
+	print OUT "int\n";
+	print OUT "22\n";
+    }
+}
+
+close IN;
+close OUT;
+
