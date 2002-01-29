c46a5fb3e6db9a16b2d764dbebfc8037faea1510
michael.ernst <devnull@localhost>
2002-01-29 04:51:29 -0500

    Restore declsdir flag.

--- java/ajax-ship/ajax/tools/benchmarks/ComparablePairsDescFile.java
+++ java/ajax-ship/ajax/tools/benchmarks/ComparablePairsDescFile.java
@@ -19,15 +19,22 @@ class ComparablePairsDescFile {
     private Hashtable detectors = new Hashtable();
     private ComparablePairsDescFileReader owner;
 
+    // directory containing desc files; removed before constructing package
+    private String descDir;
+
     /**
        Create an analysis tool that rewrites one Daikon .decls file.
 
        @param owner the container that manages the actual Ajax analysis
        @param fileName the name of the .decls file
+       @param descDir the base directory for decls files, to which package names are appended to find the
+          actual directory containing the decls file for each class. If 'null', then the current directory
+	  is assumed to be the base directory for the decls files.
     */
-    ComparablePairsDescFile(ComparablePairsDescFileReader owner, String fileName) {
+    ComparablePairsDescFile(ComparablePairsDescFileReader owner, String fileName, String descDir) {
 	this.fileName = fileName;
 	this.owner = owner;
+	this.descDir = descDir;
     }
 
     public String getFileName() {
@@ -47,13 +54,13 @@ class ComparablePairsDescFile {
                 return false;
         }
     }
-    
+
     private static void findReachableInstructions(byte[] code, int offset, int[] singleton, boolean[] visited) {
         while (!visited[offset]) {
             visited[offset] = true;
-            
+
             int[] successors = JBCCodeUtilities.getReachableSuccessors(code, offset, singleton);
-                                    
+
             if (successors.length == 1) {
                 offset = successors[0];
                 continue;
@@ -64,14 +71,14 @@ class ComparablePairsDescFile {
             }
         }
     }
-    
+
     private static int findUniqueReturn(JBCMethod method, int offset) {
         byte[] code = method.getData().getCode();
         boolean[] visited = new boolean[code.length];
         int result = -1;
-                            
+
         findReachableInstructions(code, offset, new int[1], visited);
-        
+
         for (int i = 0; i < visited.length; i++) {
             if (visited[i] && isReturn(code[i] & 0xFF)) {
                 if (result >= 0) {
@@ -81,7 +88,7 @@ class ComparablePairsDescFile {
                 }
             }
         }
-        
+
         return result;
     }
 
@@ -113,7 +120,7 @@ class ComparablePairsDescFile {
 
             return null;
     }
-  }    
+  }
 
   public static JBCType getExpressionType(JBCMethod method, int offset, JBCExpression expression) {
     Enumeration e = getExpressionTypes(method, offset, expression);
@@ -134,6 +141,36 @@ class ComparablePairsDescFile {
 	System.exit(3);
     }
 
+    /** Make the file relative to the directory. */
+    private static String stripDir(String fileName, String dir) {
+        if (dir == null) {
+            return fileName;
+	} else if (fileName.startsWith(dir)) {
+            fileName = fileName.substring(dir.length());
+            if (fileName.startsWith(File.separator)) {
+                fileName = fileName.substring(File.separator.length());
+            }
+        }
+        return fileName;
+    }
+
+    private String extractPackageName(String fileName) {
+	fileName = stripDir(fileName, descDir);
+
+	while (fileName.startsWith("." + File.separatorChar)) {
+	    fileName = fileName.substring(2);
+	}
+
+	int dirPathEnd = fileName.lastIndexOf(File.separatorChar);
+
+	if (dirPathEnd >= 0) {
+	    return fileName.substring(0, dirPathEnd).
+		replace(File.separatorChar, '.');
+	} else {
+	    return null;
+	}
+    }
+
     private static String getArrayElementName(JBCType t) {
 	if (t instanceof JBCObjectType) {
 	    return "arrayelement";
@@ -150,6 +187,7 @@ class ComparablePairsDescFile {
 
     public void configure(Analyzer analyzer) {
         int lineNum = 0;
+	String packageName = extractPackageName(fileName);
 	BufferedReader r = null;
 	JBCClass javaLangObject = analyzer.getWorld().getSystemClassLoader()
 	    .getClass("java.lang.Object");
@@ -160,20 +198,24 @@ class ComparablePairsDescFile {
             r = new BufferedReader(new FileReader(fileName));
 
             String s;
-            
+
             while ((s = r.readLine()) != null) {
                 lineNum++;
-                
+
                 if (s.equals("DECLARE")) {
                     String decl = r.readLine();
                     int lastColon = decl.lastIndexOf(':');
                     String offset = decl.substring(lastColon + 1);
                     String methodName = decl.substring(0, lastColon - 2);
-                    
+
                     lineNum++;
-                    
+
+                    // if (packageName != null) {
+                    //     methodName = packageName + "." + methodName;
+                    // }
+
                     int offsetIndex;
-                    
+
                     if (offset.startsWith("EXIT")) {
                         try {
                             offsetIndex = Integer.parseInt(offset.substring(4));
@@ -190,20 +232,20 @@ class ComparablePairsDescFile {
                         badDescFile("Unknown offset: " + offset);
                         return;
                     }
-                    
+
                     int lastDot = methodName.lastIndexOf('.');
                     String className = methodName.substring(0, lastDot);
-                    
+
                     JBCClass c = owner.getAppLoader().getClass(className);
-                    
+
                     if (c == null) {
                         badDescFile("Cannot find class " + className + " to resolve method " + methodName);
                         return;
                     }
-                    
+
                     methodName = methodName.substring(lastDot + 1);
                     methodName = methodName.replace('/', '.');
-                    
+
                     int firstParen = methodName.indexOf('(');
                     JBCMethod method;
 
@@ -213,9 +255,9 @@ class ComparablePairsDescFile {
 		      methodName = methodName.substring(0, firstParen);
 
 		      method = c.getMethod(methodName, methodSignature);
-                    
+
 		      if (method == null) {
-                        badDescFile("Method not found: " + methodName 
+                        badDescFile("Method not found: " + methodName
 					   + " (signature \"" + methodSignature
                                            + "\") in class " + className);
                         return;
@@ -231,27 +273,27 @@ class ComparablePairsDescFile {
 			}
 		      }
 		    }
-                    
+
 		    if (offsetIndex > 0) {
                         int sourceLine = offsetIndex;
-                        
+
                         offsetIndex = JBCCodeUtilities.getBytecodeOffset(method, sourceLine);
-                        
+
                         if (offsetIndex < 0) {
                             badDescFile("Line number " + sourceLine + " not found in " + method);
                             return;
                         } else {
                             int returnOffset = findUniqueReturn(method, offsetIndex);
-                            
+
                             if (returnOffset < 0) {
                                 badDescFile("Cannot find unique return instruction from offset " + offsetIndex + " in " + method);
                                 return;
                             }
-                            
+
                             offsetIndex = returnOffset;
                         }
                     }
-                    
+
                     CompactSet exprs = new CompactSet();
                     String[] varNames =
                         JBCCodeUtilities.getLocalVariableNames(method, offsetIndex);
@@ -272,7 +314,7 @@ class ComparablePairsDescFile {
 
                     JBCLocation location = new JBCLocation(method, offsetIndex);
                     Hashtable exprNames = new Hashtable();
-                    
+
                     while ((s = r.readLine()) != null && !s.equals("")) {
                         String name = s;
                         String declType = r.readLine();
@@ -292,61 +334,65 @@ class ComparablePairsDescFile {
 			      nextPart = nextBracket;
 			    }
                             String baseVar = nextPart < 0 ? name : name.substring(0, nextPart);
-                            
+
                             name = nextPart < 0 ? null : name.substring(nextPart + 1);
-                            
+
                             JBCExpression expr;
-                            
+
                             if (baseVar.equals("this")) {
                                 expr = JBCExpression.makeLocalVarExpression(0);
                             } else if (baseVar.equals("return")) {
                                 expr = JBCExpression.makeStackElemExpression(0);
                             } else {
                                 expr = null;
-                                
+
                                 for (int i = 0; i < varNames.length && expr == null; i++) {
                                     if (varNames[i].equals(baseVar)) {
                                         expr = JBCExpression.makeLocalVarExpression(i);
                                     }
                                 }
-                                
+
                                 while (expr == null && name != null) {
                                     int nextBaseDot = name.indexOf('.');
 				    baseVar = baseVar + "." + (nextBaseDot < 0 ? name : name.substring(0, nextBaseDot));
 
                                     name = nextBaseDot < 0 ? null : name.substring(nextBaseDot + 1);
-                                    
+
                                     int lastBaseDot = baseVar.lastIndexOf('.');
 
 				    if (lastBaseDot > -1) {
 				      String staticClassName = baseVar.substring(0, lastBaseDot);
 
+                                      // if (packageName != null) {
+                                      //     staticClassName = packageName + "." + staticClassName;
+                                      // }
+
 				      JBCClass staticClass = owner.getAppLoader().getClass(staticClassName);
-                                    
+
 				      if (staticClass != null) {
                                         String fieldName = baseVar.substring(lastBaseDot + 1);
                                         JBCField f = staticClass.getField(fieldName);
-                                        
+
                                         if (f != null && f.isStatic()) {
                                             expr = JBCExpression.makeStaticFieldExpression(f);
                                         }
 				      }
                                     }
                                 }
-                                    
+
                                 if (expr == null) {
                                     badDescFile("Variable not found: " + baseVar + " at line " + lineNum);
                                     return;
                                 }
                             }
-                            
+
                             if (name != null) {
 			        JBCType varType = getExpressionType(method, offsetIndex, expr);
 				if (varType == null) {
 				  badDescFile("Ambiguous type for qualified name: " + name);
 				  return;
 				}
-                    
+
                                 while (expr != null && name != null) {
     				    nextDot = name.indexOf('.');
 				    nextBracket = name.indexOf('[');
@@ -376,13 +422,13 @@ class ComparablePairsDescFile {
 					    return;
 					}
 				    }
-                            
+
                                     name = nextPart < 0 ? null : name.substring(nextPart + 1);
-                                    
+
 				    if (containingClass == null) {
 				    } else if (fieldName.equals("]")) {
 				      JBCType t = objType.getArrayElementType();
-				     
+
 				      if (t == null) {
 					  if (containingClass.getClassName().equals("java.util.Vector")) {
 					      JBCField f = containingClass.getField("elementData");
@@ -420,7 +466,7 @@ class ComparablePairsDescFile {
                                         badDescFile("Field not found: " + fieldName + " in " + containingClass);
                                         return;
 				      }
-                                    
+
 				      if (f.isStatic()) {
                                         expr = JBCExpression.makeStaticFieldExpression(f);
 				      } else {
@@ -430,7 +476,7 @@ class ComparablePairsDescFile {
 				    }
                                 }
                             }
-                            
+
 			    if (expr != null) {
 				exprs.add(expr);
 				exprNames.put(expr, originalName);
@@ -439,23 +485,23 @@ class ComparablePairsDescFile {
 				    .getFieldName().endsWith("arrayelement")) {
 				    JBCExpression indexExpr = ((JBCUserFieldExpression)expr).getBase()
 					.makeUserFieldExpression(javaLangObject.registerUserField("arraylength", false));
-				    
+
 				    exprs.add(indexExpr);
 				    exprNames.put(indexExpr, originalName + "#");
 				}
 			    }
                         }
                     }
-                    
+
                     lineNum++;
-                    
+
                     ComparablePairsDetector detector =
                         new ComparablePairsDetector(location, exprs, exprNames, decl);
                     DatumSpecifier[] specifiers = { detector };
                     ResultListener[] listeners = { detector };
-                    
+
                     (new UnboundedSetTracker(analyzer, specifiers, listeners)).start();
-                    
+
                     detectors.put(decl, detector);
                 }
             }
@@ -512,7 +558,7 @@ class ComparablePairsDescFile {
 	  }
 
 	  lineNum++;
-          w.write(s + "\n");                
+          w.write(s + "\n");
 
 	  if (s.equals("DECLARE")) {
 	    String decl = r.readLine();
@@ -527,7 +573,7 @@ class ComparablePairsDescFile {
 		String declType = r.readLine();
 		String repType = r.readLine();
 		String compatibility = r.readLine();
-                            
+
                 w.write(name + "\n" + declType + "\n" + repType + "\n"
                     + compatibility + "\n");
 		lineNum += 4;
@@ -576,9 +622,9 @@ class ComparablePairsDescFile {
 		String declType = r.readLine();
 		String repType = r.readLine();
 		String compatibility = r.readLine();
-                        
+
 		lineNum += 4;
-                            
+
 		JBCExpression expr = (JBCExpression)namesToExprs.get(name);
 
                 if (expr == null) {
@@ -621,26 +667,26 @@ class ComparablePairsDescFile {
     public void printBasicReport(Writer w) throws IOException {
         for (Enumeration e = detectors.elements(); e.hasMoreElements();) {
             ComparablePairsDetector detector = (ComparablePairsDetector)e.nextElement();
-            
+
             w.write(detector.getDetectorName() + "\n");
-            
+
             Hashtable exprNames = detector.getExprNames();
             Hashtable results = detector.getResults();
-            
+
             if (results.size() == 0) {
                 JBCLocation loc = detector.getLocation();
-                
+
                 w.write("// Dead code at offset " + loc.getOffset() + " in " + loc.getMethod() + "\n");
             } else {
                 for (Enumeration e2 = exprNames.keys(); e2.hasMoreElements();) {
                     JBCExpression expr = (JBCExpression)e2.nextElement();
-                    
+
                     w.write((String)exprNames.get(expr) + "\n");
-                    
+
                     StringBuffer buf = new StringBuffer();
-                    
+
                     Object o = results.get(expr);
-                    
+
                     if (o != null) {
                         for (Enumeration e3 = UnboundedSetTracker.enumerateIntermediate(o); e3.hasMoreElements();) {
                             if (buf.length() > 0) {
@@ -649,11 +695,11 @@ class ComparablePairsDescFile {
                             buf.append(exprNames.get(e3.nextElement()));
                         }
                     }
-                    
+
                     w.write(buf.toString() + "\n");
                 }
             }
-                        
+
             w.write("\n");
         }
     }
--- java/ajax-ship/ajax/tools/benchmarks/ComparablePairsDescFileReader.java
+++ java/ajax-ship/ajax/tools/benchmarks/ComparablePairsDescFileReader.java
@@ -18,14 +18,14 @@ public class ComparablePairsDescFileReader extends Benchmark {
     private boolean rewriteDescFile;
     private JBCClassLoader appLoader;
     private Vector descFileReaders = new Vector();
-    
+
     public JBCClassLoader getAppLoader() {
 	return appLoader;
     }
 
     protected ComparablePairsDescFileReader() {
     }
-    
+
     public void configure(Analyzer analyzer) {
         analyzer.setSemantics(new CombiningSemantics());
 
@@ -55,27 +55,25 @@ public class ComparablePairsDescFileReader extends Benchmark {
 	   Otherwise we produce a raw matrix giving compatibility data. */
         rewriteDescFile = args.extractBoolOption("-rewrite");
 
-        // IGNORING declsdir now
+	/* Specifies the base directory for the decls files. The package that a decls file belongs to is computed
+	   by stripping this base directory off the decls file name. */
         String declsDir = args.extractStringOption("-declsdir", null);
-        if (declsDir != null) {
-            Globals.localError("Declsdir ignored: " + declsDir);
-	}
 
 	/* One or more decls files come here */
 	String fileName = args.extractNextArg("<descfile>");
-	
-	descFileReaders.addElement(new ComparablePairsDescFile(this, fileName));
+
+	descFileReaders.addElement(new ComparablePairsDescFile(this, fileName, declsDir));
 
 	while (true) {
 	    fileName = args.extractNextOptionalArg();
 	    if (fileName == null) {
 		break;
 	    } else {
-		descFileReaders.addElement(new ComparablePairsDescFile(this, fileName));
+		descFileReaders.addElement(new ComparablePairsDescFile(this, fileName, declsDir));
 	    }
 	}
     }
-    
+
     public static void main(String[] args) {
         GeneralBenchmark.run(args, new ComparablePairsDescFileReader(), "[-rewrite] <descfile> [descfiles...]");
     }
