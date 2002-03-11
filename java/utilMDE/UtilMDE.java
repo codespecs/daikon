3849503abde115188f485f51d524de437a8e7c60
michael.ernst <devnull@localhost>
2002-03-11 12:39:23 -0500

    Tweak comments.

--- java/utilMDE/UtilMDE.java
+++ java/utilMDE/UtilMDE.java
@@ -457,21 +457,25 @@ public final class UtilMDE {
 
 
   /**
-   * Like randomElements(Iterator, int, Random), but supplies a default for
-   * the last argument.
+   * Return an ArrayList containing num_elts randomly chosen
+   * elements from the iterator, or all the elements of the iterator if
+   * there are fewer.  It examines every element of the iterator, but does
+   * not keep them all in memory.
    **/
   public static ArrayList randomElements(Iterator itor, int num_elts) {
     return randomElements(itor, num_elts, r);
   }
   private static Random r = new Random();
 
-  // This method returns an ArrayList containing num_elts randomly chosen
-  // elements from the iterator, or all the elements of the iterator if
-  // there are fewer.  It examines every element of the iterator, but does
-  // not keep them all in memory.
-  // The elements are chosen with the following probabilities, where n == num_elts:
-  //  n n/2 n/3 n/4 n/5
+  /**
+   * Return an ArrayList containing num_elts randomly chosen
+   * elements from the iterator, or all the elements of the iterator if
+   * there are fewer.  It examines every element of the iterator, but does
+   * not keep them all in memory.
+   **/
   public static ArrayList randomElements(Iterator itor, int num_elts, Random random) {
+    // The elements are chosen with the following probabilities, where n == num_elts:
+    //   n n/2 n/3 n/4 n/5
     ArrayList result = new ArrayList(num_elts);
     int i=1;
     for (int n=0; n<num_elts && itor.hasNext(); n++, i++) {
