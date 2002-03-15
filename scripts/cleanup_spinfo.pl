96d15353c922f6bdffade2c6dfc11e1f541e4902
dodoo <devnull@localhost>
2002-03-15 00:30:46 -0500

    perl script to clean up a spinfo file. removes all conditions which involve comparisons against numbers, unless the number is -2, -1, 0, 1 or 2

---
+++ scripts/cleanup_spinfo.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+# remove uninteresting splitting conditions from a file of splitting conditions
+# Reads from standard input and prints to standard out.
+# Removes all conditions that compare variables against numbers, unless the numbers
+# are -2, -1, 0, 1, 2
+
+use English;
+$WARNING = 0;          # -w flag
+#use strict;
+
+my %all_conditions = ();
+
+nextline:
+    while (<>) {
+	if ( /(utilMDE|warning|\.class)/ ) {
+	    next;
+	} elsif ( /for\s*\(int/ ) {
+	    next;
+	} elsif (/\[.*?\.\.\.*?\]/) {
+	    next;
+	} else {
+	    # we don't want splitting conditions comparing a variable against a 
+	    # number, except if the number is -2, -1 , 0, 1, 2, 
+	    $afterString = $_;
+	    while ($afterString =~ /^.*?(\d+)/) {
+		$afterString = $';
+		$number = $1;
+		if ($number !~ /^\s*-?(0|1|2)\s*$/) {  
+		    next nextline;
+		}
+	    }
+	    $hashkey = $_;
+	    $hashkey =~ s/\s*//;
+	    
+	    if ( ! exists $all_conditions{$hashkey} ) {
+		$all_conditions{$hashkey} = "1";
+		if (/PPT_NAME/) { 
+		    print "\n";
+		}
+		print $_;
+	    }
+	}
+    }
