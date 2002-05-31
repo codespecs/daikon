d2c345dd5547808fb6a039147a9121266abad2dc
dodoo <devnull@localhost>
2002-05-31 14:48:48 -0400

    modified to take in more than one decls file

--- scripts/decls-add-cluster.pl
+++ scripts/decls-add-cluster.pl
@@ -1,33 +1,28 @@
 #!/usr/bin/env perl
+## Adds the variable "cluster" to a decls file.
+## usage: decls-add-cluster.pl @decls_files
 
 use English;
 use strict;
 $WARNING = 1;			# "-w" flag
 
-my $decls_file = $ARGV[0];
-my $decls_new;
-if (scalar(@ARGV) == 1) {
-  $decls_file =~ /.*\/(\S*)\.decls/;
-  $decls_new = "$1_new.decls";
-} elsif (scalar(@ARGV) == 2) {
-  $decls_new = $ARGV[1];
-} else {
-  die "Wrong number of arguments (" . scalar(@ARGV) . ") to decls-add-cluster.pl";
-}
-
-open (IN, $decls_file) || die "couldn't open $decls_file\n";
-open (OUT, ">$decls_new") || die "couldn't open $decls_new for output\n";
-
-while (<IN>) {
-    my $line = $_;
-    print OUT $line;
-    if ($line =~ /:::/) {
-	print OUT "cluster\n";
-	print OUT "int\n";
-	print OUT "int\n";
-	print OUT "22\n";
+foreach my $decls_file (@ARGV) {
+    $decls_file =~ /.*\/(\S*)\.decls/;
+    my $decls_cluster = "$1_daikon_temp.decls";
+    print " $decls_cluster ";
+    open (IN, $decls_file) || die "couldn't open $decls_file\n";
+    open (OUT, ">$decls_cluster") || die "couldn't open $decls_cluster for output\n";
+    
+    while (<IN>) {
+	my $line = $_;
+	print OUT $line;
+	if ($line =~ /:::/) {
+	    print OUT "cluster\n";
+	    print OUT "int\n";
+	    print OUT "int\n";
+	    print OUT "22\n";
+	}
     }
+    close IN;
+    close OUT;
 }
-
-close IN;
-close OUT;
