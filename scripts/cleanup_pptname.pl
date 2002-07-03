5122fb271f4378cf3da33ceaaf638edade989ddd
dodoo <devnull@localhost>
2002-07-03 16:15:14 -0400

    removed unnecessary substitutions in cleaning the prog. point name of unwanted characters

--- scripts/cleanup_pptname.pl
+++ scripts/cleanup_pptname.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 
-# cleanup_pptname.pl - remove unwanted characters from a program point
+# cleanup_pptname.pl - remove non-word characters from a program point
 # name, as declared in a decls file.
 
 my @unwanted = ("<", ">","\\\\", "\\/", ";", "\\\(", "\\\)");
@@ -12,8 +12,7 @@ $pptfilename =~ s/Ljava.lang././;
 foreach my $token (@unwanted) {
     $pptfilename =~ s/$token//g;
 }
-$pptfilename =~ s/</./;
-$pptfilename =~ s/>/./;
+
 $pptfilename =~ s/\(\s*(\S+)\s*\)/_$1_/;
 
 #replace two or more dots in a row with just one dot
