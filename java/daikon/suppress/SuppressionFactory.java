abf20cf0fe056bbab4a6f535e228177018112293
tohn <tohn@alum.mit.edu>
2002-08-12 16:25:26 -0400

    Added findLessThan helper method.

--- java/daikon/suppress/SuppressionFactory.java
+++ java/daikon/suppress/SuppressionFactory.java
@@ -2,6 +2,7 @@ package daikon.suppress;
 
 import daikon.*;
 import daikon.inv.Invariant;
+import daikon.inv.binary.twoScalar.*;
 
 import java.util.*;
 import java.io.Serializable;
@@ -77,12 +78,99 @@ public abstract class SuppressionFactory implements Serializable {
    * @param template a filled template.
    * @param inv the Invariant that is being suppressed
    **/
-  protected SuppressionLink linkFromTemplate (SuppressionTemplate template, Invariant inv) {
+  protected SuppressionLink linkFromTemplate (SuppressionTemplate template,
+                                              Invariant inv) {
     Assert.assertTrue (template.filled, "Template must be filled");
     List suppressors = new ArrayList();
     suppressors.addAll (Arrays.asList(template.results));
     SuppressionLink sl = new SuppressionLink (this, inv, suppressors);
     return sl;
+  }
+
+
+  /**
+   * Attempt to find an Invariant of v1 < v2 or v1 <= v2 to use to
+   * suppress arg.  Generate the appropariate SuppressionLink if
+   * found.  Used as helper by many factories, so that checking v2 >
+   * v1 is the same as v1 < v2, etc.  Only works on integers because
+   * this is used for array indexing.
+   * @param arg The Invariant to suppress.
+   * @param interval The interval that the two VarInfos have to vary by (at least).
+   * @param inv The invariant whose ppt in which we are looking for
+   * the < relationship.
+   **/
+
+  public SuppressionLink findLessEqual (VarInfo v1, VarInfo v2,
+                                        Invariant inv, int interval) {
+    
+    {
+      SuppressionTemplate template = new SuppressionTemplate();
+      template.invTypes = new Class[] {IntLessThan.class};
+      template.varInfos = new VarInfo[][] {new VarInfo[] {v1, v2}};
+      inv.ppt.parent.fillSuppressionTemplate(template);
+      if (template.filled) {
+        IntLessThan resultInv = (IntLessThan) template.results[0];
+        VarInfo leftResult = template.transforms[0][0];
+        if (leftResult == resultInv.var1()
+            && interval <= 0) {
+          // && resultInv.interval >= interval) {
+          // Not used because having IntComparison invariants keep
+          // track of their interval makes them weakening invariants,
+          // which hurts performance.
+          return linkFromTemplate (template, inv);
+        }
+      }
+    }
+
+    {
+      SuppressionTemplate template = new SuppressionTemplate();
+      template.invTypes = new Class[] {IntLessEqual.class};
+      template.varInfos = new VarInfo[][] {new VarInfo[] {v1, v2}};
+      inv.ppt.parent.fillSuppressionTemplate(template);
+      if (template.filled) {
+        IntLessEqual resultInv = (IntLessEqual) template.results[0];
+        VarInfo leftResult = template.transforms[0][0];
+        if (leftResult == resultInv.var1()
+            && interval <= 0) {
+          // && resultInv.interval >= interval) {
+          return linkFromTemplate (template, inv);
+        }
+      }
+    }
+
+    {
+      SuppressionTemplate template = new SuppressionTemplate();
+      template.invTypes = new Class[] {IntGreaterThan.class};
+      template.varInfos = new VarInfo[][] {new VarInfo[] {v1, v2}};
+      inv.ppt.parent.fillSuppressionTemplate(template);
+      if (template.filled) {
+        IntGreaterThan resultInv = (IntGreaterThan) template.results[0];
+        VarInfo leftResult = template.transforms[0][0];
+        if (leftResult == resultInv.var2()
+            && interval <= 0) {
+          // && resultInv.interval >= interval) {
+          return linkFromTemplate (template, inv);
+        }
+      }
+    }
+
+    {
+      SuppressionTemplate template = new SuppressionTemplate();
+      template.invTypes = new Class[] {IntGreaterEqual.class};
+      template.varInfos = new VarInfo[][] {new VarInfo[] {v1, v2}};
+      inv.ppt.parent.fillSuppressionTemplate(template);
+      if (template.filled) {
+        IntGreaterEqual resultInv = (IntGreaterEqual) template.results[0];
+        VarInfo leftResult = template.transforms[0][0];
+        if (leftResult == resultInv.var2()
+            && interval <= 0) {
+          // && resultInv.interval >= interval) {
+          return linkFromTemplate (template, inv);
+        }
+      }
+    }
+
+    return null;
   }
 
 }
