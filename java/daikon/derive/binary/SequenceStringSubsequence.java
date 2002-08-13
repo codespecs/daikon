8f71f9d8f71b363ba1d327c665fa8ffcf9c4fdba
tohn <tohn@alum.mit.edu>
2002-08-13 19:20:53 -0400

    Make parent class for all SequenceXXXSubsequence type derived variables.  Used by suppression factories in daikon.inv.unary.sequence.SingleSequence.

--- java/daikon/derive/binary/SequenceStringSubsequence.java
+++ java/daikon/derive/binary/SequenceStringSubsequence.java
@@ -8,12 +8,12 @@ import daikon.derive.*;
 import utilMDE.*;
 
 public final class SequenceStringSubsequence
-  extends BinaryDerivation
+  extends SequenceSubsequence
 {
   // We are Serializable, so we specify a version to allow changes to
   // method signatures without breaking serialization.  If you add or
   // remove fields, you should change this number to the current date.
-  static final long serialVersionUID = 20020122L;
+  static final long serialVersionUID = 20020801L;
 
   // Variables starting with dkconfig_ should only be set via the
   // daikon.config.Configuration interface.
@@ -22,20 +22,6 @@ public final class SequenceStringSubsequence
    **/
   public static boolean dkconfig_enabled = true;
 
-  // base1 is the sequence
-  // base2 is the scalar
-  public VarInfo seqvar() { return base1; }
-  public VarInfo sclvar() { return base2; }
-
-  // Indicates whether the subscript is an index of valid data or a limit
-  // (one element beyond the data of interest).  The first (or last)
-  // element of the derived variable is seqvar()+index_shift.
-  public final int index_shift;
-
-  // True for deriving from the start of the sequence to the scalar: B[0..I]
-  // False for deriving from the scalar to the end of the sequence: B[I..]
-  public final boolean from_start;
-
   /**
    * @param from_start true means the range goes 0..n; false means the
    * range goes n..end.  (n might be fudged through off_by_one)
@@ -43,12 +29,7 @@ public final class SequenceStringSubsequence
    * the range; false means we should include it
    **/
   public SequenceStringSubsequence (VarInfo vi1, VarInfo vi2, boolean from_start, boolean off_by_one) {
-    super(vi1, vi2);
-    this.from_start = from_start;
-    if (off_by_one)
-      index_shift = from_start ? -1 : +1;
-    else
-      index_shift = 0;
+    super (vi1, vi2, from_start, off_by_one);
   }
 
   public ValueAndModified computeValueAndModified(ValueTuple full_vt) {
@@ -93,37 +74,6 @@ public final class SequenceStringSubsequence
     String [] subarr = ArraysMDE.subarray(val1_array, begin_inclusive, end_exclusive - begin_inclusive);
     subarr = (String []) Intern.intern(subarr);
     return new ValueAndModified(subarr, mod);
-  }
-
-  protected VarInfo makeVarInfo() {
-    VarInfo seqvar = seqvar();
-    VarInfo sclvar = sclvar();
-
-    VarInfoName name;
-    if (from_start) {
-      if (index_shift == 0) {
-        // q[0..c]
-        name = seqvar.name.applySlice(null, sclvar.name);
-      } else if (index_shift == -1) {
-        // q[0..c-1]
-        name = seqvar.name.applySlice(null, sclvar.name.applyDecrement());
-      } else {
-        throw new UnsupportedOperationException("Unsupported shift: " + index_shift);
-      }
-    } else {
-      if (index_shift == 0) {
-        // q[c..]
-        name = seqvar.name.applySlice(sclvar.name, null);
-      } else if (index_shift == 1) {
-        // q[c+1..]
-        name = seqvar.name.applySlice(sclvar.name.applyIncrement(), null);
-      } else {
-        throw new UnsupportedOperationException("Unsupported shift: " + index_shift);
-      }
-    }
-
-    return new VarInfo(name, seqvar.type, seqvar.file_rep_type,
-                       seqvar.comparability, seqvar.aux);
   }
 
   public  boolean isSameFormula(Derivation other) {
