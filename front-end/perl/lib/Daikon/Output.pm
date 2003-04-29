0a0e0f2be316e1f54c44a5e87537bd1525f605d5
smcc <smcc@csail.mit.edu>
2003-04-29 18:39:05 -0400

    Add a flags return to daikon_output_spec, for things like hasNull and isParam. Produce "nonsensical" when a maybe reference isn't. Change the meaning of $single in daikon_output_spec to implement distributing over arrays. Add license comment

--- front-end/perl/lib/Daikon/Output.pm
+++ front-end/perl/lib/Daikon/Output.pm
@@ -2,6 +2,17 @@ package Daikon::Output;
 
 # Routines for printing Perl values in the format that Daikon likes.
 
+# This file is part of the Daikon distribution. It may be used and
+# redistributed under the same terms as the rest of Daikon or, at your
+# option, under the same terms as Perl itself, following either the
+# GNU General Public License or the Perl Artistic License.  The Daikon
+# Perl front end, of which this file is a part, and the Daikon dynamic
+# invariant detection tool are separate programs, neither derived from
+# the other, which are merely aggregated for convenience of
+# distribution. As such, licensing the Perl front end under the terms
+# of the GPL neither requires nor entitles you to license other parts
+# of the Daikon distribution under the same terms.
+
 use strict;
 use Exporter;
 use Carp ('carp', 'cluck', 'croak', 'confess');
@@ -71,7 +82,7 @@ sub escape_char {
 # Convert an arbitrary string into a quoted string in a
 # Daikon-friendly format.
 sub output_string {
-    no warnings 'uninitialized';
+    no warnings 'uninitialized'; # Silently treat undef as ""
     my($s_ref) = @_;
     my $s = $$s_ref;
     $s =~ s/([\\\"\r\n])/escape_char($1)/eg;
@@ -88,14 +99,15 @@ sub output_string {
 # name, $suffix is text to write at the end, $type is the type to tell
 # Daikon, $rep_type is the representation type to tell Daikon, and
 # &output is a sub to convert the value (passed by reference) into a
-# string.
+# string. Because Perl has pretty high subroutine call overhead, the
+# use of closures to represent the output code isn't especially fast:
+# peformance could perhaps be improved, at the expense of elegance, by
+# generating textual code and eval-ing it.
 
-# If the $single argument is true, we're in a context like an array
-# element, so we can only return one value. Otherwise, we can return
-# many specs, like one for each field of an object.
-
-# This routine is still unfinished in places, but it works for simple
-# cases.
+# If the $single argument is true, this output is already going to go
+# in a sequence, so we can't output a nested sequence. We can however
+# output any fixed number of fields, which will be distributed over
+# the sequence.
 
 # Note that when passing $prefix and $suffix up between recursive
 # calls, you usually want to append to the prefix and prepend to the
@@ -105,43 +117,61 @@ sub output_string {
 sub daikon_output_spec {
     my($t, $single) = @_;
     if ($t eq "int") {
-        return ["", "", "int", "int", \&output_int];
+        return ["", "", "int", "int", \&output_int, {}];
     } elsif ($t eq "bit") {
-        return ["", "", "int", "int", \&output_int];
+        return ["", "", "int", "int", \&output_int, {}];
     } elsif ($t eq "num") {
-        return ["", "", "double", "double", \&output_num];
+        return ["", "", "double", "double", \&output_num, {}];
     } elsif ($t eq "str") {
         # It seems that if you use strings in an array, Daikon works
         # better if you say the representation type is
         # "java.lang.String", though in other cases just "String"
         # works fine.
-        return ["", "", "String", "java.lang.String", \&output_string];
+        return ["", "", "String", "java.lang.String", \&output_string, {}];
     } elsif (ref($t)) {
         if ($t->[0] eq "array") {
             if ($single) {
-                return ["\\", "", "Object", "int",
-                        sub { output_int(\ (address($_[0]))); }];
+                # We must have gotten here by a reference, so we'll
+                # have to be content with the address form of the
+                # reference.
+                return ();
             } else {
-                my($prefix, $suffix, $type, $rep_type, $output) =
-                    @{daikon_output_spec($t->[1], 1)};
-                return [$prefix, "[]".$suffix, $type."[]", $rep_type."[]",
-                        sub { "[" . join(" ", map($output->(\$_), @{$_[0]}))
-                                  . "]"}];
+                my @elt_specs = daikon_output_spec($t->[1], 1);
+                my @specs;
+                for my $spec (@elt_specs) {
+                    my($prefix, $suffix, $type, $rep_type, $output, $flags)
+                      = @$spec;
+                    push @specs,
+                      [$prefix, "[]".$suffix,
+                       $type."[]", $rep_type."[]",
+                       sub { "[" . join(" ", map($output->(\$_), @{$_[0]}))
+                               . "]"}, $flags];
+                }
+                return @specs;
             }
         } elsif ($t->[0] eq "hash") {
+            return () if $single;
             my @specs;
-            my($kprefix, $ksuffix, $ktype, $krep_type, $koutput) =
-                @{daikon_output_spec($t->[1], 1)};
-            push @specs, [$kprefix, ".keys".$ksuffix, $ktype."[]",
-                          $krep_type."[]",
-                          sub { "[" . join(" ", map($koutput->(\$_),
-                                                    keys %{$_[0]})) . "]" }];
-            my($vprefix, $vsuffix, $vtype, $vrep_type, $voutput) =
-                @{daikon_output_spec($t->[2], 1)};
-            push @specs, [$vprefix, ".values".$vsuffix, $vtype."[]",
-                          $vrep_type."[]",
-                          sub { "[" . join(" ", map($voutput->(\$_),
-                                                    values %{$_[0]})) . "]" }];
+            for my $spec (daikon_output_spec($t->[1], 1)) {
+                my($kprefix, $ksuffix, $ktype, $krep_type, $koutput, $flags)
+                  = @$spec;
+                push @specs, [$kprefix, ".keys".$ksuffix, $ktype."[]",
+                              $krep_type."[]",
+                              sub { "[" .
+                                      join(" ", map($koutput->(\$_),
+                                                    keys %{$_[0]})) . "]" },
+                              $flags];
+            }
+            for my $spec (daikon_output_spec($t->[2], 1)) {
+                my($vprefix, $vsuffix, $vtype, $vrep_type, $voutput, $flags)
+                  = @$spec;
+                push @specs, [$vprefix, ".values".$vsuffix, $vtype."[]",
+                              $vrep_type."[]",
+                              sub { "[" .
+                                      join(" ", map($voutput->(\$_),
+                                                    values %{$_[0]})) . "]" },
+                              $flags];
+            }
             return @specs;
         } elsif ($t->[0] eq "list") {
             die if $single; # lists should only be at top level
@@ -149,61 +179,62 @@ sub daikon_output_spec {
             my @specs;
             for my $index (0 .. $num_fixed - 1) {
                 for my $spec (daikon_output_spec($t->[1][$index], 0)) {
-                    my($prefix, $suffix, $type, $rep_type, $output) = @$spec;
+                    my($prefix, $suffix, $type, $rep_type, $output, $flags)
+                      = @$spec;
                     push @specs, [$prefix, "[$index]".$suffix, $type,
                                   $rep_type,
-                                  sub { $output->(\ ($_[0]->[$index])) }];
+                                  sub { $output->(\ ($_[0]->[$index])) },
+                                  $flags];
                 }
             }
             if ($t->[2]) {
                 my $rest = $t->[2];
                 for my $spec (daikon_output_spec($rest, 1)) {
-                    my($prefix, $suffix, $type, $rep_type, $output) = @$spec;
-                    $rep_type = "java.lang.String" if $rep_type eq "String";
+                    my($prefix, $suffix, $type, $rep_type, $output, $flags)
+                      = @$spec;
+                    my $slice = $num_fixed ? "[$num_fixed..]" : "";
                     push @specs,
-                        [$prefix, $suffix."[$num_fixed..][]", $type."[]",
+                        [$prefix, $suffix . $slice . "[]", $type."[]",
                          $rep_type."[]",
                          sub { "[" .
                                    join(" ",
                                         map($output->(\$_),
                                             @{$_[0]}[$num_fixed .. $#{$_[0]}]))
-                                       . "]" }];
+                                       . "]" },
+                         $flags];
                 }
             }
             return @specs;
         } elsif ($t->[0] eq "object") {
-            if ($single) {
-                return ["\\", "", "Object", "int",
-                        sub { output_int(\ (address(${$_[0]}))); }];
-            } else {
-                my @specs;
-                # When this was enabled, I'd picked ".Class" to look
-                # distinct from usual field names that are
-                # lowercase. But Daikon treats ".class" specially, so
-                # maybe it should be that.
-#                push @specs, ["", ".Class", "String",
-#                             "java.lang.String", sub { '"'. ref($_[0]).'"' }];
-                for my $k (keys %{$t->[1]}) {
-                    for my $spec (daikon_output_spec($t->[1]{$k})) {
-                        my($prefix, $suffix, $type, $rep_type, $output)
-                            = @$spec;
-                        push @specs, [$prefix, ".$k$suffix", $type, $rep_type,
-                                      sub { exists($_[0]->{$k}) ? 
-                                                $output->(\ ($_[0]->{$k}))
-                                            : $output->(\undef) }];
-                    }
+            my @specs;
+            # When this was enabled, I'd picked ".Class" to look
+            # distinct from usual field names that are
+            # lowercase. But Daikon treats ".class" specially, so
+            # maybe it should be that.
+#            push @specs, ["", ".Class", "String",
+#                         "java.lang.String", sub { '"'. ref($_[0]).'"' }];
+            for my $k (keys %{$t->[1]}) {
+                for my $spec (daikon_output_spec($t->[1]{$k}, $single)) {
+                    my($prefix, $suffix, $type, $rep_type, $output, $flags)
+                      = @$spec;
+                    push @specs, [$prefix, ".$k$suffix", $type, $rep_type,
+                                  sub { exists($_[0]->{$k}) ? 
+                                          $output->(\ ($_[0]->{$k}))
+                                            : $output->(\undef) },
+                                  $flags];
                 }
-                for my $k (keys %{$t->[2]}) {
-                    for my $spec (daikon_output_spec($t->[2]{$k})) {
-                        my($prefix, $suffix, $type, $rep_type, $output)
-                            = @$spec;
-                        push @specs, [$prefix, ".$k()$suffix", $type,
-                                      $rep_type,
-                                      sub { $output->(\scalar($_[0]->$k)) }];
-                    }
-                }
-                return @specs;
             }
+            for my $k (keys %{$t->[2]}) {
+                for my $spec (daikon_output_spec($t->[2]{$k}, $single)) {
+                    my($prefix, $suffix, $type, $rep_type, $output, $flags)
+                      = @$spec;
+                    push @specs, [$prefix, ".$k()$suffix", $type,
+                                  $rep_type,
+                                  sub { $output->(\scalar($_[0]->$k)) },
+                                  $flags];
+                }
+            }
+            return @specs;
         } elsif ($t->[0] eq "ref") {
             # Some Perl documenters have tried to promulgate "sigil"
             # as the name for the funny characters at the front of
@@ -218,24 +249,30 @@ sub daikon_output_spec {
 #           } else {
 #               $sigil = '$';
 #           }
-
             if ($t->[1] eq "top") {
                 return ["", "", "Object", "hashcode",
-                        sub { output_int(\ (address($_[0]))); }];
+                        sub { output_int(\ (address(${$_[0]}))); },
+                        {addr => 1}];
             } else {
                 my @specs;
                 for my $spec (daikon_output_spec($t->[1], $single)) {
-                    my($prefix, $suffix, $type, $rep_type, $output) = @$spec;
+                    my($prefix, $suffix, $type, $rep_type, $output, $flags)
+                      = @$spec;
                     push @specs, [$prefix, '.deref'.$suffix, $type, $rep_type,
-                                  sub { $output->(${$_[0]}) }];
+                                  sub {
+                                      die "nonsensical" unless ref ${$_[0]};
+                                      $output->(${$_[0]})
+                                  },
+                                  {%$flags, saw_deref => 1}];
                 }
                 push @specs, ["", "", "Object", "hashcode",
-                              sub { output_int(\ (address(${$_[0]}))); }];
-                return $single ? $specs[0] : @specs;
+                              sub { output_int(\ (address(${$_[0]}))); },
+                              {addr => 1}];
+                return @specs;
             }
         } elsif ($t->[0] eq "maybe") {
             my $is_def = ["", ".is_defined", "boolean", "boolean",
-                          sub { defined(${$_[0]}) ? "true" : "false"}];
+                          sub { defined(${$_[0]}) ? "true" : "false"}, {}];
             if ($t->[1] eq "bottom") {
                 # 'nothing' is always either the empty string or the
                 # undefined value, so the only interesting information
@@ -243,23 +280,30 @@ sub daikon_output_spec {
                 return $is_def;
             }
             my $is_empty = ["", ".is_empty", "boolean", "boolean",
-                            sub { length(${$_[0]})==0 ? "true" : "false"}];
+                            sub { no warnings 'uninitialized';
+                                  length(${$_[0]})==0 ? "true" : "false"}, {}];
             my @extra_specs = ($is_def, $is_empty);
             if ($t->[1] eq "bit") {
                 # Boolean is a special case, since we want 'bit's to
                 # show up as integers, but 'bool's to show up as
                 # booleans.
                 my $bool = ["", "", "boolean", "boolean",
-                            sub { ${$_[0]} ? "true" : "false"}];
-                return $single ? $bool : ($bool, @extra_specs);
+                            sub { ${$_[0]} ? "true" : "false"}, {}];
+                return ($bool, @extra_specs);
             } elsif ($t->[1] eq "str") {
                 my @specs = daikon_output_spec($t->[1], $single);
                 # is_empty isn't needed, since the empty string is a
                 # regular string.
-                return $single ? $specs[0] : (@specs, $is_def);
+                return (@specs, $is_def);
             } else {
                 my @specs = daikon_output_spec($t->[1], $single);
-                return $single ? $specs[0] : (@specs, @extra_specs);
+                my @lifted_specs;
+                foreach my $spec (@specs) {
+                    my @spec = @$spec;
+                    $spec[1] = ".value" . $spec[1];
+                    push @lifted_specs, [@spec];
+                }
+                return (@specs, @extra_specs);
             }
         }
     }
@@ -270,6 +314,19 @@ sub daikon_output_spec {
 # string representations.
 my %specs;
 
+sub simplify_name {
+    my($name) = @_;
+    # Turn .deref.foo into .foo, except when foo is "is_defined" or
+    # "is_empty". Internal derefs usually make the name longer without
+    # adding information, so we'd like to get rid of them, but in the
+    # case of a maybe reference to a maybe, they're needed to
+    # distinguish properties describing the definedness or emptiness
+    # of the referrer and referent maybes.
+    $name =~ s/(\.deref)(\.\w+)/
+      ($2 eq ".is_defined" || $2 eq ".is_empty") ? $1.$2 : $2/eg;
+    return $name;
+}
+
 # Format a variable declaration for a .decls file, given the name for
 # the variable and list of specifications. Returns a list of lines.
 sub declare_var_spec {
@@ -277,18 +334,19 @@ sub declare_var_spec {
     my @lines = ();
 
     for my $spec (@specs) {
-        my($prefix, $suffix, $type, $rep_type, undef) = @$spec;
-        my $name = "$prefix$varname$suffix";
-        my $is_param = "";
-        if ($varname ne "return" and $name !~ /\.deref/) {
-            # It might be more elegant to do this by counting
-            # dereferences as part of the spec, rather than text
-            # matching the name. It would be more work, though.
-            $is_param = " # isParam = true";
+        my($prefix, $suffix, $type, $rep_type, undef, $flags) = @$spec;
+        my $name = simplify_name("$prefix$varname$suffix");
+        my @aux;
+        if ($varname ne "return" and not $flags->{saw_deref}) {
+            push @aux, "isParam = true";
         }
-        $name =~ s/\.deref\././g;
+        if ($flags->{addr}) {
+            push @aux, "hasNull = false";
+        }
+        my $aux = join(", ", @aux);
+        $aux = " # $aux" if length $aux;
         push @lines, "$name\n"; # Variable name
-        push @lines, "$type$is_param\n";
+        push @lines, "$type$aux\n";
         push @lines, "$rep_type\n";
         push @lines, "22\n"; # comparability: not available
     }
@@ -300,7 +358,7 @@ sub declare_var_spec {
 sub declare_var {
     my($varname, $type) = @_;
     if (not exists $specs{$type}) {
-        $specs{$type} = [daikon_output_spec(parse_type($type, 0))];
+        $specs{$type} = [daikon_output_spec(parse_type($type), 0)];
     }
     return declare_var_spec($varname, @{$specs{$type}});
 }
@@ -311,14 +369,21 @@ sub declare_var {
 sub output_var_spec {
     my($fh, $varname, $value, @specs) = @_;
     for my $spec (@specs) {
-        my($prefix, $suffix, undef, undef, $output) = @$spec;
-        my $name = "$prefix$varname$suffix";
-        # This is a cosmetic hack to reduce the number of redundant
-        # ".deref"s in the output. It could be done more carefully.
-        $name =~ s/\.deref\././g;
+        my($prefix, $suffix, undef, undef, $output, undef) = @$spec;
+        my $name = simplify_name("$prefix$varname$suffix");
         print $fh "$name\n";
-        print $fh $output->($value), "\n";
-        print $fh "1\n"; # Pretend always modified
+        my $out_str;
+        eval {
+            $out_str = $output->($value);
+        };
+        if ($@ =~ /nonsensical/) {
+            print $fh "nonsensical\n2\n";
+        } elsif (length $@) {
+            die $@; # rethrow exception
+        } else {
+            print $fh "$out_str\n";
+            print $fh "1\n"; # Pretend always modified
+        }
     }
 }
 
@@ -328,7 +393,7 @@ sub output_var_spec {
 sub output_var {
     my($fh, $varname, $value, $type) = @_;
     if (not exists $specs{$type}) {
-        $specs{$type} = [daikon_output_spec(parse_type($type, 0))];
+        $specs{$type} = [daikon_output_spec(parse_type($type), 0)];
     }
     output_var_spec($fh, $varname, $value, @{$specs{$type}});
 }
