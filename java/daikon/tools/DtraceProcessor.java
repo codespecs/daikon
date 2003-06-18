cd233677542b04cd2012d66c6b299b60e0b4fc2f
akcabac <devnull@localhost>
2003-06-18 16:25:28 -0400

    An interface for accessing dtrace files.

---
+++ java/daikon/tools/DtraceProcessor.java
@@ -0,0 +1,11 @@
+package daikon.tools;
+import java.util.*;
+
+
+public interface DtraceProcessor {
+   //
+   // void Auto class visitors
+   //
+   public void visit(daikon.PptTopLevel ppt, List n);
+
+}
