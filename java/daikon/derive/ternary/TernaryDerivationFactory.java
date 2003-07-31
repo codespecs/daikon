efa42e7b8ffe3386e8298ea174bbe9b3052565e7
akcabac <devnull@localhost>
2003-07-31 13:26:23 -0400

    Added three static methods for type and comparability checks.

--- java/daikon/derive/ternary/TernaryDerivationFactory.java
+++ java/daikon/derive/ternary/TernaryDerivationFactory.java
@@ -23,4 +23,63 @@ public abstract class TernaryDerivationFactory implements DerivationFactory {
   public abstract TernaryDerivation[] instantiate(VarInfo vi1, VarInfo vi2,
                                                   VarInfo vi3);
 
+
+  /**
+   * Check if vi1 and vi2 can be a part of a VarInfo triple based on
+   * their types.
+   * @param vi1
+   * @param vi2
+   * @return a boolean which is true if vi1 and vi2 can belong to
+   * a valid VarInfo triple. (false otherwise)
+   */
+  public static boolean checkType (VarInfo vi1, VarInfo vi2){
+    if ((vi1.rep_type == ProglangType.INT_ARRAY)
+        && (vi2.rep_type == ProglangType.INT))
+      return true;
+    else if ((vi1.rep_type == ProglangType.INT)
+             && (vi2.rep_type == ProglangType.INT_ARRAY))
+      return true;
+    else if ((vi1.rep_type == ProglangType.INT)
+             && (vi2.rep_type == ProglangType.INT))
+      return true;
+    else
+      return false;
+  }
+
+  /**
+   * Check if vi1 and vi2 can be a part of a VarInfo triple based on
+   * their types.
+   * @param vi1
+   * @param vi2
+   * @return a ProglangType which is the type of VarInfo needed to make
+   * a valid VarInfo triple out of vi1 and vi2 (false otherwise)
+   */
+  public static ProglangType returnType (VarInfo vi1, VarInfo vi2) {
+    if ((vi1.rep_type == ProglangType.INT_ARRAY)
+        && (vi2.rep_type == ProglangType.INT))
+      return ProglangType.INT;
+    else if ((vi1.rep_type == ProglangType.INT)
+             && (vi2.rep_type == ProglangType.INT_ARRAY))
+      return ProglangType.INT;
+    else if ((vi1.rep_type == ProglangType.INT)
+             && (vi2.rep_type == ProglangType.INT))
+      return ProglangType.INT_ARRAY;
+    else
+      return null;
+  }
+
+  /**
+   * Checks if two base variables are comparable to each other.
+   *
+   * @param vi1
+   * @param vi2
+   * @return a boolean which is true if vi1 and vi2 are comparable.
+   * (false otherwise).
+   */
+  public static boolean checkComparability(VarInfo vi1, VarInfo vi2){
+    if (vi1.indexCompatible(vi2))
+      return true;
+    else
+      return false;
+  }
 }
