7efdd84548e746855881c0a6f778f4dc7d24c1a1
jhp <jhp@csail.mit.edu>
2004-01-27 14:30:50 -0500

    - Changed FunctionUnary test to Numeric.BitwiseComplement

--- java/daikon/test/inv/InvariantTester.java
+++ java/daikon/test/inv/InvariantTester.java
@@ -37,9 +37,9 @@ public class InvariantTester extends TestCase {
     Invariant inv1, inv2, inv2_2, inv2_3, inv2_4, inv2_5, inv2_6, inv3, inv4, inv5, inv6;
 
     Configuration.getInstance().apply
-      ("daikon.inv.binary.twoScalar.FunctionUnary.enabled = 1");
+      ("daikon.inv.binary.twoScalar.NumericInt.enabled = 1");
 
-    inv1 = FunctionUnary.instantiate(slice, new String[] { "foo", "bar" }, 0, false);
+    inv1 = NumericInt.BitwiseComplement.instantiate(slice);
     Assert.assertTrue(c.compare(inv1, inv1) == 0);
 
     inv2 = IntEqual.instantiate(slice);
@@ -48,18 +48,18 @@ public class InvariantTester extends TestCase {
     inv2_4 = IntLessEqual.instantiate(slice);
     inv2_5 = IntGreaterThan.instantiate(slice);
     inv2_6 = IntGreaterEqual.instantiate(slice);
-    Assert.assertTrue(c.compare(inv1, inv2) < 0);
-    Assert.assertTrue(c.compare(inv1, inv2_2) < 0);
-    Assert.assertTrue(c.compare(inv1, inv2_3) < 0);
-    Assert.assertTrue(c.compare(inv1, inv2_4) < 0);
-    Assert.assertTrue(c.compare(inv1, inv2_5) < 0);
-    Assert.assertTrue(c.compare(inv1, inv2_6) < 0);
+    Assert.assertTrue(c.compare(inv1, inv2) > 0);
+    Assert.assertTrue(c.compare(inv1, inv2_2) > 0);
+    Assert.assertTrue(c.compare(inv1, inv2_3) > 0);
+    Assert.assertTrue(c.compare(inv1, inv2_4) > 0);
+    Assert.assertTrue(c.compare(inv1, inv2_5) > 0);
+    Assert.assertTrue(c.compare(inv1, inv2_6) > 0);
 
     inv3 = LinearBinary.instantiate(slice);
-    Assert.assertTrue(c.compare(inv3, inv1) > 0);
+    Assert.assertTrue(c.compare(inv3, inv1) < 0);
 
     inv4 = IntNonEqual.instantiate(slice);
-    Assert.assertTrue(c.compare(inv1, inv4) < 0);
+    Assert.assertTrue(c.compare(inv1, inv4) > 0);
 
     inv5 = Implication.makeImplication(ppt, inv1, inv2, false, null, null);
     inv6 = Implication.makeImplication(ppt, inv1, inv3, false, null, null);
@@ -67,15 +67,15 @@ public class InvariantTester extends TestCase {
 
     inv5 = Implication.makeImplication(ppt, inv2, inv1, false, null, null);
     inv6 = Implication.makeImplication(ppt, inv2, inv3, false, null, null);
-    Assert.assertTrue(c.compare(inv5, inv6) < 0);
+    Assert.assertTrue(c.compare(inv5, inv6) > 0);
 
     inv5 = Implication.makeImplication(ppt, inv3, inv2, false, null, null);
     inv6 = Implication.makeImplication(ppt, inv3, inv1, false, null, null);
-    Assert.assertTrue(c.compare(inv5, inv6) > 0);
+    Assert.assertTrue(c.compare(inv5, inv6) < 0);
 
     inv5 = Implication.makeImplication(ppt, inv1, inv4, false, null, null);
     inv6 = Implication.makeImplication(ppt, inv3, inv4, false, null, null);
-    Assert.assertTrue(c.compare(inv5, inv6) < 0);
+    Assert.assertTrue(c.compare(inv5, inv6) > 0);
 
     inv5 = Implication.makeImplication(ppt, inv2, inv4, false, null, null);
     inv6 = Implication.makeImplication(ppt, inv4, inv1, false, null, null);
@@ -84,14 +84,14 @@ public class InvariantTester extends TestCase {
     VarInfo[] vars2 = { Common.makeIntVarInfo("x"), Common.makeIntVarInfo("z") };
     PptTopLevel ppt2 = Common.makePptTopLevel("Foo:::OBJECT", vars2);
     PptSlice slice2 = new PptSlice2(ppt2, vars2);
-    inv2 = FunctionUnary.instantiate(slice2, new String[] {"foo", "bar"}, 0, false);
+    inv2 = NumericInt.BitwiseComplement.instantiate(slice2);
     Assert.assertTrue(c.compare(inv1, inv2) < 0);
 
     vars2[0] = Common.makeIntVarInfo("a");
     vars2[1] = Common.makeIntVarInfo("y");
     ppt2 = Common.makePptTopLevel("Foo:::OBJECT", vars2);
     slice2 = new PptSlice2(ppt2, vars2);
-    inv2 = FunctionUnary.instantiate(slice2, new String[] {"foo","bar"}, 0, false);
+    inv2 = NumericInt.BitwiseComplement.instantiate (slice2);
     Assert.assertTrue(c.compare(inv1, inv2) > 0);
   }
 
