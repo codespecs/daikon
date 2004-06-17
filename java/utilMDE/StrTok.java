94c7fe0bece9500409403dad9f6e1d760f69ec73
jhp <jhp@csail.mit.edu>
2004-06-16 23:48:42 -0400

    - Added more complete javadoc comments.   - Renamed some methods to have more standard java names

--- java/utilMDE/StrTok.java
+++ java/utilMDE/StrTok.java
@@ -5,7 +5,24 @@ import java.io.*;
 
 /**
  * Provides a somewhat simpler interface for tokenizing strings than
- * does StreamTokenizer.  All tokenizing is done by StreamTokenizer.
+ * does StreamTokenizer.  All tokenizing is done by StreamTokenizer. <p>
+ *
+ * The major difference from StreamTokenizer is that all tokens are
+ * returned as strings.  EOF returns a null, EOL returns an empty string.
+ * Delimiters are returned as one character strings.  Words and numbers
+ * are returned as strings.  Quoted strings are also returned as strings
+ * including their quote characters (so they can easily be differentiated
+ * from words and numbers).
+ *
+ * Other differences are: <ul>
+ *  <Li> Automatic setup for tokenizing strings
+ *  <li> Underscores are included in identifiers (words)
+ *  <li> I/O errors (which should be impossible when tokenizing strings) are
+ *       converted to RuntimeExceptions so that every call doesn't have to
+ *       be included in a try block
+ *  <li> Convenience functions isWord(), isQString(), and need()
+ *  <li> String tokens are interned for easier comparisons.
+ * </ul>
  */
 public class StrTok {
 
@@ -13,6 +30,9 @@ public class StrTok {
   public StreamTokenizer stok;
   Error err = new Error();
 
+  /**
+   * Creates a tokenizer for the specified string.
+   */
   public StrTok (String s) {
 
     reader = new StringReader (s);
@@ -21,11 +41,16 @@ public class StrTok {
   }
 
   /**
-   * Class for error handling.  Users can provide a different implementation
-   * to have errors handled differently.
+   * Default Class for error handling.  Throws a RuntimeException when an
+   * error occurs.
+   *
+   * @see StrTok#set_error_handler(StrTok.Error)
    */
   public static class Error {
 
+    /**
+     * Called when an unexpected token is found (see {@link #need(String)})
+     */
     public void tok_error (String s) {
       throw new RuntimeException ("StrTok error: " + s);
     }
@@ -50,6 +75,10 @@ public class StrTok {
     return (token());
   }
 
+  /**
+   * Returns the current token.
+   * @see #nextToken()
+   */
   public String token() {
 
     int ttype = stok.ttype;
@@ -82,30 +111,55 @@ public class StrTok {
                                 " from StreamTokenizer");
   }
 
+  /**
+   * Specifies the single line comment character.
+   * @see StreamTokenizer#commentChar(int)
+   */
   public void commentChar (int ch) {
     stok.commentChar (ch);
   }
 
+  /**
+   * Specifies that matching pairs of this character delimit string constants.
+   * @see StreamTokenizer#quoteChar(int)
+   */
   public void quoteChar (int ch) {
     stok.quoteChar (ch);
   }
 
+  /**
+   * Returns the type of the current token.
+   * @see StreamTokenizer#ttype
+   */
   public int ttype() {
     return stok.ttype;
   }
 
-  public boolean word() {
+  /** Returns true if the current token is a word (identifier) */
+  public boolean isWord() {
     return (stok.ttype == StreamTokenizer.TT_WORD);
   }
 
-  public boolean qstring() {
+  /** Returns true if the current token is a quoted string */
+  public boolean isQString() {
     return ((stok.sval != null) && (stok.ttype > 0));
   }
 
+  /**
+   * Sets the error handler.  The default error handler will throw a
+   * runtime exception on errors.
+   * @see Error
+   */
   public void set_error_handler (Error err) {
     this.err = err;
   }
 
+  /**
+   * Reads the next token and checks that it matches tok.  If it does
+   * not match, calls the current error handling routine (see {@link
+   * #set_error_handler(StrTok.Error) set_error_handler()}).
+   * If it does match, just returns.
+   */
   public void need (String tok) {
 
     String t = nextToken();
