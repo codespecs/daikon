7c6d5ec0957835319dc8e835e8024cbf3c1493f9
jhp <jhp@csail.mit.edu>
2004-07-29 17:44:06 -0400

    - Added is_instantiate_ok()   - Support unary and binary suppressees and recursive suppressions

--- java/daikon/suppress/NISuppressionSet.java
+++ java/daikon/suppress/NISuppressionSet.java
@@ -11,8 +11,7 @@ import java.util.*;
 
 /**
  * Class that defines a set of non-instantiating suppressions for a single
- * invariant (suppressee).  Basically this just a list of suppressions, but
- * creating a class allows this to be somewhat more clear.
+ * invariant (suppressee).
  */
 public class NISuppressionSet {
 
@@ -70,6 +69,13 @@ public class NISuppressionSet {
     }
   }
 
+  /**
+   * NIS process a falsified invariant. This method should be called for
+   * each falsified invariant in turn.  Any invariants for which inv is
+   * the last valid suppressor are added to new_invs.
+   *
+   * @deprecated  Use NIS.process_falsified_invs() instead.
+   */
   public void falsified (Invariant inv, List new_invs) {
 
     // Get the ppt we are working in
@@ -162,7 +168,7 @@ public class NISuppressionSet {
    * If the falsification of inv removed the last valid suppression then
    * instantiates the suppressee
    */
-  public void check_falsified (PptTopLevel ppt, VarInfo[] vis, Invariant inv,
+  private void check_falsified (PptTopLevel ppt, VarInfo[] vis, Invariant inv,
                                List new_invs) {
 
     // process each suppression in the set, marking each suppressor as
@@ -209,14 +215,16 @@ public class NISuppressionSet {
    * Determines whether or not the suppression set is valid in the
    * specified ppt and var_infos.  The suppression set is valid if any
    * of its suppressions are valid.  A suppression is valid if all of
-   * its non-missing suppressors are true.
+   * its suppressors are true.
+   *
+   * @see is_instantiate_ok() for a check that considers missing
    */
   public boolean suppressed (PptTopLevel ppt, VarInfo[] var_infos) {
 
     // Check each suppression to see if it is valid
     for (int i = 0; i < suppression_set.length; i++ ) {
       String status = suppression_set[i].check (ppt, var_infos, null);
-      if ((status == NIS.VALID) || (status == NIS.MISSING)) {
+      if (status == NIS.VALID) {
         if (Debug.logOn() || NIS.debug.isLoggable (Level.FINE))
           Debug.log (NIS.debug, getClass(), ppt, var_infos, "suppression "
             + suppression_set[i] + " is " + status + " in ppt " + ppt
@@ -233,23 +241,63 @@ public class NISuppressionSet {
   }
 
   /**
+   * Determines whether or not the suppression set is valid in the
+   * specified slice.  The suppression set is valid if any of its
+   * suppressions are valid.  A suppression is valid if all of its
+   * non-missing suppressors are true.
+   */
+  public boolean is_instantiate_ok (PptSlice slice) {
+
+    return (is_instantiate_ok (slice.parent, slice.var_infos));
+  }
+
+  /**
+   * Determines whether or not the suppressee of the suppression set
+   * should be instantiated.  Instantiation is ok only if each
+   * suppression is invalid.  A suppression is valid if all of
+   * its non-missing suppressors are true.
+   */
+  public boolean is_instantiate_ok (PptTopLevel ppt, VarInfo[] var_infos) {
+
+    // Check each suppression to see if it is valid
+    for (int i = 0; i < suppression_set.length; i++ ) {
+      String status = suppression_set[i].check (ppt, var_infos, null);
+      if ((status == NIS.VALID) || (status == NIS.MISSING)) {
+        if (Debug.logOn() || NIS.debug.isLoggable (Level.FINE))
+          Debug.log (NIS.debug, getClass(), ppt, var_infos, "suppression "
+            + suppression_set[i] + " is " + status + " in ppt " + ppt
+            + " with var infos " + VarInfo.toString (var_infos));
+        return (false);
+      }
+    }
+
+    if (Debug.logOn() || NIS.debug.isLoggable (Level.FINE))
+      Debug.log (NIS.debug, getClass(), ppt, var_infos, "suppression " + this
+                  + " is not valid in ppt " + ppt + " with var infos "
+                  + VarInfo.toString (var_infos));
+    return (true);
+  }
+
+  /**
    * Instantiates the suppressee over the specified variables in the
    * specified ppt.  The invariant is added to the new_invs list, but
    * not to the slice.  The invariant is added to the slice later when
    * the sample is applied to it.  That guarantees that it is only applied
    * the sample once.
+   *
+   * @deprecated
    */
-  public void instantiate (PptTopLevel ppt, VarInfo[] vis, List new_invs) {
+  private void instantiate (PptTopLevel ppt, VarInfo[] vis, List new_invs) {
 
     NIS.new_invs_cnt++;
 
     // If the suppressee will be falsified by the sample, don't bother
     // to create it.
     NISuppressee suppressee = suppression_set[0].suppressee;
-    if (suppressee.check (NIS.vt, vis) == InvariantStatus.FALSIFIED) {
-      NIS.false_invs_cnt++;
-      return;
-    }
+    // if (suppressee.check (NIS.vt, vis) == InvariantStatus.FALSIFIED) {
+    //  NIS.false_invs_cnt++;
+    //  return;
+    // }
 
     if (Assert.enabled) {
       for (int i = 0; i < vis.length; i++)
@@ -315,6 +363,55 @@ public class NISuppressionSet {
 
   }
 
+
+  /**
+   * Each suppression where a suppressor matches the suppressee in ss is
+   * augmented by additional suppression(s) where the suppressor is replaced
+   * by each of its suppressions.  This allows recursive suppressions.
+   *
+   * For example, consider the suppressions:
+   *
+   *    (r == arg1) && (arg2 <= arg1) ==> r = max(arg1,arg2)
+   *    (arg2 == arg1) ==> arg2 <= arg1
+   *
+   * The suppressor (arg2 <= arg1) in the first suppression matches the
+   * suppressee in the second suppression.  In order for the first
+   * suppression to work even when (arg2 <= arg1) is suppressed, the
+   * second suppression is added to the first:
+   *
+   *    (r == arg1) && (arg2 <= arg1) ==> r = max(arg1,arg2)
+   *    (r == arg1) && (arg2 == arg1) ==> r = max(arg1,arg2)
+   *
+   * When (arg2 <= arg1) is suppressed, the second suppression for max
+   * will still suppress max.  If (arg2 == arg1) is falsified, the
+   * (arg2 <= arg1) invariant will be created and can continue to suppress
+   * max (as long as it is not falsified itself).
+   */
+  public void recurse_definitions (NISuppressionSet ss) {
+
+    // Get all of the new suppressions
+    List/*NISuppression*/ new_suppressions = new ArrayList();
+    for (int i = 0; i < suppression_set.length; i++) {
+      new_suppressions.addAll (suppression_set[i].recurse_definition (ss));
+    }
+    Assert.assertTrue (new_suppressions.size() > 0);
+
+    // Create a new suppression set with all of the suppressions.
+    NISuppression[] new_array
+      = new NISuppression [suppression_set.length + new_suppressions.size()];
+    for (int i = 0; i < suppression_set.length; i++)
+      new_array[i] = suppression_set[i];
+    for (int i = 0; i < new_suppressions.size(); i++)
+      new_array[suppression_set.length + i]
+        = (NISuppression) new_suppressions.get(i);
+    suppression_set = new_array;
+  }
+
+
+  /** Returns the suppressee **/
+  public NISuppressee get_suppressee() {
+    return suppression_set[0].suppressee;
+  }
 
   /**
    * Clears the suppressor state in each suppression.
