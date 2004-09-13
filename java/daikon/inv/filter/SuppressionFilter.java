726b3f781d35d521ac11c1cf45aeeb16c68bd539
jhp <jhp@csail.mit.edu>
2004-09-12 20:53:24 -0400

    - Deprecated the filter

--- java/daikon/inv/filter/SuppressionFilter.java
+++ java/daikon/inv/filter/SuppressionFilter.java
@@ -6,26 +6,14 @@ import java.util.logging.Level;
 
 /**
  * Filter for not printing invariants suppressed during checking.
+ * JHP: remove
  **/
 public class SuppressionFilter extends InvariantFilter {
   public String getDescription() {
-    return "Suppress invariants that aren't checked during run";
+    return "Suppress invariants that aren't checked during run [DEPRECATED]";
   }
 
   boolean shouldDiscardInvariant( Invariant inv ) {
-    if (inv instanceof Implication) {
-      Implication imp = (Implication) inv;
-      if (imp.consequentSuppressor() != null) {
-        return (true);
-      }
-    }
-    if (inv.getSuppressor() != null) {
-      if (inv.logOn() || PrintInvariants.debugFiltering.isLoggable(Level.FINE)) {
-        inv.log (PrintInvariants.debugFiltering,
-                 "suppressed by: " + inv.getSuppressor());
-      }
-      return true;
-    }
-    return false;
+    return (false);
   }
 }
