6d798209daf1ca5d54d8b7422c330a4964d13aaf
smcc <smcc@csail.mit.edu>
2004-11-30 13:49:39 -0500

    A script to count the number of occurrences of program points in a .dtrace file, and the number of lines in each. Provides a useful summary when the .dtrace contents are too volatile or the .dtrace file itself too big.

---
+++ scripts/dtrace-count.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use 5.006;
+use warnings;
+
+# $ppt_count{ppt_name} = [count, num_lines];
+my %ppt_count;
+
+my %long_names;
+
+$/ = ""; # Read by paragraph
+while (<>) {
+    /^(.*):::([A-Z\d]+)$/m or die "Unexpected PPT name";
+    my $name = "$1:::$2";
+    $long_names{$1}{$2} = 1;
+    s/\n+$//;
+    my $line_count = ($_ =~ tr/\n/\n/) + 1;
+    if (exists $ppt_count{$name}) {
+	$ppt_count{$name}[0]++;
+	die "Mismatched line count ($ppt_count{$name}[1] vs. $line_count)".
+	  " for $name"
+	    unless $ppt_count{$name}[1] == $line_count;
+    } else {
+	$ppt_count{$name}[0] = 1;
+	$ppt_count{$name}[1] = $line_count;
+    }
+}
+
+for my $short_name (sort keys %long_names) {
+    print "$short_name";
+    for my $ppt_part (sort keys %{$long_names{$short_name}}) {
+	my $name = "${short_name}:::$ppt_part";
+	print " $ppt_part/$ppt_count{$name}[1]: $ppt_count{$name}[0]";
+    }
+    print "\n";
+}
+
