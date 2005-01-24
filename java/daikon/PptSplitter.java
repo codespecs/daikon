cea897f9cd0011683550acc26c9067ffbc526fa8
jhp <jhp@csail.mit.edu>
2005-01-24 18:47:30 -0500

    - Changed the creation of implications to choose a leader from the     exclusive invariant pairs and use only that leader as a predicate.     Also, no longer creates implications that are obvious or suppressed.     Its not clear that this last test is appropriate.

--- java/daikon/PptSplitter.java
+++ java/daikon/PptSplitter.java
@@ -327,7 +327,7 @@ public class PptSplitter implements Serializable {
         Invariant[] invs = (Invariant[]) i.next();
         invs[0].log ("exclusive condition with " + invs[1].format());
         invs[1].log ("exclusive condition with " + invs[0].format());
-        debug.fine ("-- " + Invariant.toString (invs));
+        debug.fine ("-- " + invs[0] + " -- " + invs[1]);
       }
       debug.fine ("Found " + different_invs_vec.size() + " different invariants ");
       for (Iterator i = different_invs_vec.iterator(); i.hasNext(); ) {
@@ -336,7 +336,7 @@ public class PptSplitter implements Serializable {
           invs[0].log (invs[0] + " differs from "  + invs[1]);
         if (invs[1] != null)
           invs[1].log (invs[0] + " differs from "  + invs[1]);
-        debug.fine ("-- " + Invariant.toString (invs));
+        debug.fine ("-- " + invs[0] + " -- " + invs[1]);
       }
     }
 
@@ -375,225 +375,78 @@ public class PptSplitter implements Serializable {
       return;
     }
 
-
-    // Create array versions of each
-    Invariant[][] exclusive_invariants
-      = (Invariant[][])exclusive_invs_vec.toArray(new Invariant[0][0]);
-    Invariant[][] different_invariants
-      = (Invariant[][])different_invs_vec.toArray(new Invariant[0][0]);
-
-    // Add an implication from each of a pair of mutually exclusive
-    // invariants to everything that differs (at all) about the two
-
-    // split into two in order to use indexOf
-    Invariant[] excls1 = new Invariant[exclusive_invariants.length];
-    Invariant[] excls2 = new Invariant[exclusive_invariants.length];
-    for (int i=0; i<exclusive_invariants.length; i++) {
-      excls1[i] = exclusive_invariants[i][0];
-      excls2[i] = exclusive_invariants[i][1];
-    }
-
-    // Keep track of all of the implications created below
-    List imps = new ArrayList();
-
-    for (int i=0; i < exclusive_invariants.length; i++) {
-      Assert.assertTrue(exclusive_invariants[i].length == 2);
-      Invariant excl1 = exclusive_invariants[i][0];
-      Invariant excl2 = exclusive_invariants[i][1];
-      Assert.assertTrue(excl1 != null);
-      Assert.assertTrue(excl2 != null);
-
-      for (int j=0; j < different_invariants.length; j++) {
-        Assert.assertTrue(different_invariants[j].length == 2);
-        Invariant diff1 = different_invariants[j][0];
-        Invariant diff2 = different_invariants[j][1];
-
-        Assert.assertTrue((diff1 == null) || (diff2 == null)
-                          || (ArraysMDE.indexOf(excls1, diff1)
-                              == ArraysMDE.indexOf(excls2, diff2)));
-
-        // This adds an implication to itself; bad.
-        // If one of the diffs implies the other, then should not add
-        // an implication for the weaker one.
-        if (diff1 != null) {
-          int index1 = ArraysMDE.indexOf(excls1, diff1);
-          if ((index1 == -1) || (index1 > i)) {
-            boolean iff = (index1 != -1);
-            add_implication (parent, excl1, diff1, iff, orig_invs);
+    // Remove exclusive invariants from the different invariants list
+    // It would be better not to have added them in the first place,
+    // but this is easier for now.
+    for (Iterator ii = different_invs_vec.iterator(); ii.hasNext(); ) {
+      Invariant[] diff_invs = (Invariant[]) ii.next();
+      if (diff_invs[0] != null) {
+        Assert.assertTrue (diff_invs[1] == null);
+        // debug.fine ("Considering inv0 " + diff_invs[0]);
+        for (Iterator jj = exclusive_invs_vec.iterator(); jj.hasNext(); ) {
+          Invariant[] ex_invs = (Invariant[]) jj.next();
+          if (ex_invs[0] == diff_invs[0]) {
+            debug.fine ("removed exclusive invariant " + ex_invs[0]);
+            ii.remove();
+            break;
           }
         }
-        if (diff2 != null) {
-          int index2 = ArraysMDE.indexOf(excls2, diff2);
-          if ((index2 == -1) || (index2 > i)) {
-            boolean iff = (index2 != -1);
-            add_implication (parent, excl2, diff2, iff, orig_invs);
+      } else {
+        Assert.assertTrue (diff_invs[1] != null);
+        // debug.fine ("Considering inv1 " + diff_invs[1]);
+        for (Iterator jj = exclusive_invs_vec.iterator(); jj.hasNext(); ) {
+          Invariant[] ex_invs = (Invariant[]) jj.next();
+          if (ex_invs[1] == diff_invs[1]) {
+            debug.fine ("removed exclusive invariant " + ex_invs[1]);
+            ii.remove();
+            break;
           }
         }
       }
     }
 
-    if (debug.isLoggable (Level.FINE)) {
-      debug.fine (" Orig Joiner View ");
-      for (Iterator i = parent.joiner_view.invs.iterator(); i.hasNext(); )
-        debug.fine ("-- " + i.next());
-    }
-
-
-/// Does this work?
-/// ??? MDE
-    // Given bi-implications "A<=>B", "B<=>C", "C<=>D", etc., mark one of
-    // the equivalent implications as the canonical one, to avoid
-    // repeatedly printing the same fact (as both "A=>Z" and "B=>Z", for
-    // example).
-
-    HashMap /* Invariant->Invariant */ canonical_inv = new LinkedHashMap();
-    {
-      // The key is the canonical invariant for each element in the set.
-      // This information can be computed from canonical_inv.
-      HashMap /* Invariant -> HashSet[Invariant] */ inv_group = new LinkedHashMap();
-
-      // System.out.println(name + "implication canonicalization");
-      for (Iterator itor = parent.joiner_view.invs.iterator();
-                                                            itor.hasNext(); ) {
-        Invariant inv = (Invariant) itor.next();
-        if ((inv instanceof Implication) && ((Implication) inv).iff) {
-          Implication impl = (Implication) inv;
-          // System.out.println("Bi-implication: " + impl.format());
-          Invariant canon1 = (Invariant) canonical_inv.get(impl.predicate());
-          Invariant canon2 = (Invariant) canonical_inv.get(impl.consequent());
-          if ((canon1 != null) && (canon2 != null) && (canon1 != canon2)) {
-            // Move all the invariants for canon2 over to canon1
-            // This solves the potential problem: that I am not iterating
-            // through the invariants in any particular order that will
-            // guarantee that I don't see A and B, then C and D, and then A
-            // and C (which both already have different canonical
-            // versions).
-            HashSet hs1 = (HashSet) inv_group.get(canon1);
-            HashSet hs2 = (HashSet) inv_group.get(canon2);
-            inv_group.remove(canon2);
-            for (Iterator itor2=hs2.iterator(); itor2.hasNext(); ) {
-              Invariant inv2 = (Invariant) itor2.next();
-              hs1.add(inv2);
-              canonical_inv.put(inv2, canon1);
-            }
-            // System.out.print("Current set:");
-            // for (Iterator itor2=hs1.iterator(); itor2.hasNext(); ) {
-            //   Invariant inv2 = (Invariant) itor2.next();
-            //   System.out.print("    " + inv2.format());
-            // }
-            // System.out.println();
-          } else {
-            Invariant canon = (canon1 != null) ? canon1 : (canon2 != null) ? canon2 : impl.predicate();
-            // System.out.println("Canonical: " + canon.format());
-            canonical_inv.put(impl.predicate(), canon);
-            canonical_inv.put(impl.consequent(), canon);
-            HashSet hs = (HashSet) inv_group.get(canon);
-            if (hs == null) {
-              hs = new LinkedHashSet();
-              inv_group.put(canon, hs);
-            }
-            hs.add(impl.predicate());
-            hs.add(impl.consequent());
-            // System.out.print("Current set (2):");
-            // for (Iterator itor2=hs.iterator(); itor2.hasNext(); ) {
-            //   Invariant inv2 = (Invariant) itor2.next();
-            //   System.out.print("    " + inv2.format());
-            // }
-            // System.out.println();
-          }
-        }
-      }
-
-      // Now adjust which of the invariants are canonical.
-      // (That is why inv_group was computed above.)
-
-      for (Iterator itor=inv_group.keySet().iterator(); itor.hasNext(); ) {
-        Invariant canon_orig = (Invariant) itor.next();
-        // System.out.println("Outer loop: " + canon_orig.format());
-        HashSet hs = (HashSet) inv_group.get(canon_orig);
-        if (hs.size() == 1) {
-          continue;
-        }
-        Invariant canon_new = null;
-        String canon_new_formatted = null;
-        for (Iterator cand_itor=hs.iterator(); cand_itor.hasNext(); ) {
-          Invariant candidate = (Invariant) cand_itor.next();
-          String candidate_formatted = candidate.format();
-          // System.out.println("Comparing:" + lineSep + "    " + candidate_formatted); System.out.println("    " + canon_new_formatted);
-          // It is also desirable to be over the prestate;
-          // but that is only true for variables that are modified.
-          // A variable without "orig()" is fine if it's not modified.
-          boolean canon_new_undesirable
-            = ((canon_new == null) // avoid NullPointerException
-               || (canon_new_formatted.indexOf("\"null\"") != -1)
-               || (canon_new_formatted.indexOf("return") != -1));
-          boolean candidate_undesirable
-            = ((candidate_formatted.indexOf("\"null\"") != -1)
-               || (candidate_formatted.indexOf("return") != -1));
-          if ((canon_new == null)
-              || canon_new_undesirable
-              || ((! candidate_undesirable)
-                  && (candidate_formatted.length() < canon_new_formatted.length()))) {
-            canon_new = candidate;
-            canon_new_formatted = candidate_formatted;
-          }
-        }
-        if (canon_new != canon_orig) {
-          // Don't set inv_group, lest I get a ConcurrentModificationException
-          // inv_group.put(canon_new, hs);
-          // inv_group.remove(canon_orig);
-          for (Iterator inv_itor=hs.iterator(); inv_itor.hasNext(); ) {
-            Invariant inv = (Invariant) inv_itor.next();
-            Assert.assertTrue(canonical_inv.get(inv) == canon_orig);
-            canonical_inv.put(inv, canon_new);
-          }
-        }
-      }
-      // inv_group is no longer up-to-date now.
-      // I could have created an inv_group_2 during the above computation
-      // and set inv_group to it if I liked.
-    }
-
-    // Prune out implications over non-canonical invariants
-    // TODO: Why not avoid introducing them in the first place?
-
-    Vector to_remove = new Vector();
-    for (Iterator itor = parent.joiner_view.invs.iterator(); itor.hasNext(); ) {
-      Invariant inv = (Invariant) itor.next();
-      if (inv instanceof Implication) {
-        Implication impl = (Implication) inv;
-        Invariant cpred = (Invariant) canonical_inv.get(impl.predicate());
-        Invariant ccons = (Invariant) canonical_inv.get(impl.consequent());
-        boolean pred_non_canon = ((cpred != null)
-                                  && (impl.predicate() != cpred));
-        boolean cons_non_canon = ((ccons != null)
-                                  && (impl.consequent() != ccons));
-        if ((! impl.iff)
-            && (pred_non_canon || cons_non_canon)) {
-          // TODO: This is an inefficient call.  It would be better to avoid
-          // introducing the noncanonical invariants in the first place.
-          to_remove.add(inv);
+    // Get the canonical predicate invariants from the exclusive list.
+    // We pick the first one that is neither obvious or suppressed.
+    // If all are either obvious or suppressed, we just pick the first
+    // one in the list
+    Invariant[] con_invs = new Invariant[2];
+    for (Iterator ii = exclusive_invs_vec.iterator(); ii.hasNext(); ) {
+      Invariant[] invs = (Invariant[]) ii.next();
+      for (int jj = 0; jj < con_invs.length; jj++) {
+        if (con_invs[jj] == null) {
+          Invariant orig = (Invariant) orig_invs.get (invs[jj]);
+          if ((orig.isObvious() == null) && !orig.is_ni_suppressed())
+            con_invs[jj] = invs[jj];
         }
       }
     }
-
-    parent.joiner_view.removeInvariants(to_remove);
-
-    if (Debug.logOn() || debug.isLoggable (Level.FINE)) {
-      debug.fine ("Joiner View ");
-      for (Iterator i = parent.joiner_view.invs.iterator(); i.hasNext(); ) {
-        Implication imp = (Implication) i.next();
-        imp.log ("In joiner view");
-        debug.fine ("-- " + imp);
-      }
-      debug.fine ("Removed from Joiner View ");
-      for (Iterator i = to_remove.iterator(); i.hasNext(); ) {
-        Implication imp = (Implication) i.next();
-        imp.log ("removed from joiner view");
-        debug.fine ("-- " + imp);
+    Invariant[] first = (Invariant[]) exclusive_invs_vec.get(0);
+    for (int jj = 0; jj < con_invs.length; jj++) {
+      if (con_invs[jj] == null) {
+        System.out.println ("Warning: No non-obvious non-suppressed exclusive"
+                            + " invariants found in " + parent.name);
+        // Assert.assertTrue (false);
+        con_invs[jj] = first[jj];
       }
     }
 
+    // Create double-implications for each exclusive invariant
+    for (int ii = 0; ii < exclusive_invs_vec.size(); ii++) {
+      Invariant[] invs = (Invariant[]) exclusive_invs_vec.get(ii);
+      for (int jj = 0; jj < con_invs.length; jj++) {
+        if (con_invs[jj] != invs[jj])
+          add_implication (parent, con_invs[jj], invs[jj], true, orig_invs);
+      }
+    }
+
+    // Create single implication for each different invariant
+    for (int ii = 0; ii < different_invs_vec.size(); ii++) {
+      Invariant[] invs = (Invariant[]) different_invs_vec.get(ii);
+      for (int jj = 0; jj < con_invs.length; jj++) {
+        if (invs[jj] != null)
+          add_implication (parent, con_invs[jj], invs[jj], false, orig_invs);
+      }
+    }
 
   } // add_implications_pair
 
@@ -604,6 +457,8 @@ public class PptSplitter implements Serializable {
    * from its child conditionals.
    *
    * This is different from the slices that actually exist at the parent
+   * because there may be implications created from invariants in child
+   * slices that only exist in one child (and thus don't exists in the parent)
    * because there may be implications created from invariants in child
    * slices that only exist in one child.
    **/
@@ -684,10 +539,10 @@ public class PptSplitter implements Serializable {
         // // be few invariants in a slice, so breaking out is of minimal
         // // benefit.)
         // Assert.assertTrue(inv1.isExclusiveFormula(inv2)
-        //                   == inv2.isExclusiveFormula(inv1),
-        //               "Bad exclusivity: " + inv1.isExclusiveFormula(inv2)
-        //                + " " + inv2.isExclusiveFormula(inv1)
-        //                + "    " + inv1.format() + "    " + inv2.format());
+        //                  == inv2.isExclusiveFormula(inv1),
+        //              "Bad exclusivity: " + inv1.isExclusiveFormula(inv2)
+        //               + " " + inv2.isExclusiveFormula(inv1)
+        //               + "    " + inv1.format() + "    " + inv2.format());
         if (inv1.isExclusiveFormula(inv2)) {
           result.add(new Invariant[] { inv1, inv2 });
         }
@@ -758,26 +613,26 @@ public class PptSplitter implements Serializable {
                                Invariant consequent, boolean iff,
                                Map orig_invs) {
 
+    Assert.assertTrue (predicate != null);
+    Assert.assertTrue (consequent != null);
+
     Invariant orig_pred = (Invariant) orig_invs.get (predicate);
     Invariant orig_cons = (Invariant) orig_invs.get (consequent);
     Assert.assertTrue (orig_pred != null);
     Assert.assertTrue (orig_cons != null);
 
-    // JHP: If this code is enabled, other implications are added.  It
-    // is not at all clear to me how/why that happens.  This should be
-    // investigated.  For now, though, the implication is created here
-    // and noted as obvious in Implication.isObviousDynamically_SomeInEquality.
-    // Also, this could possibly be better implemented by changing the
-    // way that we create the list of invariants that is in one conditional
-    // and not in the other to not include an invariant if it is suppressed
-    // on the other side.  This would have the pleasant side effect of not
-    // forcing all of the suppressed invariants to be created before
+    // Don't add consequents that are obvious or suppressed.
+    // JHP: Jan 2005: It might be better to create them anyway and
+    // only suppress them in printing.  Also, this could possibly be
+    // better implemented by changing the way that we create the list
+    // of invariants that is in one conditional and not in the other
+    // to not include an invariant if it is suppressed on the other
+    // side.  This would have the pleasant side effect of not forcing
+    // all of the suppressed invariants to be created before
     // determining implications.
-    if (false && orig_cons.is_ni_suppressed()) {
-      if (Debug.logOn())
-        orig_cons.log ("not creating implication, suppressed");
+        if ((orig_cons.isObvious() != null) || orig_cons.is_ni_suppressed())
       return;
-    }
+
 
     // System.out.println("add_implication:");
     // System.out.println("  predicate = " + predicate.format());
