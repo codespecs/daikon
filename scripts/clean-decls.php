5770c45cc806e0589b678ef6d7d563d82d1ac99d
jhp <jhp@csail.mit.edu>
2005-02-05 18:52:29 -0500

    - Cleans out the header of decls files so they can be more easily diffed.

---
+++ scripts/clean-decls.php
@@ -0,0 +1,36 @@
+#! /usr/bin/env php
+<?php
+// clean-decls.php
+//
+// Reads from standard in and writes to standard out.  Decl header info
+// is removed so that diffs can work more cleanly.
+
+// main
+{
+  $STDIN = STDIN;
+
+  while (!feof ($STDIN)) {
+    $line = trim (fgets ($STDIN));
+    // echo "$line\n";
+    if (strpos ($line, "// Declarations for") === 0)
+      continue;
+    if (strpos ($line, "// Written") === 0) {
+      $line = fgets ($STDIN);
+      continue;
+    }
+    if ($line == "VarComparability") {
+      $line = fgets ($STDIN);
+      $line = fgets ($STDIN);
+      continue;
+    }
+
+    if ($line == "ListImplementors") {
+      while ($line != "")
+        $line = trim(fgets ($STDIN));
+      $line = trim (fgets ($STDIN));
+      continue;
+    }
+
+    printf ("%s\n", $line);
+  }
+}
