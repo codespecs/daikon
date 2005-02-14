9686f92b0e841121857cda190710b23ef8d80a87
daikonuser <devnull@localhost>
2005-02-13 23:55:30 -0500

    Change version to 4.0.1

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -40,8 +40,8 @@ public final class Daikon {
     throw new Error("do not instantiate");
   }
 
-  public final static String release_version = "4.0.0";
-  public final static String release_date = "February 7, 2005";
+  public final static String release_version = "4.0.1";
+  public final static String release_date = "February 13, 2005";
   public static final String release_string =
     "Daikon version "
       + release_version
