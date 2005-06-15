0b410f95a1162cb4919963acf35ce381562049d4
felly <devnull@localhost>
2005-06-15 14:35:14 -0400

    added comments

--- java/daikon/chicory/RootInfo.java
+++ java/daikon/chicory/RootInfo.java
@@ -3,12 +3,15 @@ package daikon.chicory;
 
 
 /**
- * 
+ *  This is a subtype of DaikonInfo and is used as a "placeholder" for the 
+ *  root of the tree.  It contains no variable information other than what is
+ *  stored in its children.
  */
 public class RootInfo extends DaikonInfo
 {
     public RootInfo()
     {
+		//the root needs no name
         super(null);
     }
     
