2ec05fda6faa54ec4a407669565bdffcb7215a8f
jhp <jhp@csail.mit.edu>
2005-07-28 11:12:47 -0400

    - Added the type of the return value   - Added process() method that adds all of the fields of the return     type as children to the ReturnInfo node

--- java/daikon/chicory/ReturnInfo.java
+++ java/daikon/chicory/ReturnInfo.java
@@ -8,14 +8,27 @@ package daikon.chicory;
  */
 public class ReturnInfo extends DaikonVariableInfo
 {
+    Class return_type = null;
+
     public ReturnInfo()
     {
         super("return");
     }
-    
+
+    public ReturnInfo (Class return_type)
+    {
+        super("return");
+        this.return_type = return_type;
+    }
+
     public Object getMyValFromParentVal(Object value)
     {
         throw new RuntimeException("Don't call getMyValFromParentVal on ReturnInfo objects");
     }
-    
+
+    /** Processes any children of the return and adds them to the tree **/
+    public void process (int depth)
+    {
+        process_children (return_type, depth, false);
+    }
 }
