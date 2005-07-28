7c2100846cdcbfa6ee7b6368f67029626b619944
jhp <jhp@csail.mit.edu>
2005-07-28 11:12:36 -0400

    - Reformatted comments for 80 columns   - Added the component type of the array as a field   - Added constructor that takes the component type   - Added process() method that adds all of the fields in the array     type as children to the ArrayInfo node

--- java/daikon/chicory/ArrayInfo.java
+++ java/daikon/chicory/ArrayInfo.java
@@ -1,12 +1,16 @@
 package daikon.chicory;
 
+import java.lang.reflect.Field;
 
 /**
- *  The ArrayInfo class is a subtype of DaikonVariableInfo used for variable types which are
- * arrays (ie, their name ends with "[]").
+ *  The ArrayInfo class is a subtype of DaikonVariableInfo used for
+ * variable types which are arrays (ie, their name ends with "[]").
  */
 public class ArrayInfo extends DaikonVariableInfo
 {
+    /** Component type of the array **/
+    Class array_type = null;
+
     /**
      * Constructs an ArrayInfo object with the specified name
      * @param theName The variable name. Should end with "[]"
@@ -14,6 +18,12 @@ public class ArrayInfo extends DaikonVariableInfo
     public ArrayInfo(String theName)
     {
         super(theName, true);
+    }
+
+    public ArrayInfo (String parent_name, Class array_type) {
+
+        super (parent_name + "[]", true);
+        this.array_type = array_type;
     }
 
     public Object getMyValFromParentVal(Object value)
@@ -31,4 +41,12 @@ public class ArrayInfo extends DaikonVariableInfo
         else
             return DTraceWriter.getListFromArray(value);
     }
+
+    /** Processes any children of this array and adds them to the tree **/
+    public void process (int depth)
+    {
+        process_children (array_type, depth, true);
+    }
+
+
 }
