605b5052375510b8bd008635039d5bc517ce7a50
jhp <jhp@csail.mit.edu>
2005-10-04 17:34:02 -0400

    - Add SeqIndexDisableAll (defaults to false).

--- java/daikon/inv/unary/sequence/SingleSequence.java
+++ java/daikon/inv/unary/sequence/SingleSequence.java
@@ -19,6 +19,15 @@ public abstract class SingleSequence
   // remove fields, you should change this number to the current date.
   static final long serialVersionUID = 20031024L;
 
+  /**
+   * Boolean.  Set to true to disable all SeqIndex invariants
+   * (SeqIndexIntEqual, SeqIndexFloatLessThan, etc).  This overrides the
+   * settings of the individual SeqIndex enable configuration options.
+   * To disable only some options, the options must be disabled
+   * individually.
+   */
+  public static boolean dkconfig_SeqIndexDisableAll = false;
+
   protected SingleSequence(PptSlice ppt) {
     super(ppt);
     // System.out.println("Created SingleSequence invariant " + this + " at " + ppt);
