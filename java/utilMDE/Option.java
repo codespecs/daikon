ff720017fb9fcc46432cb13eee6cf56da55c0ef8
jhp <jhp@csail.mit.edu>
2006-02-08 17:52:23 -0500

    - fixed typo

--- java/utilMDE/Option.java
+++ java/utilMDE/Option.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
  * is in the format '[-c] [type] description' where '-c' is a single
  * character short name for the option, 'type' is a description of the
  * option more specific than its java type (eg, filename) and 'description'
- * is a descriptin of the option suitable for a usage printout.
+ * is a description of the option suitable for a usage printout.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
