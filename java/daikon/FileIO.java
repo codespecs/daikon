0868ca4e8d31cce5cb5377c972cb88ca6f192f68
jaimeq <devnull@localhost>
2006-06-02 15:22:45 -0400

    Added warning messages in FileIO for when dataflow hierarchy is used on trace files that did not come from a programming language.

--- java/daikon/FileIO.java
+++ java/daikon/FileIO.java
@@ -789,7 +789,57 @@ public final class FileIO {
     }
 
     process_unmatched_procedure_entries();
+
+    warn_if_hierarchy_mismatch(all_ppts);
   }
+
+  // Determine if dataflow hierarchy should have been used, and print
+  // warning if this does not match Daikon.use_dataflow_hierarchy.
+  // Dataflow hierarchy should be used only when all program points
+  // correspond to points normally found in traces from a
+  // programming languages
+  private static void warn_if_hierarchy_mismatch(PptMap all_ppts) {
+    boolean all_program_points = true;
+
+    // go through each top level ppt, and make all_program_points
+    // false if at least one of them is not a program point normally
+    // found in traces from programming languages
+    for(Iterator<PptTopLevel> all_ppts_iter = all_ppts.ppt_all_iterator();
+        all_ppts_iter.hasNext(); ) {
+      PptTopLevel ppt_top_level = all_ppts_iter.next();
+      all_program_points =
+        all_program_points &&
+        (ppt_top_level.ppt_name.isExitPoint() ||
+         ppt_top_level.ppt_name.isEnterPoint() ||
+         ppt_top_level.ppt_name.isThrowsPoint() ||
+         ppt_top_level.ppt_name.isObjectInstanceSynthetic() ||
+         ppt_top_level.ppt_name.isClassStaticSynthetic() ||
+         ppt_top_level.ppt_name.isGlobalPoint());
+    }
+
+    // if all program points correspond to a programming language,
+    // but the dataflow hierarchy has been turned off, then
+    // suggest not using the --nohierarchy flag
+    if(all_program_points && (!Daikon.use_dataflow_hierarchy)) {
+      System.out.println("Warning: data trace appears to be over" +
+                         " a program execution, but dataflow" +
+                         " hierarchy has been turned off," +
+                         " consider running Daikon without the" +
+                         " --nohierarchy flag");
+    }
+
+    // if some of the program points do not correspond to a
+    // points from a programming language, and the dataflow
+    // hierarchy is being used, suggest using the --nohierarchy flag.
+    if(Daikon.use_dataflow_hierarchy && (!all_program_points)) {
+      System.out.println("Warning: Daikon is using a dataflow" +
+                         " hierarchy analysis on a data trace" +
+                         " that does not appear to be over a" +
+                         " program execution, consider running"+
+                         " Daikon with the --nohierarchy flag.");
+    }
+  }
+
 
   /**
    * Class used to specify the processor to use for sample data.  By
