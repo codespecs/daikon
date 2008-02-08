25ab873ac1b44841b27babe311c7599824da5454
carlos.pche <devnull@localhost>
2008-02-08 09:26:21 -0500

    Better documentation

--- java/asm/InstructionUtils.java
+++ java/asm/InstructionUtils.java
@@ -6,17 +6,54 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-
+/**
+ * Utility methods that operate on collections of instructions.
+ *
+ */
 public class InstructionUtils {
 
+  /**
+   * Computes a set of binary variables that are guaranteed to be redundant.
+   * It does this by inspecting the instructions along the path and finding
+   * binary variables associated with a register or dereference expression
+   * that is not killed by any instruction executed between the variable
+   * addresses.
+   * 
+   */
   public static Map<String, Set<String>> computeRedundantVars(List<IInstruction> path) {
 
+    // For the purposes of this code, we will say that an instruction occurs
+    // at a particular `time' where time is just the instruction's index in the path.
+    //
+    // Given a binary variable <address>:<name>, we refer to <name> as its
+    // `base name', and to <address>:<name> as its `full name'.
+    
+    // Maps base names to the last time the bv was killed. 
     Map<String, Integer> timeKilled = new LinkedHashMap<String, Integer>();
+    
+    // Maps base names to the last time a bv with the given base name
+    // was declared a leader.
     Map<String, Integer> leaders = new LinkedHashMap<String, Integer>();
+    
+    // Maps full names to the set of other full names that represent
+    // variables that are redundant to the key. We call the key the `leader'.
     Map<String, Set<String>> redundantVars = new LinkedHashMap<String, Set<String>>();
 
+    // For debugging only: the set of all addresses seen so far.
     Set<String> addresses = new LinkedHashSet<String>();
 
+    // We detect redundant variables as follows.
+    // For each bv var that appears in the LHS of instruction i (appearing
+    // at time t):
+    //
+    // if this is the first time we have seen var:
+    //     make t:var a leader
+    //     set its killed time to -1 (it's never been killed)
+    // else (we've seen it before)
+    //     if var was last killed after it was declared a leader
+    //       make it a leader
+    //     else
+    //       it's redundant. Add it to the appropriate redundant set.
     for (int time = 0; time < path.size(); time++) {
 
       IInstruction instr = path.get(time);
@@ -37,8 +74,6 @@ public class InstructionUtils {
 
           // If we've never seen this variable, make it a leader.
           if (!leaders.keySet().contains(var)) {
-            // System.out.println("NEW " + var + " ENTRYSET" +
-            // leaders.entrySet());
             leaders.put(var, time);
             timeKilled.put(var, -1);
             redundantVars.put(instrAddress + ":" + var,
