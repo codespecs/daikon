590539d7a8949ec5d20792489dd70ed05d2ccf1b
carlos.pche <devnull@localhost>
2008-02-08 08:57:43 -0500

    Added top-level comment.

--- java/asm/KillerInstruction.java
+++ java/asm/KillerInstruction.java
@@ -4,7 +4,14 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Set;
 
-
+/**
+ * Represents a set of instructions that may or may not be executed,
+ * and whose only effect is that they may kill one or more variables.
+ *
+ * A killer instruction is the abstraction we use to think of all
+ * the possible paths between a basic block and one of its dominators,
+ * and the effect of these paths on the state of the program.
+ */
 public class KillerInstruction implements IInstruction {
 
   private Collection<IInstruction> instructions;
