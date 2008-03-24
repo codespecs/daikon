c48dc17c2683c6f7cc002df23136bd217b72e00e
jhp <jhp@csail.mit.edu>
2008-03-24 14:45:42 -0400

    - Added comment for enable variable   - Fixed some other comments that referred to NonZero   - Removed format() and repr() methods.  These are better     inherited from Invariant.

--- java/daikon/inv/unary/scalar/IsPointer.java
+++ java/daikon/inv/unary/scalar/IsPointer.java
@@ -10,15 +10,15 @@ import daikon.inv.OutputFormat;
 import daikon.inv.ValueSet;
 
 /**
- * IsPointer is an invariant that heuristically determines whether 
- * an integer represents a pointer (a 32-bit memory address). 
- * Since both a 32-bit integer and an address have the same representation, 
+ * IsPointer is an invariant that heuristically determines whether
+ * an integer represents a pointer (a 32-bit memory address).
+ * Since both a 32-bit integer and an address have the same representation,
  * sometimes a a pointer can be mistaken for an integer. When this happens,
  * several scalar invariants are computed for integer variables. Most of them
  * would not make any sense for pointers. Determining whether a 32-bit variable
- * is a pointer can thus spare the computation of many irrelevant invariants. 
- * 
- * Jeff suggested the following heuristic: if all samples observed for an 
+ * is a pointer can thus spare the computation of many irrelevant invariants.
+ *
+ * Jeff suggested the following heuristic: if all samples observed for an
  * integer variable are greater than 100000 or smaller than -100000, then
  * it is likely that the integer variable represents a pointer (also,
  * we allow 0 as a valid value for a pointer). This heuristic was confirmed
@@ -31,7 +31,8 @@ public class IsPointer extends SingleScalar {
     private static IsPointer proto;
 
     private static final long serialVersionUID = 20080221L;
-    
+
+    /** Boolean. True iff IsPointer invariants should be considered **/
     public static boolean dkconfig_enabled = false;
 
     // pointers values in ff_prepare.dtrace were starting from 65536,
@@ -42,11 +43,11 @@ public class IsPointer extends SingleScalar {
 
     protected IsPointer(PptSlice ppt) {
         super(ppt);
-        
-    }
-    
 
-    /** Returns the prototype invariant for NonZero **/
+    }
+
+
+    /** Returns the prototype invariant for IsPointer **/
     public static Invariant get_proto() {
       if (proto == null)
         proto = new IsPointer(null);
@@ -55,25 +56,25 @@ public class IsPointer extends SingleScalar {
 
     @Override
     protected Invariant instantiate_dyn(PptSlice slice) {
-        return new IsPointer(slice); 
+        return new IsPointer(slice);
     }
-    
+
     @Override
     public boolean enabled() {
         return dkconfig_enabled;
-    } 
-    
+    }
+
     @Override
     public boolean instantiate_ok(VarInfo[] vis) {
         if (!super.valid_types(vis))
             return false;
 
         ProglangType file_rep_type = vis[0].file_rep_type;
-        
+
         return (file_rep_type == ProglangType.INT);
     }
 
-    
+
     @Override
     public InvariantStatus add_modified(long value, int count) {
         return check_modified(value, count);
@@ -85,31 +86,20 @@ public class IsPointer extends SingleScalar {
             return InvariantStatus.FALSIFIED;
         return InvariantStatus.NO_CHANGE;
       }
-    
+
     private boolean isWithinPointerRange(long value) {
         if (value == 0)
             return true;
-        return (value >= largestNonPointerValue) || (value <= smallestNonPointerValue); 
+        return (value >= largestNonPointerValue) || (value <= smallestNonPointerValue);
     }
 
-    
-    @Override
-    public String format() {
-        String name = var().jml_name();
-        ValueSet.ValueSetScalar vs = (ValueSet.ValueSetScalar) ppt.var_infos[0].get_value_set();
-        return name + " isPointer"; 
-    }
-    
-    @Override
-    public String repr() {
-        return format();
-    }
-    
+
     @Override
     public String format_using(OutputFormat format) {
-        return format();
+      String varname = var().name_using (format);
+      return varname + "  isPointer";
     }
-    
+
     protected double computeConfidence() {
         return 1 - computeProbability();
     }
