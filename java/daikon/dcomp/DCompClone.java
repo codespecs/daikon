e5d2ca8322d6e0f5ee14f73dfcde0fba1fdfcbc3
jhp <jhp@csail.mit.edu>
2008-06-06 16:54:23 -0400

    - Interface that indicates that the class has an instrumented clone method.

---
+++ java/daikon/dcomp/DCompClone.java
@@ -0,0 +1,8 @@
+package daikon.dcomp;
+
+/**
+ * Classes implementing this interface have been instrumented by DynComp
+ * and have implemented a clone method.
+ */
+public interface DCompClone {
+}
