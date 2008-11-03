225894adcec989a1b87cf1bbbb81e82d6123b333
jhp <jhp@csail.mit.edu>
2008-11-03 16:01:56 -0500

    - Printout the linenumber of the actual log call if we are not showing     the traceback

--- java/daikon/LogHelper.java
+++ java/daikon/LogHelper.java
@@ -94,11 +94,25 @@ public final class LogHelper {
         loggerName = loggerName + padding_arrays[20 - len];
       }
 
-      // If we aren't generating tracebacks, include the src class/method
+      // If we aren't generating tracebacks, find the src class/method/line
+      // where the log was called from
       String src = "";
-      if (!Debug.dkconfig_showTraceback)
-        src = record.getSourceClassName().replaceAll ("\\w*\\.", "")
-                + "." + record.getSourceMethodName() + ": ";
+      if (!Debug.dkconfig_showTraceback) {
+        Throwable stack = new Throwable("debug traceback");
+        stack.fillInStackTrace();
+        StackTraceElement[] ste_arr = stack.getStackTrace();
+        for (int ii = ste_arr.length-1; ii >= 0; ii--) {
+          StackTraceElement ste = ste_arr[ii];
+          if (ste.getClassName().startsWith ("java") ||
+              ste.getClassName().contains ("daikon.LogHelper") ||
+              ste.getMethodName().equals ("log") ||
+              ste.getClassName().contains ("daikon.Debug"))
+            continue;
+          src = ste.getFileName() + " " + ste.getLineNumber() + ": ";
+        }
+      }
+        // src = record.getSourceClassName().replaceAll ("\\w*\\.", "")
+        //        + "." + record.getSourceMethodName() + ": ";
 
       return "@ " + loggerName + " " + src + record.getMessage()
              + Global.lineSep;
