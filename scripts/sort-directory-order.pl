dba2ddadcfb1689c5316fd910b54605ffcd2f92d
michael.ernst <devnull@localhost>
2009-06-14 02:33:52 -0400

    Improve documentation.

--- scripts/sort-directory-order.pl
+++ scripts/sort-directory-order.pl
@@ -1,13 +1,46 @@
 #!/usr/bin/env perl
 
 ## Sorts the input lines by directory order:
-##   everything in a given directory first, then subdirectories one after another
+##   first, every file in a given directory, in sorted order
+##   then, process subdirectories recursively, in sorted order
+## Example output order:
+##   annotatenullable.txt
+##   chicory.txt
+##   varinfoaux.txt
+##   varinfoname.txt
+##   asm/asmfile.txt
+##   asm/dsforest.txt
+##   asm/pptfile.txt
+##   asm/testredundantvars.txt
+##   asm/x86instruction.txt
+##   chicory/arrayinfo.txt
+##   chicory/chicorypremain.txt
+##   derive/derivation.txt
+##   derive/derivationfactory.txt
+##   derive/valueandmodified.txt
+##   derive/binary/binaryderivation.txt
+##   derive/binary/binaryderivationfactory.txt
+##   derive/binary/sequencespredicatefactoryfloat.txt
+##   derive/binary/sequencespredicatefloat.txt
+##   derive/ternary/sequencefloatarbitrarysubsequence.txt
+##   derive/ternary/sequencefloatarbitrarysubsequencefactory.txt
+##   derive/ternary/ternaryderivation.txt
+##   derive/ternary/ternaryderivationfactory.txt
+##   derive/unary/sequenceinitial.txt
+##   derive/unary/sequenceinitialfactory.txt
+##   derive/unary/unaryderivation.txt
+##   derive/unary/unaryderivationfactory.txt
 
-## This can be used with "find" to put the results in a more sensible order
-## for use in TAGS files, etc.:  instead of
-##   etags `findfile '*.java'`
+## This can be used with "find" to put the results in sorted order.
+## (find's default order has to do with where files happen to be stored on
+## disk.)  Sorted order may be more sensible to a user, or may make other
+## commands more deterministic.  (But for determinism alone, you might as
+## well just use "sort", which is faster than this script.)
+##
+## For example, when making a TAGS file, instead of
+##   etags `find . -iname '*.java' -print`
 ## do
-##   etags `findfile '*.java' | sort-directory-order.pl`
+##   etags `find . -iname '*.java' -print | sort-directory-order.pl`
 
 ## Mac OS X has a "-s" option to "find" that is similar (but not
 ## necessarily identical) to "find | sort".
