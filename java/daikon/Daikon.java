4b625818cc26ebf4c5e449362e6afc404ca55cb0
daikonuser <devnull@localhost>
2009-12-20 23:08:49 -0500

    Update version number (why wasn't this committed earlier?)

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -79,8 +79,8 @@ public final class Daikon {
 
   // Don't change the order of the modifiers on these strings as they
   // are automatically updated as part of the release process
-  public final static String release_version = "4.6.2";
-  public final static String release_date = "October 1, 2009";
+  public final static String release_version = "4.6.3";
+  public final static String release_date = "December 18, 2009";
   public final static String release_string =
     "Daikon version "
       + release_version
