4843d345789308da00919560c7f8ac88a56599e2
jhp <jhp@csail.mit.edu>
2010-04-15 11:26:26 -0400

    Always use 1024M of memory for instrumented program

--- java/daikon/DynComp.java
+++ java/daikon/DynComp.java
@@ -292,7 +292,7 @@ public class DynComp {
     cmdlist.add (cp);
     cmdlist.add ("-ea");
     cmdlist.add ("-esa");
-    cmdlist.add ("-Xmx1024");
+    cmdlist.add ("-Xmx1024m");
     if (!no_jdk)
       cmdlist.add ("-Xbootclasspath:" + rt_file + separator + cp);
 
