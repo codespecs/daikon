031dfcfa5f185a63d899da3a41847481de9d62c4
Michael Ernst <mernst@cs.washington.edu>
2012-09-07 20:17:25 -0700

    Make VIndexBottom the default for null and int literals

--- java/typequals/VIndexBottom.java
+++ java/typequals/VIndexBottom.java
@@ -3,7 +3,10 @@ package typequals;
 import java.lang.annotation.*;
 import checkers.quals.*;
 
+import com.sun.source.tree.Tree;
+
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @TypeQualifier
 @SubtypeOf({ValueIndex.class, VarIndex.class, VIndexUnqualified.class})
+@ImplicitFor(trees={Tree.Kind.NULL_LITERAL, Tree.Kind.INT_LITERAL})
 public @interface VIndexBottom {}
