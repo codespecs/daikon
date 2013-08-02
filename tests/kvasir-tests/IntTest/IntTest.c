47654153ceb6ba356dcd1b1a9adb1b09a98875d4
Mark Roberts <markro@cs.washington.edu>
2013-08-02 08:46:25 -0700

    update another goal file

--- tests/kvasir-tests/IntTest/IntTest.c
+++ tests/kvasir-tests/IntTest/IntTest.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 
 int globalX = 5;
 char* whatsUp = "what's up dawg!!!";
 
-int globalIntArray[18] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+//int globalIntArray[18] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};
 
 char**** returnIntSum(int* u, float* a, char* str, char*** dummy, double** b, int uninit)
 {
-  int c = **b;
-  int foo = uninit;
-  int bar = *a;
-  **b *= 2;
-
-  *a = 2.718;
-
-  if (u)
-    returnIntSum(0, a, str + 3, dummy, b, uninit);
-  else
-    return 0xDEADDEAD;
+  //int c = **b;
+  //int foo = uninit;
+  //int bar = *a;
+  //**b *= 2;
+//
+  //*a = 2.718;
+//
+  //if (u)
+    //returnIntSum(0, a, str + 3, dummy, b, uninit);
+  //else
+    //return 0xDEADDEAD;
 
   return 0xABBABEEF;
 }
@@ -29,13 +29,13 @@ int main()
   double x;
   double* xPtr = &x;
   char** ridiculous = &whatsUp;
-  int* undef = (int*)(0xDEADBEEF);
-  char* uninitString = (char*)(0xABBADABA);
+  //int* undef = (int*)(0xDEADBEEF);
+  //char* uninitString = (char*)(0xABBADABA);
   int uninit;
-  int init = 5;
+  //int init = 5;
   int stackArray13[13] = {13,12,11,10,9,8,6,5,4,3}; // Only 10 elts. init
-  int* intArray = (int*)calloc(8, sizeof(int));
-  int* intArrayUninit = (int*)malloc(38*sizeof(int));
+  //int* intArray = (int*)calloc(8, sizeof(int));
+  //int* intArrayUninit = (int*)malloc(38*sizeof(int));
 
   //  printf("&globalX=%p\n", &globalX);
   //  printf("&&whatsUp=%p, &whatsUp=%p, whatsUp=%p\n", &ridiculous, &whatsUp, whatsUp);
