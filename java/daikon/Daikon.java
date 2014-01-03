d160e888a771b044adf3cb3ce8b659d1827c50a1
Mark Roberts <markro@cs.washington.edu>
2014-01-03 09:13:23 -0800

    Update release version number and date.

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -79,8 +79,8 @@ public final class Daikon {
 
   // Don't change the order of the modifiers on these strings as they
   // are automatically updated as part of the release process
-  public final static String release_version = "4.7.5";
-  public final static String release_date = "December 12, 2013";
+  public final static String release_version = "4.7.7";
+  public final static String release_date = "January 2, 2014";
   public final static String release_string =
     "Daikon version "
       + release_version
