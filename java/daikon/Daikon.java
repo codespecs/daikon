336b007d7b4ea95c6926d30ec1ed70ec2713d70f
Michael Ernst <mernst@cs.washington.edu>
2015-03-12 14:58:01 -0300

    Fix trivial typo
    Former-commit-id: f34b769f25dfd3a62d65473a51f51bd3b9979f02

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -148,7 +148,7 @@ public final class Daikon {
   // Problem with setting this to true:
   //  get no invariants over any value that can ever be missing
   // Problem with setting this to false:
-  //  due to differrent number of samples, IsEqualityComparison is
+  //  due to different number of samples, IsEqualityComparison is
   //  non-transitive (that is specially handled in the code)
   public static final boolean invariants_check_canBeMissing = false;
 
