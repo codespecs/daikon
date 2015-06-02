32d3a18c0b768284d80d32f66259b68978bd4030
Mark Roberts <markro@cs.washington.edu>
2015-06-02 14:42:02 -0700

    Bump version number for ongoing development.
    Former-commit-id: d95857b3ae3c536e043343d333814ed37e294016

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -85,7 +85,7 @@ public final class Daikon {
 
   // Don't change the order of the modifiers on these strings as they
   // are automatically updated as part of the release process
-  public final static String release_version = "5.2.6";
+  public final static String release_version = "5.2.7";
   public final static String release_date = "June 2, 2015";
   public final static String release_string =
     "Daikon version "
