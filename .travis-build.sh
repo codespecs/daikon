adab8bff97abaf716ca6d5f1a57d83034d9bbcaa
Michael Ernst <mernst@cs.washington.edu>
2015-11-12 09:02:41 -0800

    Add Travis CI files

---
+++ .travis-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ROOT=$TRAVIS_BUILD_DIR/..
+cd $ROOT
+
+# Same as in Jenkins; should abstract out
+make all-dist
+make -C java very-clean
+make showvars compile javadoc
+make -C doc
+make daikon.jar
+make -C java dcomp_rt.jar
+make -C tests clean all
