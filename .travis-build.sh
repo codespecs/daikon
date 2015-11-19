b38fbfb6b0cb2d84c92ee1b7dce324abe0d85808
Mark Roberts <markro@cs.washington.edu>
2015-11-19 09:49:07 -0800

    debugging - run a single test

--- .travis-build.sh
+++ .travis-build.sh
@@ -5,7 +5,15 @@
 
 # Same as in Jenkins; should abstract out
 # make -C java very-clean
-make showvars compile daikon.jar javadoc
-make -C doc
+
+# temp changes while debugging
+
+# make showvars compile daikon.jar javadoc
+make showvars compile daikon.jar
+# make -C doc
 make -C java dcomp_rt.jar
-make -C tests all
+# make -C tests all
+cd tests/daikon-tests/suppress02
+make diffs
+
+/usr/lib/jvm/java-8-oracle/bin/java -ea -Xmx1500m -cp ".:/usr/lib/jvm/java-8-oracle/jre/lib/rt.jar:/usr/lib/jvm/java-8-oracle/lib/tools.jar:/tmp/travis/tests/daikon-tests/suppress02:/home/travis/build/codespecs/daikon/java:/home/travis/build/codespecs/daikon/java/lib/java-getopt.jar:/home/travis/build/codespecs/daikon/java/lib/plume.jar"    daikon.Daikon -o /home/travis/build/codespecs/daikon/tests/daikon-tests/suppress02/Suppress02.spinfo.inv.gz --config config.txt --config_option daikon.split.PptSplitter.suppressSplitterErrors=false --no_text_output /tmp/travis/tests/daikon-tests/suppress02/Suppress02-dyncomp.dtrace.gz Suppress02.spinfo-static
