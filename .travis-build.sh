05287cd05141ed124a7f1d5e27e18898fc98a529
Michael Ernst <mernst@cs.washington.edu>
2015-12-09 20:47:13 -0800

    Set TEXI2HTML environment variable

--- .travis-build.sh
+++ .travis-build.sh
@@ -2,5 +2,8 @@
 
 # TODO: The tests ought to work even if $DAIKONDIR is not set.
 export DAIKONDIR=`pwd`
+# On Travis-CI's Ubuntu 12.04 infrastructure, makeinfo does not take the
+# --split=chapter command-line argument but texi2html does.
+export TEXI2HTML=texi2html
 
 make nightly-test
