42e57ae1d9b5e8c25912d9411b6d67bebf3bc92a
Mark Roberts <markro@cs.washington.edu>
2016-01-25 12:23:46 -0800

    increase timeout to 20 seconds for compiling splitters

--- java/daikon/split/SplitterFactory.java
+++ java/daikon/split/SplitterFactory.java
@@ -65,7 +65,7 @@ public class SplitterFactory {
    * seconds, after which the compilation process is terminated and
    * retried, on the assumption that it has hung.
    **/
-  public static int dkconfig_compile_timeout = 10;
+  public static int dkconfig_compile_timeout = 20;
 
   private static /*@MonotonicNonNull*/ FileCompiler fileCompiler; // lazily initialized
 
