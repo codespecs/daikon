20effc3795a33fb6d0207403e5cd291436b0045e
Mark Roberts <markro@cs.washington.edu>
2016-04-05 13:39:22 -0700

    Bump version number for ongoing development.

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -86,7 +86,7 @@ public final class Daikon {
 
   // Don't change the order of the modifiers on these strings as they
   // are automatically updated as part of the release process
-  public final static String release_version = "5.3.0";
+  public final static String release_version = "5.3.1";
   public final static String release_date = "April 5, 2016";
   public final static String release_string =
     "Daikon version "
