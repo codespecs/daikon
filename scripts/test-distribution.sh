ee8b22f1585827c87bdecec63d33e1baf6766150
Michael Ernst <mernst@cs.washington.edu>
2016-04-07 09:19:21 -0700

    Script to test the Daikon distribution

---
+++ scripts/test-distribution.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Download the distribution and run "make distribution-check".
+
+
+# Fail the whole script if any command fails
+set -e
+
+# Convert OSTYPE from a shell variable to a system environment variable.
+export OSTYPE
+
+mkdir -p ~/tmp
+cd ~/tmp
+
+# DAIKONVERSION=`wget -q http://plse.cs.washington.edu/staging-daikon/download/doc/VERSION -O - | xargs echo -n`
+DAIKONVERSION=`curl --fail -s http://plse.cs.washington.edu/staging-daikon/download/doc/VERSION | xargs echo -n`
+
+rm -rf daikon-$DAIKONVERSION.tar.gz daikon-$DAIKONVERSION
+# wget http://plse.cs.washington.edu/staging-daikon/download/daikon-$DAIKONVERSION.tar.gz
+curl --fail -O http://plse.cs.washington.edu/staging-daikon/download/daikon-$DAIKONVERSION.tar.gz
+
+tar xzf daikon-$DAIKONVERSION.tar.gz
+cd daikon-$DAIKONVERSION
+export DAIKONDIR=`pwd`
+source scripts/daikon.bashrc
+make distribution-check
