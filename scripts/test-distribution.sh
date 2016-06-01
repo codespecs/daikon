7263a18274e170754b63bc3ce1028e229028f39a
Michael Ernst <mernst@cs.washington.edu>
2016-06-01 16:59:33 -0700

    Set pipefail option

--- scripts/test-distribution.sh
+++ scripts/test-distribution.sh
@@ -8,6 +8,8 @@ if [ -z "${JAVA_HOME+xxx}" ]; then echo JAVA_HOME is not set; exit 1; fi
 
 # Fail the whole script if any command fails
 set -e
+# Fail if any command in a pipeline fails
+set -o pipefail
 # Echo commands before executing them
 set -x
 
