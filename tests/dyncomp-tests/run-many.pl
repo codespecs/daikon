150a5ba5894d38b74dc7ce1490f0fd75339a1060
Mark Roberts <markro@cs.washington.edu>
2016-07-20 15:00:48 -0700

    correct paths for invoking java

--- tests/dyncomp-tests/run-many.pl
+++ tests/dyncomp-tests/run-many.pl
@@ -1,4 +1,15 @@
 #!/usr/bin/perl
+
+# run-many.pl
+# This script is used to run fjalar mutiltiple times
+# for a single test scenario.  It assumes that arg0
+# is a script that will invoke fjalar and that STDIN
+# has been redirected to a file where each line is
+# a set of arguments to be passed to the arg0 script.
+# In practice, one adds the line:
+#   TESTS_SCRIPT := <name of file containing args to fjalar>
+# to the scenario Makefile and the rest is automatic.
+
 my $kvasir = $ARGV[0];
 my $kvasir_short = $kvasir;
 $kvasir_short =~ s!^.*/(.*)$!$1!;
