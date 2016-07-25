c7b2b988949c9f53aebad07306eaa9aa60273a65
Mark Roberts <markro@cs.washington.edu>
2016-07-25 12:24:59 -0700

    remove unused static int - gcc 6.1 now ignores them

--- tests/kvasir-tests/static-struct/second-file.c
+++ tests/kvasir-tests/static-struct/second-file.c
@@ -14,7 +14,6 @@ static int second_int_array[5] = {1, 1, 2, 3, 5};
 int* second_examine_int(int* int_array, int* numPtr);
 
 void second_examine(struct desc *p) {
-  static int count = 100;
   second_examine_int(second_int_array, &second_int);
 }
 
