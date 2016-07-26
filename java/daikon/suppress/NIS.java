03cad05f5b4d1aee81ad70e7cca312c9db58ceb5
Suzanne Millstein <smillst@cs.washington.edu>
2016-07-26 10:59:58 -0700

    Work around for typetools/checker-framework#764

--- java/daikon/suppress/NIS.java
+++ java/daikon/suppress/NIS.java
@@ -874,7 +874,9 @@ public class NIS {
   /**
    * Returns true if the specified class is an antecedent in any NI suppression
    */
-  /*@RequiresNonNull("suppressor_map")*/
+  //Use class name in front of static field to work around Checker Framework issue 764
+  // https://github.com/typetools/checker-framework/issues/764
+  /*@RequiresNonNull("NIS.suppressor_map")*/
   /*@Pure*/
   public static boolean is_suppressor(Class<? extends Invariant> cls) {
     return (suppressor_map.containsKey(cls));
