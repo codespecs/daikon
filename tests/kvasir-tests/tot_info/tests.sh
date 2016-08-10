7c3494d30403a1f00ae87628a428c0e28f225cef
Mark Roberts <markro@cs.washington.edu>
2016-08-10 14:12:54 -0700

    update for changed command line argument name

--- tests/kvasir-tests/tot_info/tests.sh
+++ tests/kvasir-tests/tot_info/tests.sh
@@ -1,2 +1,2 @@
 --decls-file=daikon-output/tot_info.decls ./tot_info < tst45
---decls-only --with-dyncomp --decls-file=daikon-output/tot_info.dyncomp.decls --no-dyncomp-gc ./tot_info < tst45
+--decls-only --with-dyncomp --decls-file=daikon-output/tot_info.dyncomp.decls --dyncomp-gc-num-tags=0 ./tot_info < tst45
