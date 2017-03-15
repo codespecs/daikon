fc13aad108dd1de799e1bc7811fc40a09e3b430d
Michael Ernst <mernst@cs.washington.edu>
2017-03-15 01:17:39 -0700

    Accommodate change in modifier order

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -78,8 +78,6 @@ public final class Daikon {
    */
   public static int dkconfig_progress_delay = 1000;
 
-  // Don't change the order of the modifiers on these strings as they
-  // are automatically updated as part of the release process
   public static final String release_version = "5.5.x";
   public static final String release_date = "January 5, 2017";
   public static final String release_string =
