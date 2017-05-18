8b3164856bccc30be07fa599f7a910d7e9978bf1
Michael Ernst <mernst@cs.washington.edu>
2017-05-18 16:22:34 -0700

    Add test for utility class

---
+++ java/daikon/test/config/HtmlToTexinfoTest.java
@@ -0,0 +1,46 @@
+package daikon.test.config;
+
+import daikon.config.*;
+import junit.framework.*;
+
+public class HtmlToTexinfoTest extends TestCase {
+
+  public static void main(String[] args) {
+    daikon.LogHelper.setupLogs(daikon.LogHelper.INFO);
+    junit.textui.TestRunner.run(new TestSuite(HtmlToTexinfoTest.class));
+  }
+
+  public HtmlToTexinfoTest(String name) {
+    super(name);
+  }
+
+  public void testHtmlToTexinfo() {
+    testHtmlToTexinfo("some &lt;text&gt; &amp; I like {it}", "some <text> & I like @{it@}");
+    testHtmlToTexinfo(
+        "The <tt>--conf_limit</tt> command-line option to Daikon",
+        "The @code{--conf_limit} command-line option to Daikon");
+  }
+
+  private void testHtmlToTexinfo(String in, String expected) {
+    String actual = HtmlToTexinfo.htmlToTexinfo(in);
+    assert actual.equals(expected) : "Expected " + expected + ", got " + actual;
+  }
+
+  public void testJavadocHtmlToTexinfo() {
+    // Same as for testHtmlToTexinfo
+    testJavadocHtmlToTexinfo("some &lt;text&gt; &amp; I like {it}", "some <text> & I like @{it@}");
+    testJavadocHtmlToTexinfo(
+        "The <tt>--conf_limit</tt> command-line option to Daikon",
+        "The @code{--conf_limit} command-line option to Daikon");
+
+    // Additional test casess
+    testJavadocHtmlToTexinfo(
+        "Split bi-implications {@code a <==> b} into two separate implications {@code a ==> b} and {@code b ==> a})",
+        "Split bi-implications @code{a <==> b} into two separate implications @code{a ==> b} and @code{b ==> a})");
+  }
+
+  private void testJavadocHtmlToTexinfo(String in, String expected) {
+    String actual = HtmlToTexinfo.javadocHtmlToTexinfo(in);
+    assert actual.equals(expected) : "Expected " + expected + ", got " + actual;
+  }
+}
