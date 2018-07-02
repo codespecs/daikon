84a0c26669e97adca4d14074426339bc03bdfe02
Mark Roberts <markro@cs.washington.edu>
2018-07-02 13:44:30 -0700

    Change version to 5.6.6

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -77,8 +77,8 @@ public final class Daikon {
    */
   public static int dkconfig_progress_delay = 1000;
 
-  public static final String release_version = "5.6.5";
-  public static final String release_date = "April 3, 2018";
+  public static final String release_version = "5.6.6";
+  public static final String release_date = "July 2, 2018";
   public static final String release_string =
       "Daikon version "
           + release_version
