690f91e35522c994444bc610bcbde2baaf505b0a
Michael Ernst <mernst@alum.mit.edu>
2018-08-16 12:48:59 -0700

    Re-enable requireJavadoc

--- .travis-build.sh
+++ .travis-build.sh
@@ -64,12 +64,11 @@ if [[ "${GROUP}" == "misc" || "${GROUP}" == "all" ]]; then
   # Documentation
   make javadoc doc-all
 
-## TEMPORARILY disable, for this commit only
-#   (git diff "${TRAVIS_COMMIT_RANGE/.../..}" > /tmp/diff.txt 2>&1) || true
-#   (make -C java requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
-#   [ -s /tmp/diff.txt ] || (echo "/tmp/diff.txt is empty" && false)
-#   wget https://raw.githubusercontent.com/plume-lib/plume-scripts/master/lint-diff.py
-#   python lint-diff.py --strip-diff=2 --strip-lint=1 /tmp/diff.txt /tmp/warnings.txt
+  (git diff "${TRAVIS_COMMIT_RANGE/.../..}" > /tmp/diff.txt 2>&1) || true
+  (make -C java requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
+  [ -s /tmp/diff.txt ] || (echo "/tmp/diff.txt is empty" && false)
+  wget https://raw.githubusercontent.com/plume-lib/plume-scripts/master/lint-diff.py
+  python lint-diff.py --strip-diff=2 --strip-lint=1 /tmp/diff.txt /tmp/warnings.txt
 fi
 
 if [[ "${GROUP}" == "kvasir" || "${GROUP}" == "all" ]]; then
