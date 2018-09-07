246d3bf4da5802d5ee7718faefd4938c112e3c3c
Michael Ernst <mernst@cs.washington.edu>
2018-09-06 17:27:47 -0700

    Permit empty diffs in some situations [ci skip]

--- .travis-build.sh
+++ .travis-build.sh
@@ -66,7 +66,7 @@ if [[ "${GROUP}" == "misc" || "${GROUP}" == "all" ]]; then
 
   (git diff "${TRAVIS_COMMIT_RANGE/.../..}" > /tmp/diff.txt 2>&1) || true
   (make -C java requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
-  [ -s /tmp/diff.txt ] || (echo "/tmp/diff.txt is empty" && false)
+  [ -s /tmp/diff.txt ] || ([[ "${TRAVIS_BRANCH}" != "master" && "${TRAVIS_EVENT_TYPE}" == "push" ]] || (echo "/tmp/diff.txt is empty" && false))
   wget https://raw.githubusercontent.com/plume-lib/plume-scripts/master/lint-diff.py
   python lint-diff.py --strip-diff=2 --strip-lint=1 /tmp/diff.txt /tmp/warnings.txt
 fi
