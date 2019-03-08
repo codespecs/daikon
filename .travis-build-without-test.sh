8040e6aa9f585a3321107aa9949264915d7a4f13
Mark Roberts <markro@cs.washington.edu>
2019-03-08 11:25:15 -0800

    another try at getting os info

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -4,7 +4,9 @@
 set -e
 
 # get linux version details
-lsb_release -a
+lsb_release -a || true
+# quick and dirty way to get info and avoid python 2/3 problems with lsb_release
+cat /etc/*release || true
 
 export SHELLOPTS
 
