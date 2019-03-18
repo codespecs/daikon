30f47f56729caf9a7f7350f497b986aedf6d6b4f
Mark Roberts <markro@cs.washington.edu>
2019-03-18 16:46:32 -0700

    remove duplicate version debug code

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -3,9 +3,7 @@
 # Fail the whole script if any command fails
 set -e
 
-# get linux version details
-/usr/bin/lsb_release -a || true
-# quick and dirty way to get info and avoid python 2/3 problems with lsb_release
+# quick and dirty way to get version info and avoid python 2/3 problems with lsb_release
 cat /etc/*release || true
 
 export SHELLOPTS
