5fa0e61f5abadfe56daf6141a85a4ddc99fb00c5
Suzanne Millstein <smillst@cs.washington.edu>
2019-07-02 09:51:05 -0700

    Use 2.9.0 version of checker-qual.jar

--- java/typequals/vindex/qual/VIndexBottom.java
+++ java/typequals/vindex/qual/VIndexBottom.java
@@ -2,11 +2,11 @@ package typequals.vindex.qual;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
-import org.checkerframework.framework.qual.ImplicitFor;
 import org.checkerframework.framework.qual.LiteralKind;
+import org.checkerframework.framework.qual.QualifierForLiterals;
 import org.checkerframework.framework.qual.SubtypeOf;
 
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @SubtypeOf({ValueIndex.class, VarIndex.class, VIndexUnqualified.class})
-@ImplicitFor(literals = {LiteralKind.NULL, LiteralKind.INT})
+@QualifierForLiterals(LiteralKind.INT)
 public @interface VIndexBottom {}
