ef06dc610f60633e3e08f5d55a7255d1c89c9e16
Michael Ernst <mernst@cs.washington.edu>
2019-07-10 13:49:47 -0700

    Tweak comment

--- .travis-build.sh
+++ .travis-build.sh
@@ -67,7 +67,7 @@ if [[ "${GROUP}" == "misc" || "${GROUP}" == "all" ]]; then
   git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
     || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
   eval `/tmp/plume-scripts/ci-info DEFAULT-ORGANIZATION`
-  # if [ -n "$CI_IS_PR" ] ; then
+  # if [ "$CI_IS_PR" == "true" ] ; then
   (git diff $CI_COMMIT_RANGE > /tmp/diff.txt 2>&1) || true
   (make -C java requireJavadocPrivate > /tmp/warnings.txt 2>&1) || true
   [ -s /tmp/diff.txt ] || (echo "/tmp/diff.txt is empty; try pulling base branch (often master) into compare branch (often feature branch)" && false)
