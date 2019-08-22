f0b5a22283688428f222f91749fa39d66ef18850
Michael Ernst <mernst@cs.washington.edu>
2019-08-21 17:46:48 -0700

    Put type-checking logic in a separate script

---
+++ scripts/test-typecheck.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This is the "typecheck" job of the pull request.
+
+set -e
+set -o pipefail
+set -o verbose
+set -o xtrace
+export SHELLOPTS
+
+export JAVA_HOME=${JAVA_HOME:-`which javac|xargs readlink -f|xargs dirname|xargs dirname`}
+
+make showvars compile daikon.jar
+
+cd .. && git clone https://github.com/typetools/checker-framework.git
+(cd ../checker-framework && source checker/bin-devel/build.sh)
+export CHECKERFRAMEWORK=`realpath ../checker-framework`
+
+make -C java typecheck
