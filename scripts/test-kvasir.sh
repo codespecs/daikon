4fcfc235b5f0685f5cf923798452ac3180d04657
Michael Ernst <mernst@cs.washington.edu>
2019-09-05 16:49:46 -0700

    Use correct fork and branch of fjalar repository

--- scripts/test-kvasir.sh
+++ scripts/test-kvasir.sh
@@ -15,6 +15,14 @@ echo ".travis-build.sh is running kvasir and DynComp tests"
 # Running Kvasir tests here may seem redundant with the fjalar project's Travis
 # build; however, it means that they are run on each branch and pull request.
 
+# Get correct version of Kvasir/fjalar
+git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
+  || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
+eval `/tmp/plume-scripts/ci-info codespecs`
+if [ ! -d ../fjalar ] ; then
+  git clone ${GIT_OPTIONS} https://github.com/$CI_ORGANIZATION/fjalar.git -b $CI_BRANCH ../fjalar
+fi
+
 # The Valgrind configure script fails if SHELLOPTS is defined.
 export -n SHELLOPTS
 make kvasir
