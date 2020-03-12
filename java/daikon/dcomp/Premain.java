906d96c355f50498b90c18ba3729601a7c12902b
Michael Ernst <mernst@cs.washington.edu>
2020-03-12 05:19:39 -0700

    Capitalization

--- java/daikon/dcomp/Premain.java
+++ java/daikon/dcomp/Premain.java
@@ -74,7 +74,7 @@ public class Premain {
               // When we create the instrumented version of these methods we
               // should include this annotation.  However, BCEL does not support
               // this at this time.  So for now, we do not instrument these methods.
-              // (There are many more in the Jdk sources, but so far no problems.)
+              // (There are many more in the JDK sources, but so far no problems.)
               "java.lang.Class.forName",
               "java.lang.Class.newInstance",
 
@@ -200,7 +200,7 @@ public class Premain {
     try {
       transformer =
           (ClassFileTransformer)
-          loader.loadClass("daikon.dcomp.Instrument").getDeclaredConstructor().newInstance();
+              loader.loadClass("daikon.dcomp.Instrument").getDeclaredConstructor().newInstance();
     } catch (Exception e) {
       throw new RuntimeException("Unexpected error loading Instrument", e);
     }
