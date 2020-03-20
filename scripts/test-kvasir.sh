0ce219687cf59d3bf7694817ae8125ae9cb52f81
Michael Ernst <mernst@cs.washington.edu>
2020-03-19 17:59:21 -0700

    Fix capitalization

--- scripts/test-kvasir.sh
+++ scripts/test-kvasir.sh
@@ -18,7 +18,7 @@ if [ ! -d ../fjalar ] ; then
   if [ -d "/tmp/$USER/plume-scripts" ] ; then
     (cd /tmp/$USER/plume-scripts && git pull -q) > /dev/null 2>&1
   else
-    mkdir -P "/tmp/$USER" && git -C "/tmp/$USER" clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
+    mkdir -p "/tmp/$USER" && git -C "/tmp/$USER" clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
   fi
   /tmp/$USER/plume-scripts/git-clone-related codespecs fjalar
 fi
