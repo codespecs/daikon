6b3879a127722cc8d915e1bfa9c461239cec6acd
Michael Ernst <mernst@cs.washington.edu>
2020-04-04 13:30:40 -0700

    Add Javadoc

--- java/daikon/Daikon.java
+++ java/daikon/Daikon.java
@@ -1423,6 +1423,8 @@ public final class Daikon {
    *
    * @param field a field; must be static
    * @param value the value to set the field to
+   * @throws IllegalAccessException if {@code field} is enforcing Java language access control and
+   *     the underlying field is either inaccessible or final.
    */
   // This method exists to reduce the scope of the warning suppression.
   @SuppressWarnings("nullness:argument.type.incompatible") // field is static, so object may be null
