123feaa7de50160479a27e224e3ef469d0d1c60e
Michael Ernst <mernst@cs.washington.edu>
2020-04-08 21:10:15 -0700

    Use fewer Make options

--- scripts/test-quick-txt-diff.sh
+++ scripts/test-quick-txt-diff.sh
@@ -10,7 +10,7 @@ make showvars compile daikon.jar
 
 make dyncomp-jdk
 MAKE_VERSION=$(make --version 2>&1 | head -1)
-if [[ $MAKE_VERSION =~ "GNU Make 4" ]]; then
-  MPARG_ARG="MPARG=-Otarget"
-fi
+# if [[ $MAKE_VERSION =~ "GNU Make 4" ]]; then
+#   MPARG_ARG="MPARG=-Otarget"
+# fi
 make -C tests $MPARG_ARG quick-txt-diff results
