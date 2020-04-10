1130321fd052c76ecd5d06fe3a8f4f8f0af5f3c0
Michael Ernst <mernst@cs.washington.edu>
2020-04-09 21:32:49 -0700

    Reduce parallelism on CI, to ease debugging

--- scripts/test-quick-txt-diff.sh
+++ scripts/test-quick-txt-diff.sh
@@ -9,8 +9,8 @@ export SHELLOPTS
 make showvars compile daikon.jar
 
 make dyncomp-jdk
-MAKE_VERSION=$(make --version 2>&1 | head -1)
+# MAKE_VERSION=$(make --version 2>&1 | head -1)
 # if [[ $MAKE_VERSION =~ "GNU Make 4" ]]; then
 #   MPARG_ARG="MPARG=-Otarget"
 # fi
-make -C tests $MPARG_ARG quick-txt-diff results
+make -C tests MPARG=-j1 quick-txt-diff results
