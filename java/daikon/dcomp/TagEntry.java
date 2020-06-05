e114cfa9ed679b93d557829fcb1c85c18c0c8e6c
Michael Ernst <mernst@cs.washington.edu>
2020-06-05 15:24:31 -0700

    TagEntry refactoring and documentation improvements

--- java/daikon/dcomp/TagEntry.java
+++ java/daikon/dcomp/TagEntry.java
@@ -8,33 +8,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.StringJoiner;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
- * Union/Find datastructure for Objects without the ranking optimization. All references to the
+ * Union-Find datastructure for Objects without the ranking optimization. All references to the
  * Objects are weak so that they will be removed from the sets when no longer referenced.
  */
-
-/* TagEntry now implements tracing, which means that if A trace-points to B,
- * then we guarantee that A and B have directly interacted (stronger condition
- * than being in the same comparability set). Additionally, each trace-point
- * is tagged with a class name, method name, and line number indicating where
- * the relevant interaction occurred.
- *
- * TODO:
- *
- *   Let A &rarr; B and C &rarr; D be in the same tree, where D is not a child of A
- *   and B is not a child of C. When union(A, C), consider x = rank(C) - rank(B)
- *   and y = rank(A) - rank(D). If either x or y is greater than 0, then A &rarr; C
- *   if x &gt; y, and C &rarr; A otherwise.
- *
- *   Tracers should list line numbers and files where they are created.
- *
- *   Union-by-rank on normal and tracer trees.
- *
- * -charlest
- */
-
+// TODO: Make TagEntry generic.
 @SuppressWarnings("interning")
 class TagEntry extends WeakReference<Object> {
 
@@ -51,16 +32,30 @@ class TagEntry extends WeakReference<Object> {
   private @Nullable TagEntry parent;
 
   /**
-   * Element in the tree that this element interacted with. Important!: tracer is null if this has
-   * no tracer.
+   * An element in the tree that this element interacted with. If A trace-points to B, then A and B
+   * have directly interacted (stronger condition than being in the same comparability set).
    *
-   * <p>if TRACING_ENABLED?
+   * <p>This is not a symmetric relation; for each interaction, at most one TagEntry's {@code
+   * tracer} field is set.
+   *
+   * <p>Is null for the root, and also is null if DynComp.trace_file is null.
+   *
+   * <p>This is not necessarily an interaction with the parent, because {@link #find} sets {@link
+   * #parent} but does not set {@link #tracer}.
    */
   private @Nullable TagEntry tracer;
 
-  protected String trace_loc = "";
+  /**
+   * The class name, method name, and line number where the interaction occurred. Is null if {@link
+   * #tracer} is null.
+   */
+  protected String tracer_loc = "";
 
-  /** Create an entry as a separate set. */
+  /**
+   * Create an entry as a separate set. Does not put {@code obj} in {@link object_map}.
+   *
+   * @param obj the object to put in a new set
+   */
   public TagEntry(Object obj) {
     super(obj);
     this.parent = null;
@@ -68,15 +63,25 @@ class TagEntry extends WeakReference<Object> {
     // System.out.printf("Make %s with parent %s%n", this, this.parent);
   }
 
-  /** Create an entry and add it to an existing set. */
+  /**
+   * Create an entry and add it to an existing set. Does not put {@code obj} in {@link object_map}.
+   *
+   * @param obj the object to put in a new set
+   * @param parent the parent of the new set
+   */
   public TagEntry(Object obj, TagEntry parent) {
     super(obj);
     this.parent = parent;
-    this.tracer = parent; // if TRACING_ENABLED?
+    this.tracer = parent;
     // System.out.printf("Made %s with parent p%s%n", this, this.parent);
   }
 
-  /** Creates a set that only contains obj. */
+  /**
+   * Creates a set that only contains obj. Puts {@code obj} in {@link object_map}.
+   *
+   * @param obj the object to create a new set for
+   * @return a new set containing {@code obj}
+   */
   public static TagEntry create(Object obj) {
     assert !object_map.containsKey(obj);
     TagEntry entry = new TagEntry(obj);
@@ -92,55 +97,19 @@ class TagEntry extends WeakReference<Object> {
     assert (obj1 != null) && (obj2 != null);
     // debug.log("union of '%s' and '%s'%n", obj1, obj2);
 
-    TagEntry o1 = get_entry(obj1), o2 = get_entry(obj2);
-    TagEntry r1 = o1.find(), r2 = o2.find();
+    TagEntry te1 = get_entry(obj1);
+    TagEntry te2 = get_entry(obj2);
+    TagEntry root1 = te1.find();
+    TagEntry root2 = te2.find();
 
-    // if TRACING_ENABLED?
-
-    if (r1 != r2) {
-      r2.parent = r1;
+    if (root1 != root2) {
+      root2.parent = root1;
       if (DynComp.trace_file != null) {
-        o1.rootMe();
-        o2.rootMe();
-        o2.tracer = o1;
-        o2.trace_loc = generateTraceString();
+        te1.rootMe();
+        te2.rootMe();
+        te2.tracer = te1;
+        te2.tracer_loc = generateTraceString();
       }
-    }
-  }
-
-  public static String generateTraceString() {
-    ArrayList<StackTraceElement> blarg =
-        new ArrayList<StackTraceElement>(
-            Arrays.<StackTraceElement>asList(new Exception().getStackTrace()));
-    if (blarg.get(blarg.size() - 1).getClassName().equals("daikon.dcomp.Premain$ShutdownThread")) {
-      return "";
-    }
-    do {
-      blarg.remove(0);
-    } while (blarg.get(0).getClassName().equals("daikon.dcomp.DCRuntime")
-        || blarg.get(0).getClassName().equals("daikon.dcomp.TagEntry"));
-
-    if (daikon.DynComp.trace_line_depth == 1) {
-      StackTraceElement first = blarg.get(0);
-      return first.getClassName() + ":" + first.getMethodName() + "(), " + first.getLineNumber();
-    } else {
-      ArrayList<String> blarg2 = new ArrayList<>(daikon.DynComp.trace_line_depth);
-      try {
-        for (int i = 0; i < daikon.DynComp.trace_line_depth; i++) {
-          StackTraceElement ste = blarg.get(i);
-          String cname = ste.getClassName();
-          cname = cname.substring(cname.lastIndexOf("."));
-          String fname = ste.getMethodName();
-          int lnum = ste.getLineNumber();
-          blarg2.add(cname + ":" + fname + "(), " + lnum);
-        }
-      } catch (IndexOutOfBoundsException e) {
-      }
-      String result = "";
-      for (String s : blarg2) {
-        result += s + " <- ";
-      }
-      return result.substring(0, result.length() - 4);
     }
   }
 
@@ -149,12 +118,10 @@ class TagEntry extends WeakReference<Object> {
 
     assert obj != null;
     TagEntry entry = object_map.get(obj);
-    if (entry == null) entry = create(obj);
+    if (entry == null) {
+      entry = create(obj);
+    }
     return entry;
-  }
-
-  public static String get_line_trace(Object obj) {
-    return get_entry(obj).trace_loc;
   }
 
   /**
@@ -167,25 +134,29 @@ class TagEntry extends WeakReference<Object> {
       return this;
     }
 
-    // Find the tag at the top of the list
-    TagEntry tag = this;
-    while (tag.parent != null) tag = tag.parent;
-    TagEntry top = tag;
+    // Find the canonical representative
+    TagEntry root = this;
+    while (root.parent != null) {
+      root = root.parent;
+    }
 
-    // Set everyone to point to the top
-    tag = this;
+    // Set everyone to point to the root
+    TagEntry tag = this;
     while (tag.parent != null) {
       TagEntry next = tag.parent;
-      tag.parent = top;
+      tag.parent = root;
       tag = next;
     }
 
-    return top;
+    return root;
   }
 
   /**
-   * Given an Object returns the Object that is the representative for its set. If there is no entry
-   * for Object in the map, it must be in a set by itself.
+   * Given an Object, returns the Object that is the representative for its set. If there is no
+   * entry for Object in the map, it must be in a set by itself.
+   *
+   * @param obj the object to find the representative for
+   * @return the representative of the set containing {@code obj}
    */
   public static Object find(Object obj) {
     assert obj != null;
@@ -202,11 +173,35 @@ class TagEntry extends WeakReference<Object> {
     // If the root object has been garbage collected, just return
     // a reference to the tag instead.  Since the caller has no references
     // to the root, it can't matter what we returned.
+    // TODO: In that case, why not return obj?
     Object root_ref = root.get();
-    if (root_ref == null) root_ref = root;
+    if (root_ref == null) {
+      root_ref = root;
+    }
     return root_ref;
   }
 
+  ///////////////////////////////////////////////////////////////////////////
+  /// Tracers
+  ///
+
+  /**
+   * Return information about where the given object interacted with some other object in its set.
+   *
+   * @param obj an object in the union-find data structure
+   * @return information about where the given object interacted with some other object in its set
+   */
+  public static String get_line_trace(Object obj) {
+    return get_entry(obj).tracer_loc;
+  }
+
+  /**
+   * Return the canonical member of this object's set, based on tracers. Returns null if this
+   * object's interactions were not recorded.
+   *
+   * @param obj an object that might be in the union-find data structure
+   * @return the canonical member of this object's set (based on tracers), or possibly null
+   */
   public static @Nullable Object tracer_find(Object obj) {
     TagEntry entry = object_map.get(obj);
     if (entry == null) {
@@ -217,7 +212,9 @@ class TagEntry extends WeakReference<Object> {
       return null;
     }
     Object tr_ref = tracer.get();
-    if (tr_ref == null) tr_ref = tracer;
+    if (tr_ref == null) {
+      tr_ref = tracer;
+    }
     return tr_ref;
   }
 
@@ -228,42 +225,119 @@ class TagEntry extends WeakReference<Object> {
     }
     TagEntry troot = entry.getTraceRoot();
     Object tr_ref = troot.get();
-    if (tr_ref == null) tr_ref = troot;
+    if (tr_ref == null) {
+      tr_ref = troot;
+    }
     return tr_ref;
   }
 
   /**
    * Recursively traces from this object to the root of its tracer tree, and reverses the direction
    * of every pointer on the path, such that this object is now the root of its tracer tree.
-   * (Imprecise wording, I know.)
+   *
+   * @param newTracer the new {@code tracer} value for this
+   * @param tloc the new {@code tracer_loc} value for this
    */
-  @SuppressWarnings("nullness") // catches NullPointerException
   public void reroute(@Nullable TagEntry newTracer, String tloc) {
-    try {
-      this.tracer.reroute(this, trace_loc);
-    } catch (NullPointerException e) {
-    } finally {
-      this.tracer = newTracer;
-      this.trace_loc = tloc;
+    TagEntry thisTracer = this.tracer;
+    if (thisTracer != null) {
+      // System.out.println("Tracer not null");
+      thisTracer.reroute(this, tracer_loc);
     }
-    //    if (this.tracer != null) {
-    //       System.out.println("Tracer not null");
-    //       this.tracer.reroute(this);
-    //    }
-    //    this.tracer = newTracer;
+    this.tracer = newTracer;
+    this.tracer_loc = tloc;
   }
 
   public void rootMe() {
     this.reroute(null, "");
   }
 
-  /** Returns each of the sets with elements in each set on a separate line. */
+  /**
+   * Returns the tracer of this node.
+   *
+   * @return the tracer of this node
+   */
+  public @Nullable TagEntry getTracer() {
+    return tracer;
+  }
+
+  /**
+   * Follow tracer pointers as far as possible.
+   *
+   * @return the last element in the path created by {@link #tracer} fields
+   */
+  public TagEntry getTraceRoot() {
+    if (tracer == null) {
+      return this;
+    } else {
+      return tracer.getTraceRoot();
+    }
+  }
+
+  /**
+   * Return a description of where an interaction occurred. Is essentially a stack trace of depth
+   * {@link DynComp#trace_line_depth}.
+   *
+   * @return a description of where an interaction occurred
+   */
+  public static String generateTraceString() {
+    ArrayList<StackTraceElement> stackTrace =
+        new ArrayList<StackTraceElement>(
+            Arrays.<StackTraceElement>asList(new Exception().getStackTrace()));
+    if (stackTrace
+        .get(stackTrace.size() - 1)
+        .getClassName()
+        .equals("daikon.dcomp.Premain$ShutdownThread")) {
+      return "";
+    }
+
+    while (stackTrace.get(0).getClassName().equals("daikon.dcomp.DCRuntime")
+        || stackTrace.get(0).getClassName().equals("daikon.dcomp.TagEntry")) {
+      stackTrace.remove(0);
+    }
+
+    if (DynComp.trace_line_depth == 1) {
+      return traceLineToString(stackTrace.get(0), false);
+    } else {
+      int depth = Math.min(DynComp.trace_line_depth, stackTrace.size());
+      StringJoiner result = new StringJoiner(" <- ");
+      for (int i = 0; i < depth; i++) {
+        result.add(traceLineToString(stackTrace.get(i), true));
+      }
+      return result.toString();
+    }
+  }
+
+  /**
+   * Returns a description of the given StackTraceElement.
+   *
+   * @param ste a StackTraceElement to describe
+   * @param abbreviate if true, omit package name
+   * @return a description of the given StackTraceElement.
+   */
+  private static String traceLineToString(StackTraceElement ste, boolean abbreviate) {
+    String className = ste.getClassName();
+    if (abbreviate) {
+      className = className.substring(className.lastIndexOf("."));
+    }
+    return className + ":" + ste.getMethodName() + "(), " + ste.getLineNumber();
+  }
+
+  ///////////////////////////////////////////////////////////////////////////
+  /// Debugging output
+  ///
+
+  /**
+   * Returns each of the sets with elements in each set on a separate line.
+   *
+   * @return a verbose printed representation of this
+   */
   public static String dump() {
 
     LinkedHashMap<Object, List<Object>> sets = new LinkedHashMap<>();
 
-    /* Fill sets from object_map by placing every object in an ArrayList
-     * whose key is its root. */
+    // Fill sets from object_map by placing every object in an ArrayList
+    // whose key is its root.
     for (Object obj : object_map.keySet()) {
       Object rep = find(obj);
       List<Object> set = sets.get(rep);
@@ -274,34 +348,20 @@ class TagEntry extends WeakReference<Object> {
       set.add(obj);
     }
 
-    String out = String.format("%d objects in object_map%n", object_map.size());
+    StringJoiner result = new StringJoiner(System.lineSeparator());
+    result.add(String.format("%d objects in object_map", object_map.size()));
     for (Object rep : sets.keySet()) {
       List<Object> set = sets.get(rep);
-      String line = "";
+      StringJoiner line = new StringJoiner(", ");
       for (Object entry : set) {
-        if (line != "") // "interned"
-        line += ", ";
         if (entry instanceof DaikonVariableInfo) {
-          line += String.format("%s ", ((DaikonVariableInfo) entry).getName());
+          line.add(String.format("%s ", ((DaikonVariableInfo) entry).getName()));
         } else {
-          line += String.format("%s [%s]", entry.getClass(), entry);
+          line.add(String.format("%s [%s]", entry.getClass(), entry));
         }
       }
-      out += String.format("%s%n", line);
+      result.add(String.format("%s%n", line));
     }
-    return out;
-  }
-
-  /** Returns the tracer of this node. */
-  public @Nullable TagEntry getTracer() {
-    return tracer;
-  }
-
-  public TagEntry getTraceRoot() {
-    if (tracer == null) {
-      return this;
-    } else {
-      return tracer.getTraceRoot();
-    }
+    return result.toString();
   }
 }
