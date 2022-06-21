7c6f34c0ed1d94359ae78df523e08587b66afd2e
Michael Ernst <mernst@cs.washington.edu>
2022-06-21 12:26:15 -0700

    Add interface

--- java/daikon/simplify/SessionManager.java
+++ java/daikon/simplify/SessionManager.java
@@ -171,7 +171,7 @@ public class SessionManager implements Closeable {
   }
 
   /** Helper thread which interacts with a Session, according to the enclosing manager. */
-  private class Worker extends Thread {
+  private class Worker extends Thread implements Closeable {
     private final SessionManager mgr = SessionManager.this; // just sugar
 
     /** The associated session, or null if the thread should shutdown. */
