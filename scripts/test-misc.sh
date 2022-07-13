b402c8f468cff1cdaf533f0eadbe400365368bfa
Michael Ernst <mernst@cs.washington.edu>
2022-07-13 09:32:06 -0700

    Debugging changes

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -16,40 +16,13 @@ else
   mkdir -p "/tmp/$USER" && (cd "/tmp/$USER" && git clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git)
 fi
 
-# Code style & quality
-make -C java error-prone
-
 # Code formatting
+echo "---------------- ParameterDoclet.java ----------------
+cat java/daikon/config/ParameterDoclet.java
+echo "---------------- end of ParameterDoclet.java ----------------
+make -C java reformat
+echo "---------------- ParameterDoclet.java ----------------
+cat java/daikon/config/ParameterDoclet.java
+echo "---------------- end of ParameterDoclet.java ----------------
+
 make -C java check-format
-
-# Documentation
-if java -version 2>&1 | grep -q '"1.8'; then
-  # Java version 8
-  if ! grep -q Ubuntu /etc/os-release; then
-    # Not Ubuntu
-    SKIP_JAVADOC=1
-  fi
-fi
-if [ -n "${SKIP_JAVADOC+x}" ]; then
-  echo Skipping javadoc because of https://bugs.openjdk.java.net/browse/JDK-8215542
-  exit
-else
-
-  make javadoc doc-all
-
-  # For refactorings that touch a lot of code that you don't understand, create
-  # top-level file SKIP-REQUIRE-JAVADOC.  Delete it after the pull request is merged.
-  if [ ! -f SKIP-REQUIRE-JAVADOC ]; then
-    # The `api-private` and `requireJavadoc` commands are separate to avoid
-    # assuming that they both produce absolute filenames or both produce filenames
-    # relative to the same directory.  Always run both to avoid one masking
-    # failures in the other.
-    status=0
-    # The `grep -v` prevents the make target failure from throwing off prefix guessing.
-    (make -C java api-private 2>&1 | grep -v "^Makefile:[0-9]*: recipe for target 'api-private' failed" > "/tmp/$USER/ap-warnings.txt") || true
-    "/tmp/$USER/plume-scripts/ci-lint-diff" "/tmp/$USER/ap-warnings.txt" || status=1
-    (make -C java requireJavadoc 2>&1 | grep -v "^Makefile:[0-9]*: recipe for target 'requireJavadoc' failed" > "/tmp/$USER/rj-warnings.txt") || true
-    "/tmp/$USER/plume-scripts/ci-lint-diff" "/tmp/$USER/rj-warnings.txt" || status=1
-  fi
-  exit $status
-fi
