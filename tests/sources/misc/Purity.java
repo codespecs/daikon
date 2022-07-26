f22d5a90568d4bb783d2797d2ef1407fea8127be
Michael Ernst <mernst@cs.washington.edu>
2022-07-26 12:42:41 -0700

    Remove trailing whitespace

--- tests/sources/misc/Purity.java
+++ tests/sources/misc/Purity.java
@@ -10,7 +10,7 @@ public class Purity {
 	private boolean heavy;
 	private LinkedList<Number> list1;
 	private List<Number> list2;
-	
+
 	public Purity(int value, int shift) {
 		this.value = value;
 		this.shift = shift;
@@ -22,35 +22,35 @@ public class Purity {
 			list2.add(new Integer(j));
 		}
 	}
-	
+
 	public int getValue() {
 		return value;
 	}
-	
+
 	public int getShift() {
 		return shift;
 	}
-	
+
 	public boolean isHeavy() {
 		return heavy;
 	}
-	
+
 	public Number getNum() {
 		return (Number) new Integer(value);
 	}
-	
+
 	public Integer getJWrap() {
 		return new Integer(value);
 	}
-	
+
 	public int scale(int scale) {
 		return value + shift * scale;
 	}
-	
+
 	public int sum(Number n) {
 		return value + n.intValue();
 	}
-	
+
 	public int retrieve(List<Number> l) {
 		return l.get(shift).intValue();
 	}
