eac598fe9098951228c4bb264f91dc49c66a8861
Michael Ernst <mernst@cs.washington.edu>
2022-09-13 03:04:56 -1000

    Use "superclass" instead of "super class"

--- java/jtb/ParseException.java
+++ java/jtb/ParseException.java
@@ -18,7 +18,7 @@ public class ParseException extends Exception {
    * Calling this constructor generates a new object of this type with the fields "currentToken",
    * "expectedTokenSequences", and "tokenImage" set. The boolean flag "specialConstructor" is also
    * set to true to indicate that this constructor was used to create this object. This constructor
-   * calls its super class with the empty string to force the "toString" method of parent class
+   * calls its superclass with the empty string to force the "toString" method of parent class
    * "Throwable" to print the error message in the form: ParseException: result of getMessage
    */
   public ParseException(
