504da8eb39eb7e9efce1ea07d1c40f2412ab8d63
Michael Ernst <mernst@cs.washington.edu>
2023-05-10 15:12:59 -0700

    Fix jarjar invocations

--- java/lib/create-daikon-plumelib.sh
+++ java/lib/create-daikon-plumelib.sh
@@ -5,17 +5,18 @@ set -e
 # Echo commands before executing them
 set -x
 
-java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt plume-util-*.jar daikon-util.jar
-java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt hashmap-util-*.jar daikon-util.jar
-java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt bcel-util-*.jar daikon-bcelutil.jar
+java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt bcel-util-*.jar daikon-bcel-util.jar
+java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt hashmap-util-*.jar daikon-hashmap-util.jar
 java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt options-*.jar daikon-options.jar
-java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt reflection-util-*.jar daikon-reflection.jar
+java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt plume-util-*.jar daikon-plume-util.jar
+java -cp "$HOME"/java/jarjar-pantsbuild/dist/jarjar.jar org.pantsbuild.jarjar.Main process daikon-plumelib-rules.txt reflection-util-*.jar daikon-reflection-util.jar
 mkdir -p daikonplume
 cd daikonplume
-jar -xf ../daikon-bcelutil.jar
+jar -xf ../daikon-bcel-util.jar
+jar -xf ../daikon-hashmap-util.jar
 jar -xf ../daikon-options.jar
-jar -xf ../daikon-reflection.jar
-jar -xf ../daikon-util.jar
+jar -xf ../daikon-plume-util.jar
+jar -xf ../daikon-reflection-util.jar
 jar cf daikon-plumelib.jar ./*
 cd ..
 rm daikon*.jar
