4c9e3be9bc03b39a9273321b8188940c62ea67e0
Michael Ernst <mernst@cs.washington.edu>
2023-05-16 15:53:45 -0700

    Sleep between attempts

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -13,7 +13,7 @@ make compile daikon.jar
 if [ -d "/tmp/$USER/plume-scripts" ] ; then
   (cd "/tmp/$USER/plume-scripts" && git pull -q) > /dev/null 2>&1
 else
-  mkdir -p "/tmp/$USER" && (cd "/tmp/$USER" && (git clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git || git clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git))
+  mkdir -p "/tmp/$USER" && (cd "/tmp/$USER" && (git clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git || (sleep 1m && git clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git)))
 fi
 
 # Code style & quality
