72ff100f3c1af928ec05293079d935ad3ba4b16d
Michael Ernst <mernst@cs.washington.edu>
2023-05-19 12:54:19 -0700

    Don't use deprecated `new URI()`

--- java/daikon/FileIO.java
+++ java/daikon/FileIO.java
@@ -34,7 +34,6 @@ import java.io.UncheckedIOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -1379,13 +1378,7 @@ public final class FileIO {
         InputStreamReader chicReader = new InputStreamReader(chicoryInput, UTF_8);
         reader = new LineNumberReader(chicReader);
       } else if (is_url) {
-        // `raw_filename` starts with "file:" or "jar:".
-        URL url = null; // dummy initialization for compiler's definite assignment check
-        try {
-          url = new URI(raw_filename).toURL();
-        } catch (URISyntaxException e) {
-          throw new Error(e);
-        }
+        URL url = URI.create(raw_filename).toURL();
         InputStream stream = null; // dummy initialization for compiler's definite assignment check
         try {
           stream = url.openStream();
