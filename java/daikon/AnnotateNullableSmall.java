883a6dc958dfc77b93802d5f3c2aef93409b667e
Michael Ernst <mernst@cs.washington.edu>
2023-05-31 08:47:57 -0700

    Smaller test case

---
+++ java/daikon/AnnotateNullableSmall.java
@@ -0,0 +1,7 @@
+package daikon;
+
+public class AnnotateNullableSmall {
+  public static void process_class(PptTopLevel class_ppt) {
+    String class_samples = String.format("%d", class_ppt.num_samples());
+  }
+}
