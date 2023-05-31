2e3a2154e362a259ce50c1782cac709357c609c5
Michael Ernst <mernst@cs.washington.edu>
2023-05-31 09:23:32 -0700

    Revert change

--- java/daikon/AnnotateNullableSmall.java
+++ java/daikon/AnnotateNullableSmall.java
@@ -1,6 +1,15 @@
+// This does not reproduce the problem!
+
 package daikon;
 
+/** A test class. */
 public class AnnotateNullableSmall {
+  /**
+   * A test method.
+   *
+   * @param class_ppt its input
+   * @return its output
+   */
   public static String process_class(PptTopLevel class_ppt) {
     return String.format("%d", class_ppt.num_samples());
   }
