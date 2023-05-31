9e7b76abec008ac04137bc5ab22b58836fcc488f
Michael Ernst <mernst@cs.washington.edu>
2023-05-31 11:02:43 -0700

    Add more code

--- java/daikon/AnnotateNullable2.java
+++ java/daikon/AnnotateNullable2.java
@@ -1,13 +1,18 @@
 package daikon;
 
 import daikon.PptTopLevel.PptType;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import org.checkerframework.checker.interning.qual.Interned;
+import org.checkerframework.checker.nullness.qual.KeyFor;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.plumelib.options.Option;
+import org.plumelib.options.Options;
 
 /**
  * AnnotateNullable reads a Daikon invariant file and determines which reference variables have seen
@@ -22,7 +27,7 @@ import org.plumelib.options.Option;
  * This is not necessary, however, for correct operation. File {@code annotate_nullable.config} in
  * the distribution does this.
  */
-public class AnnotateNullable2 {
+public class AnnotateNullable {
 
   // Why is this variable static?
   static PptMap ppts = new PptMap(); // dummy value, to satisfy Nullness Checker
@@ -50,6 +55,117 @@ public class AnnotateNullable2 {
    */
   @Option("-n Insert NonNull as well as Nullable annotations")
   public static boolean nonnull_annotations = false;
+
+  public static void main(String[] args) throws IOException {
+
+    Options options =
+        new Options("plume.AnnotateNullable [options] <inv_file>", AnnotateNullable.class);
+    String[] inv_files = options.parse(true, args);
+    assert inv_files.length == 1;
+
+    // Read the serialized invariant file
+    File inv_file = new File(inv_files[0]);
+    ppts = FileIO.read_serialized_pptmap(inv_file, true);
+    Daikon.all_ppts = ppts;
+    // verbose.log("Finished reading %d program points", ppts.size());
+
+    // Setup the list of proto invariants and initialize NIS suppressions
+    Daikon.setup_proto_invs();
+    Daikon.setup_NISuppression();
+
+    // Write out the definitions of our annotations
+    if (stub_format) {
+      System.out.println("import org.checkerframework.checker.nullness.qual.Nullable;");
+      System.out.println("import org.checkerframework.checker.nullness.qual.NonNull;");
+      System.out.println();
+    } else {
+      System.out.println("package org.checkerframework.checker.nullness.qual:");
+      System.out.println("annotation @Nullable:");
+      System.out.println("annotation @NonNull:");
+      System.out.println();
+    }
+
+    // Find all exit ppts that do not have a parent and determine what
+    // class they are associated with.  These are static methods for classes
+    // without any static variables (no class ppt is created if there are no
+    // static variables)
+
+    // First find all of the classes
+    for (PptTopLevel ppt : ppts.pptIterable()) {
+      if (ppt.is_object()) {
+        String classname = ppt.name().replace(":::OBJECT", "");
+        assert !class_map.containsKey(classname) : classname;
+        List<PptTopLevel> static_methods = new ArrayList<>();
+        class_map.put(classname, static_methods);
+      }
+    }
+
+    // Then, add combined exit points for static methods to their class.  A
+    // static method can be identified because it will not have the OBJECT
+    // point as a parent.
+    for (PptTopLevel ppt : ppts.pptIterable()) {
+      if (!ppt.is_combined_exit()) {
+        continue;
+      }
+
+      String name = ppt.name().replaceFirst("[(].*$", "");
+      int lastdot = name.lastIndexOf('.');
+      @SuppressWarnings("keyfor") // application invariant:  KeyFor and substring
+      // @KeyFor because class_map has entry per class, and this method is in some class
+      @KeyFor("class_map") String classname = name.substring(0, lastdot);
+      // System.out.printf("classname for ppt %s is '%s'%n", name, classname);
+      @NonNull List<PptTopLevel> static_methods = class_map.get(classname);
+      assert static_methods != null : classname;
+      static_methods.add(ppt);
+    }
+
+    // Debug print all of the static methods
+    if (false) {
+      for (String classname : class_map.keySet()) {
+        System.out.printf("class %s static methods: %s%n", classname, class_map.get(classname));
+      }
+    }
+
+    // Make sure that the static methods found by inference, match those
+    // found for any class ppts
+    for (PptTopLevel ppt : ppts.pptIterable()) {
+      if (ppt.is_class()) {
+        @SuppressWarnings(
+            "nullness") // map: retrieve class name from class Ppt name, with string manipulation
+        @NonNull List<PptTopLevel> static_methods =
+            class_map.get(ppt.name().replace(":::CLASS", ""));
+        int child_cnt = 0;
+        // TODO: Once Checker Framework issue 565 has been fixed (https://tinyurl.com/cfissue/565),
+        // change the following two lines back to
+        // for (PptRelation child_rel : ppt.children) {
+        for (int i = 0; i < ppt.children.size(); i++) {
+          PptRelation child_rel = ppt.children.get(i);
+          PptTopLevel child = child_rel.child;
+          // Skip enter ppts, all of the info is at the exit.
+          if ((child.type == PptType.ENTER) || (child.type == PptType.OBJECT)) {
+            continue;
+          }
+          child_cnt++;
+          assert static_methods.contains(child) : child;
+        }
+        assert child_cnt == static_methods.size() : static_methods;
+      }
+    }
+
+    // Process each class.
+    for (PptTopLevel ppt : ppts.pptIterable()) {
+
+      // Skip synthetic program points
+      if (ppt.name().startsWith("$")) {
+        continue;
+      }
+
+      // Skip program points that are not OBJECT ppts
+      if (ppt.is_object()) {
+        process_class(ppt);
+      }
+    }
+  }
 
   // Returns null if no corresponding class ppt exists
   private static @Nullable PptTopLevel class_for_object(PptTopLevel object_ppt) {
@@ -94,6 +210,14 @@ public class AnnotateNullable2 {
           object_ppt.ppt_name.getShortClassName(), object_ppt.num_samples(), class_samples);
     }
 
+    // Process static fields
+    if (class_ppt != null) {
+      process_obj_fields(class_ppt);
+    }
+
+    // Process member (non-static) fields
+    process_obj_fields(object_ppt);
+
     // Process static methods
     if (class_ppt != null) {
       for (PptRelation child_rel : class_ppt.children) {
@@ -103,6 +227,7 @@ public class AnnotateNullable2 {
           continue;
         }
         // debug.log("processing static method %s, type %s", child, child.type);
+        process_method(child);
       }
     } else {
       String classname = object_ppt.ppt_name.getFullClassName();
@@ -110,7 +235,9 @@ public class AnnotateNullable2 {
       @SuppressWarnings("nullness") // map: class_map has entry per classname
       @NonNull List<PptTopLevel> static_methods = class_map.get(classname);
       assert static_methods != null : classname;
-      for (PptTopLevel child : static_methods) {}
+      for (PptTopLevel child : static_methods) {
+        process_method(child);
+      }
     }
 
     // Process member (non-static) methods
@@ -121,6 +248,7 @@ public class AnnotateNullable2 {
         continue;
       }
       // debug.log("processing method %s, type %s", child, child.type);
+      process_method(child);
     }
 
     if (stub_format) {
@@ -128,5 +256,122 @@ public class AnnotateNullable2 {
     }
     System.out.println();
     System.out.println();
+  }
+
+  /**
+   * Get the annotation for the specified variable. Returns @Nullable if samples were found for this
+   * variable and at least one sample contained a null value. Returns an (interned) empty string if
+   * no annotation is applicable. Otherwise, the return value contains a trailing space.
+   */
+  public static String get_annotation(PptTopLevel ppt, VarInfo vi) {
+
+    if (vi.type.isPrimitive()) {
+      return "";
+    }
+
+    String annotation = (nonnull_annotations ? "NonNull" : "");
+    if ((ppt.num_samples(vi) > 0) && !ppt.is_nonzero(vi)) {
+      annotation = "Nullable";
+    }
+    if (annotation != "") { // interned
+      // if (! stub_format) {
+      //   annotation = "org.checkerframework.checker.nullness.qual." + annotation;
+      // }
+      annotation = "@" + annotation;
+    }
+    return annotation;
+  }
+
+  /** Print out the annotations for the specified method. */
+  public static void process_method(PptTopLevel ppt) {
+
+    assert ppt.type == PptType.EXIT : ppt;
+
+    // Get all of the parameters to the method and the return value
+    List<VarInfo> params = new ArrayList<>();
+    VarInfo retvar = null;
+    for (VarInfo vi : ppt.var_infos) {
+      if (vi.var_kind == VarInfo.VarKind.RETURN) {
+        retvar = vi;
+      } else {
+        if (vi.isParam()
+            && (vi.name() != "this") // interned
+            && !vi.isPrestate()) {
+          params.add(vi);
+        }
+      }
+    }
+
+    // The formatted annotation for the return value with a leading space, or empty string
+    String return_annotation = (retvar == null ? "" : " " + get_annotation(ppt, retvar));
+
+    // Look up the annotation for each parameter.
+    List<String> names = new ArrayList<>();
+    List<String> annos = new ArrayList<>();
+    for (VarInfo param : params) {
+      String annotation = "";
+      names.add(param.name());
+      if (param.file_rep_type.isHashcode()) {
+        annotation = get_annotation(ppt, param);
+      }
+      annos.add(annotation);
+    }
+
+    // Print out the method declaration
+    if (stub_format) {
+      System.out.printf(" %s %s(", return_annotation, ppt.ppt_name.getMethodName());
+      for (int i = 0; i < params.size(); i++) {
+        if (i != 0) System.out.printf(" ,");
+        System.out.printf("%s %s %s", annos.get(i), "type-goes-here", names.get(i));
+      }
+      System.out.printf("); // %d samples%n", ppt.num_samples());
+    } else {
+      System.out.printf("  method %s : // %d samples%n", ppt, ppt.num_samples());
+      System.out.printf("    return:%s%n", return_annotation);
+      for (int i = 0; i < params.size(); i++) {
+        // Print the annotation for this parameter
+        System.out.printf("    parameter #%d : %s // %s%n", i, annos.get(i), names.get(i));
+      }
+    }
+  }
+
+  /** Print out the annotations for each field in the object or class. */
+  public static void process_obj_fields(PptTopLevel ppt) {
+
+    for (VarInfo vi : ppt.var_infos) {
+
+      assert !vi.isPrestate() : vi;
+
+      // Skip anyone with a parent in the hierarchy.  We are only
+      // interested in them at the top (e.g., we don't want to see
+      // object fields in each method).
+      if (!vi.parents.isEmpty()) {
+        continue;
+      }
+
+      // Skip variables that are always non-null.
+      if (vi.aux.isNonNull()) {
+        continue;
+      }
+
+      // Skip any variable that is enclosed by a variable other than 'this'.
+      // These are fields and can only be annotated where they are declared.
+      VarInfo evar = vi.get_enclosing_var();
+      if ((evar != null) && !evar.name().equals("this")) {
+        // System.out.printf("  enclosed %s %s%n", vi.type, vi.name());
+        continue;
+      }
+
+      // print out the entry for this field
+      String annotation = "";
+      if (vi.file_rep_type.isHashcode()) {
+        annotation = get_annotation(ppt, vi);
+      }
+      if (stub_format) {
+        System.out.printf("  field %s %s {} // %s%n", vi, annotation, vi.type);
+      } else {
+        System.out.printf("  field %s : %s // %s%n", vi, annotation, vi.type);
+      }
+    }
   }
 }
