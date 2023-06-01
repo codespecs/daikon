ed21b5750349fee463693fb71fb47b4f571fe158
Michael Ernst <mernst@cs.washington.edu>
2023-05-31 21:36:40 -0700

    Yet yet more more minimization

--- java/daikon/AnnotateNullable2.java
+++ java/daikon/AnnotateNullable2.java
@@ -1,53 +1,8 @@
 package daikon;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import org.checkerframework.checker.interning.qual.Interned;
 import org.checkerframework.checker.nullness.qual.NonNull;
-import org.plumelib.options.Option;
 
-/**
- * AnnotateNullable reads a Daikon invariant file and determines which reference variables have seen
- * any null values. It writes to standard out an <a
- * href="https://checkerframework.org/annotation-file-utilities/annotation-file-format.html">annotation
- * file</a> with those variables. It determines which variables have seen null values by looking at
- * the NonZero invariant. If that invariant is NOT present, then the variable must have been null at
- * least once.
- *
- * <p>Since only the NonZero invariant is used, Daikon processing time can be significantly reduced
- * by turning off derived variables and all invariants other than daikon.inv.unary.scalar.NonZero.
- * This is not necessary, however, for correct operation. File {@code annotate_nullable.config} in
- * the distribution does this.
- */
 public class AnnotateNullable2 {
-
-  // Why is this variable static?
-  static PptMap ppts = new PptMap(); // dummy value, to satisfy Nullness Checker
-
-  // static SimpleLog verbose = new SimpleLog(/*enabled=*/ false);
-  // static SimpleLog debug = new SimpleLog(/*enabled=*/ false);
-
-  /** Map from a class name to the list of static functions for that class. */
-  static Map<String, List<PptTopLevel>> class_map = new LinkedHashMap<>();
-
-  // The package for the previous class.  Used to reduce duplication in
-  // output file.
-  static @Interned String last_package = "";
-
-  /**
-   * Write an output file in the stub class format (see the Checker Framework Manual), instead of in
-   * annotation file format.
-   */
-  @Option("Use the stub class file format")
-  public static boolean stub_format = false;
-
-  /**
-   * Adds NonNull annotations as well as Nullable annotations. Unlike Nullable annotations, NonNull
-   * annotations are not necessarily correct.
-   */
-  @Option("-n Insert NonNull as well as Nullable annotations")
-  public static boolean nonnull_annotations = false;
 
   /** Returns a JVM signature for the method. */
   public static String jvm_signature(PptTopLevel ppt) {
