fe8b22a1c5b4fb9cad43dbeb6b91ea1c1e4b5eb6
Michael Ernst <mernst@cs.washington.edu>
2023-06-03 20:56:46 -0700

    Add Javadoc

--- java/daikon/diff/InvNode.java
+++ java/daikon/diff/InvNode.java
@@ -9,7 +9,12 @@ import org.plumelib.util.IPair;
 /** Contains a pair of Invariants. Resides in the third level of the tree. Has no children. */
 public class InvNode extends Node<@Nullable Invariant, @NonNull Void> {
 
-  /** Either inv1 or inv2 may be null, but not both. */
+  /**
+   * Either inv1 or inv2 may be null, but not both.
+   *
+   * @param inv1 an invariant
+   * @param inv2 an invariant
+   */
   public InvNode(@Nullable Invariant inv1, @Nullable Invariant inv2) {
     super(IPair.of(inv1, inv2));
     assert !(inv1 == null && inv2 == null) : "Both invariants may not be null";
--- java/daikon/diff/Node.java
+++ java/daikon/diff/Node.java
@@ -8,16 +8,25 @@ import org.checkerframework.dataflow.qual.Pure;
 import org.plumelib.util.IPair;
 
 /**
- * All nodes must subclass this class. The type parameter CONTENT is (half of) the type of the
- * objects stored in this node: they are {@code IPair<CONTENT,CONTENT>}. The type parameter CHILD is
- * the type of the children (and is ignored if there are no children).
+ * All nodes must subclass this class.
+ *
+ * @param <CONTENT> half of the type of the objects stored in this node, which are {@code
+ *     IPair<CONTENT,CONTENT>}
+ * @param <CHILD> the type of the children; it is is ignored if there are no children
  */
 public abstract class Node<CONTENT extends @Nullable Object, CHILD> {
 
+  /** The children of this node. */
   private List<CHILD> children = new ArrayList<>();
-  // Nonsensical for RootNode
+
+  /** Nonsensical for RootNode. */
   private IPair<CONTENT, CONTENT> userObject;
 
+  /**
+   * Creates a new Node.
+   *
+   * @param userObject the user object
+   */
   protected Node(IPair<CONTENT, CONTENT> userObject) {
     this.userObject = userObject;
   }
@@ -34,15 +43,30 @@ public abstract class Node<CONTENT extends @Nullable Object, CHILD> {
     return children.iterator();
   }
 
+  /**
+   * Returns the user object pair.
+   *
+   * @return the user object pair
+   */
   public IPair<CONTENT, CONTENT> getUserObject() {
     return userObject;
   }
 
+  /**
+   * Returns the first element of the user object pair.
+   *
+   * @return the first element of the user object pair
+   */
   @Pure
   public CONTENT getUserLeft() {
     return userObject.first;
   }
 
+  /**
+   * Returns the second element of the user object pair.
+   *
+   * @return the second element of the user object pair
+   */
   @Pure
   public CONTENT getUserRight() {
     return userObject.second;
