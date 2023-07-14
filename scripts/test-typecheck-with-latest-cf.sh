33f2af524103835874949f44ddcec3658b1f4849
Michael Ernst <mernst@cs.washington.edu>
2023-07-14 08:50:16 -0700

    Typecheck with both bundled CF and latest CF

---
+++ scripts/test-typecheck-with-latest-cf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is the "typecheck" job of the pull request.
+# It uses the HEAD version of the Checker Framework: the latest commit in the GitHub repository.
+
+set -e
+set -o pipefail
+set -o verbose
+set -o xtrace
+export SHELLOPTS
+
+make showvars
+make compile daikon.jar
+
+# Use a version of the Checker Framework cloned from a GitHub
+# repository, NOT the version checked in at java/lib/checker-framework/.
+utils/plume-scripts/git-clone-related typetools checker-framework
+(cd ../checker-framework && source checker/bin-devel/build.sh)
+CHECKERFRAMEWORK=$(realpath ../checker-framework)
+export CHECKERFRAMEWORK
+
+make -C java typecheck
