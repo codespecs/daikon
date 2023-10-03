006b642c14d3ba5d82c849fca1f6e36c33fbe787
Suzanne Millstein <smillst@cs.washington.edu>
2023-10-03 10:59:26 -0700

    Use assemble to build the Checker Framework.

--- scripts/test-typecheck-with-latest-cf.sh
+++ scripts/test-typecheck-with-latest-cf.sh
@@ -15,7 +15,7 @@ make compile daikon.jar
 # Use a version of the Checker Framework cloned from a GitHub
 # repository, NOT the version checked in at java/lib/checker-framework/.
 utils/plume-scripts/git-clone-related typetools checker-framework
-(cd ../checker-framework && source checker/bin-devel/build.sh)
+(cd ../checker-framework && ./gradlew assembleForJavac --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradl    e.internal.http.connectionTimeout=60000)
 CHECKERFRAMEWORK=$(realpath ../checker-framework)
 export CHECKERFRAMEWORK
 
