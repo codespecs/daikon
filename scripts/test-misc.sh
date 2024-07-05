98d731ff03b8a5400376e6898c201dd0a5346b5f
Michael Ernst <mernst@cs.washington.edu>
2024-07-05 09:46:51 -0700

    Don't use `--filter=blob:none`

--- scripts/test-misc.sh
+++ scripts/test-misc.sh
@@ -14,7 +14,7 @@ if [ -d "/tmp/$USER/plume-scripts" ] ; then
   (cd "/tmp/$USER/plume-scripts" && git pull -q) > /dev/null 2>&1
 else
   mkdir -p "/tmp/$USER"
-  (cd "/tmp/$USER" && (git clone --filter=blob:none --depth 1 -q https://github.com/plume-lib/plume-scripts.git || (sleep 1m && git clone --filter=blob:none --depth 1 -q https://github.com/plume-lib/plume-scripts.git)))
+  (cd "/tmp/$USER" && (git clone --depth=1 --depth 1 -q https://github.com/plume-lib/plume-scripts.git || (sleep 1m && git clone --depth=1 --depth 1 -q https://github.com/plume-lib/plume-scripts.git)))
 fi
 
 # Code style & quality
