995de9570146eb5d18f556f95b6c336145a626b2
Mark Roberts <markro@cs.washington.edu>
2025-07-09 14:05:19 -0700

    remove warning for JNI access to native code

--- java/daikon/DynComp.java
+++ java/daikon/DynComp.java
@@ -304,6 +304,10 @@ public class DynComp {
       // allow DCRuntime to make reflective access to java.land.Object.clone() without a warning
       cmdlist.add("--add-opens");
       cmdlist.add("java.base/java.lang=ALL-UNNAMED");
+      if (BcelUtil.javaVersion >= 24) {
+        // needed to eliminate warning for JNI access to native code
+        cmdlist.add("--enable-native-access=ALL-UNNAMED");
+      }
       if (!no_jdk) {
         // If we are processing JDK classes, then we need our code on the boot classpath as well.
         // Otherwise, references to DCRuntime from the JDK would fail.
