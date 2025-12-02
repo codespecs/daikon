0607b724c4976dc4ff37eb15b95f7523e57430d0
Michael Ernst <mernst@cs.washington.edu>
2025-12-02 11:58:32 -0800

    Address CodeRabbit comment

--- java/daikon/dcomp/DCInstrument.java
+++ java/daikon/dcomp/DCInstrument.java
@@ -4222,7 +4222,7 @@ public class DCInstrument extends InstructionListUtils {
    */
   static void save_static_field_id(File file) throws IOException {
 
-    PrintStream ps = new PrintStream(file, "UTF-8"); // in Java 9+, use: StandardCharsets.UTF_8
+    PrintStream ps = new PrintStream(file, "UTF-8"); // in Java 10+, use: StandardCharsets.UTF_8
     for (Map.Entry<@KeyFor("static_field_id") String, Integer> entry : static_field_id.entrySet()) {
       ps.printf("%s  %d%n", entry.getKey(), entry.getValue());
     }
