18158fd2c3e80ee0fb40cfff391d6d606f8fda47
Michael Ernst <mernst@cs.washington.edu>
2025-12-19 23:19:22 -0800

    Use `is-ci.sh` from plume-scripts

--- scripts/test-typecheck-with-bundled-cf.sh
+++ scripts/test-typecheck-with-bundled-cf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Bash, not sh, because of `set -o pipefail`.
+# Use bash, not sh, because of `set -o pipefail`.
 
 # This is the "typecheck" job of the pull request.
 # It uses the bundled version of the Checker Framework: the one commited to the Daikon repostiory.
@@ -14,18 +14,19 @@ export SHELLOPTS
 # # Output expanded lines of this script as they are executed.
 # set -o xtrace
 
-make showvars
-make compile daikon.jar
-
-unset CHECKERFRAMEWORK
-
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 cd "${SCRIPT_DIR}"/..
 
+make showvars
+make compile daikon.jar
+
+# Use the bundled version of the Checker Framework.
+unset CHECKERFRAMEWORK
+
 # Under CI, there are two CPUs, but limit to 1 to avoid out-of-memory error.
-if [ -n "$("${SCRIPT_DIR}"/is-ci.sh)" ]; then
-  make -C java typecheck
+if [ -n "$(.plume-scripts/is-ci.sh)" ]; then
+  num_jobs=1
 else
   num_jobs="$(nproc || sysctl -n hw.ncpu || getconf _NPROCESSORS_ONLN || echo 1)"
-  make -C java --jobs="$num_jobs" typecheck
 fi
+make -C java --jobs="$num_jobs" typecheck
--- scripts/test-typecheck-with-latest-cf.sh
+++ scripts/test-typecheck-with-latest-cf.sh
@@ -20,24 +20,18 @@ cd "${SCRIPT_DIR}"/..
 make showvars
 make compile daikon.jar
 
-if test -d utils/git-scripts/.git; then
-  (cd utils/git-scripts && (git pull -q || (sleep 1m && (git pull || true))))
-elif ! test -d utils/git-scripts; then
-  (mkdir -p utils && cd utils && (git clone -q --depth 1 https://github.com/plume-lib/git-scripts.git || (sleep 1m && git clone -q --depth 1 https://github.com/plume-lib/git-scripts.git)))
-fi
-
 # Use a version of the Checker Framework cloned from a GitHub
 # repository, NOT the version checked in at java/lib/checker-framework/.
+make update-git-scripts
 utils/git-scripts/git-clone-related typetools checker-framework
-
 (cd ../checker-framework && ./gradlew assembleForJavac --console=plain -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000)
 CHECKERFRAMEWORK=$(realpath ../checker-framework)
 export CHECKERFRAMEWORK
 
 # Under CI, there are two CPUs, but limit to 1 to avoid out-of-memory error.
-if [ -n "$("${SCRIPT_DIR}"/is-ci.sh)" ]; then
-  make -C java typecheck
+if [ -n "$(".plume-scripts"/is-ci.sh)" ]; then
+  num_jobs=1
 else
   num_jobs="$(nproc || sysctl -n hw.ncpu || getconf _NPROCESSORS_ONLN || echo 1)"
-  make -C java --jobs="$num_jobs" typecheck
 fi
+make -C java --jobs="$num_jobs" typecheck
