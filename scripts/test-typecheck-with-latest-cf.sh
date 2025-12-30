e503f618c7d08f8c88a441a0fda576ac5233cb25
Michael Ernst <mernst@cs.washington.edu>
2025-12-29 21:38:37 -0500

    Fix target

--- scripts/test-typecheck-with-latest-cf.sh
+++ scripts/test-typecheck-with-latest-cf.sh
@@ -46,5 +46,5 @@ fi
 if [ "$#" -eq 0 ]; then
   make -C java --jobs="$num_jobs" typecheck
 else
-  make -C java --jobs="$num_jobs" "typecheck-part$1"
+  make -C java --jobs="$num_jobs" "typecheck-$1"
 fi
