c77b7c7f19192f08740a0bf4dc2e744187985765
Michael Ernst <mernst@cs.washington.edu>
2026-01-08 11:40:08 -0800

    Better cleaning and commenting

--- java/daikon/chicory/MethodInfo.java
+++ java/daikon/chicory/MethodInfo.java
@@ -214,14 +214,17 @@ public class MethodInfo {
   public void init_traversal(int depth) {
 
     traversalEnter = RootInfo.enter_process(this, depth);
-    // System.out.printf("Method %s.%s: %n ", class_info.clazz.getName(),
-    //                    this);
-    // System.out.printf("Enter daikon variable tree%n%s%n",
-    //                    traversalEnter.treeString());
+
+    if (false) {
+      System.out.printf("Method %s.%s: %n ", class_info.clazz.getName(), this);
+      System.out.printf("Enter daikon variable tree%n%s%n", traversalEnter.treeString());
+    }
 
     traversalExit = RootInfo.exit_process(this, depth);
-    // System.out.printf("Exit daikon variable tree%n%s%n",
-    //                    traversalExit.treeString());
+
+    if (false) {
+      System.out.printf("Exit daikon variable tree%n%s%n", traversalExit.treeString());
+    }
   }
 
   @SideEffectFree
