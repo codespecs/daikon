a2b3de8d0b9576a60f86d325e13a3e0d33d09ff0
Mark Roberts <markro@cs.washington.edu>
2026-07-07 13:09:09 -0700

    fix list of problem annotations for jdk26

--- java/daikon/dcomp/DCInstrument.java
+++ java/daikon/dcomp/DCInstrument.java
@@ -254,6 +254,31 @@ public class DCInstrument extends InstructionListUtils {
       };
 
   /**
+   * A list of annotations that must be removed from our instrumented copy of the
+   * original method.
+   */
+  private static final Set<String> BLACKLISTED_ANNOTATIONS = Set.of(
+    // Intrinsic processing hooks:
+    // We do not want to copy the IntrinsicCandidate annotations from
+    // the original method to our instrumented method as the signature will
+    // not match anything in the JVM's list.  This won't cause an execution
+    // problem but will produce a potentially large number of warnings.
+    "Ljdk/internal/HotSpotIntrinsicCandidate;",        // legacy from JDK 9
+    "Ljava/lang/annotation/IntrinsicCandidate;",       // legacy from JDK 16
+    "Ljdk/internal/vm/annotation/IntrinsicCandidate;", // new with JDK 26
+
+    // Project Leyden AOT optimization hooks (New in Java 26):
+    // These annotations are part of the changes to improve Java’s startup time
+    // and memory footprint through Ahead-of-Time (AOT) caching. These new
+    // annotations will cause a java/lang/ClassFormatError during initialization
+    // of the Java VM if they are not removed from our instrumented methods.
+    // (Technically, these annotations are only a problem when we instrument a
+    // JDK method, but to be thorough we do it for all methods.)
+    "Ljdk/internal/vm/annotation/AOTRuntimeSetup;",
+    "Ljdk/internal/vm/annotation/AOTSafeClassInitializer;"
+  );
+
+  /**
    * List of Object methods. Since we can't instrument Object, none of these can be instrumented,
    * and most of them don't provide useful comparability information anyway. The equals method and
    * the clone method are special-cased in the {@link #handleInvoke} routine.
@@ -663,16 +688,12 @@ public class DCInstrument extends InstructionListUtils {
 
         remove_local_variable_type_table(mgen);
 
-        // We do not want to copy the @HotSpotIntrinsicCandidate annotations from
-        // the original method to our instrumented method as the signature will
-        // not match anything in the JVM's list.  This won't cause an execution
-        // problem but will produce a massive number of warnings.
-        // JDK 11: @HotSpotIntrinsicCandidate
-        // JDK 17: @IntrinsicCandidate
+        // Do not copy any problematic annotations from the original
+        // method to our instrumented method.
         AnnotationEntryGen[] aes = mgen.getAnnotationEntries();
         for (AnnotationEntryGen item : aes) {
           String type = item.getTypeName();
-          if (type.endsWith("IntrinsicCandidate;")) {
+          if (BLACKLISTED_ANNOTATIONS.contains(type)) {
             mgen.removeAnnotationEntry(item);
           }
         }
@@ -937,16 +958,12 @@ public class DCInstrument extends InstructionListUtils {
 
         remove_local_variable_type_table(mgen);
 
-        // We do not want to copy the @HotSpotIntrinsicCandidate annotations from
-        // the original method to our instrumented method as the signature will
-        // not match anything in the JVM's list.  This won't cause an execution
-        // problem but will produce a massive number of warnings.
-        // JDK 11: @HotSpotIntrinsicCandidate
-        // JDK 17: @IntrinsicCandidate
+        // Do not copy any problematic annotations from the original
+        // method to our instrumented method.
         AnnotationEntryGen[] aes = mgen.getAnnotationEntries();
         for (AnnotationEntryGen item : aes) {
           String type = item.getTypeName();
-          if (type.endsWith("IntrinsicCandidate;")) {
+          if (BLACKLISTED_ANNOTATIONS.contains(type)) {
             mgen.removeAnnotationEntry(item);
           }
         }
