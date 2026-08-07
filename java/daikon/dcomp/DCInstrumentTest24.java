79f01e0c4b4d232487228cee29887ad48462d078
Michael Ernst <mernst@cs.washington.edu>
2026-08-07 14:46:09 -0700

    Remove warning suppression

--- java/daikon/dcomp/DCInstrumentTest24.java
+++ java/daikon/dcomp/DCInstrumentTest24.java
@@ -160,7 +160,6 @@ public final class DCInstrumentTest24 {
    *
    * @throws IOException if a class file cannot be read
    */
-  @SuppressWarnings("nullness:argument") // for assertNotNull
   @Test
   public void staleWorklistDoesNotAffectLaterClasses() throws IOException {
     boolean savedJdkInstrumented = Premain.jdk_instrumented;
