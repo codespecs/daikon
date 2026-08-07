22b6381d6b47acdf2d0f847040e1ed883de15011
Michael Ernst <mernst@cs.washington.edu>
2026-08-07 05:07:22 -0700

    Handle null

---
+++ java/daikon/test/AllTestsSuite24.java
@@ -0,0 +1,15 @@
+package daikon.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Daikon's unit tests that require Java 24 or later. Does not include system tests.
+ *
+ * @see AllTestsSuite
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  daikon.dcomp.CalcStackTest24.class,
+})
+public final class AllTestsSuite24 {}
