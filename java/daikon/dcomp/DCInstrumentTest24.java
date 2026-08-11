21897a03224566572112e74106eb86739d37e41d
Michael Ernst <mernst@cs.washington.edu>
2026-08-11 05:23:51 -0700

    Hack to make CI pass

--- java/daikon/dcomp/DCInstrumentTest24.java
+++ java/daikon/dcomp/DCInstrumentTest24.java
@@ -160,7 +160,7 @@ public final class DCInstrumentTest24 {
    *
    * @throws IOException if a class file cannot be read
    */
-  @SuppressWarnings("nullness:argument") // bug in CF?
+  @SuppressWarnings({"nullness:argument", "nullness:unneeded.suppression"}) // TODO: bug in CF?
   @Test
   public void staleWorklistDoesNotAffectLaterClasses() throws IOException {
     boolean savedJdkInstrumented = Premain.jdk_instrumented;
