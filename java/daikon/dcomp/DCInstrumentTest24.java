657ad64a9ad0fbf5c3e78b2c1eebcc906566b12f
Mark Roberts <markro@cs.washington.edu>
2026-09-06 16:20:08 -0700

    fix lock checker issue

--- java/daikon/dcomp/DCInstrumentTest24.java
+++ java/daikon/dcomp/DCInstrumentTest24.java
@@ -1029,7 +1029,8 @@ public final class DCInstrumentTest24 {
         if (!definitions.containsKey(name)) {
           return super.loadClass(name, resolve);
         }
-        synchronized (getClassLoadingLock(name)) {
+        Object lock = getClassLoadingLock(name);
+        synchronized (lock) {
           Class<?> result = findLoadedClass(name);
           if (result == null) {
             byte[] bytes = definitions.get(name);
